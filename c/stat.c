0c46ef230fa28cac0c316cf1acd67ee1e6b4c89f
William Pursell <williamp@wepay.com>
2018-02-26 13:57:26 -0800

    Add stat.c

---
+++ c/stat.c
@@ -0,0 +1,28 @@
+#include <sys/stat.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <unistd.h>
+#include <uuid/uuid.h>
+
+
+void print_user_ID(char* filepath) {
+    struct stat sb;
+    struct passwd pwent;  
+    struct passwd *pwentp;
+    char buf[_SC_GETPW_R_SIZE_MAX];
+
+    if(stat(filepath, &sb) == -1) {
+        perror("stat"); 
+    }
+
+    if (!getpwuid_r(sb.st_uid, &pwent, buf, sizeof(buf), &pwentp))  
+        printf("%10s\n", pwent.pw_name);  
+    else  
+        printf("%7d\n", sb.st_uid);  
+}
+
+
+int main(int argc, char **argv) {
+	print_user_ID(argc > 1 ? argv[1] : "foo");
+}
