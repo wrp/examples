93c46530c66fb7bb07d52783bb4850ee86ed011c
William Pursell <williamp@wepay.com>
2018-03-08 13:00:44 -0800

    Add StringIO ConfigParser example

--- python/config/example.py
+++ python/config/example.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from ConfigParser import ConfigParser
+from StringIO import StringIO
 
 C = ConfigParser()
 C.read('example.ini')
@@ -8,3 +9,18 @@ C.read('example.ini')
 print C.sections()
 print C.get('SectionOne','status')  # Case insenstive
 print C.get('SectionTwo','name')  # Case insenstive
+
+
+a = StringIO('''
+[foo]
+status: yes
+Name = bob
+
+[bar]
+status: no
+Name = doug
+''')
+
+C.readfp(a)
+print C.get('bar','status')  # Case insenstive
+print C.get('foo','name')  # Case insenstive
