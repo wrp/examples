b05df52b07b7947cae3697a8c1311236a04e0ccf
William Pursell <williamp@wepay.com>
2018-04-03 16:57:10 -0700

    Parse a properties file

--- java/properties.java
+++ java/properties.java
@@ -1,9 +1,14 @@
 
 package william;
 import java.util.Properties;
+import java.io.FileInputStream;
 
 public class properties {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
+		Properties props = new Properties();
+		props.load(new FileInputStream(args[0]));
+
+
 		System.out.println("hello from properties");
 	}
 }
