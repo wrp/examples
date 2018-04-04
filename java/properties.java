03b800f9189229d5fa09cf633a7cc371b2e9d3d7
William Pursell <williamp@wepay.com>
2018-04-03 19:30:21 -0700

    Load Yaml as a map

--- java/properties.java
+++ java/properties.java
@@ -3,17 +3,26 @@ package william;
 import java.util.Properties;
 import java.io.FileInputStream;
 import org.yaml.snakeyaml.Yaml;
+import java.util.Map;
+import java.util.HashMap;
 
 public class properties {
 	public static void main(String args[]) throws Exception {
+		Map<Object,Object> v = new HashMap<Object, Object>();
 		Properties props = new Properties();
 		props.load(System.in);
 		props.put("foo", "bar");
 		props.list(System.out);
 
+		v.put(6,9);
+		System.out.println(v.get(6));
+
 		Yaml yaml = new Yaml();
-		String doc = "\nfoo\n- bar\n- baz\n- qux\n";
+		String doc = "\nfoo: bar\nbaz: qux\n";
 		System.out.println(yaml.load(doc));
+
+		Map obya = (Map)yaml.load(doc);
+		System.out.println( obya.get("foo"));
 	}
 }
 
