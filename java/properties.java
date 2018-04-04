f7496b5ffe9bb438b3b92d626a907f2b58745f2e
William Pursell <williamp@wepay.com>
2018-04-03 17:01:53 -0700

    Actually load and set some properties

--- java/properties.java
+++ java/properties.java
@@ -6,10 +6,9 @@ import java.io.FileInputStream;
 public class properties {
 	public static void main(String args[]) throws Exception {
 		Properties props = new Properties();
-		props.load(new FileInputStream(args[0]));
-
-
-		System.out.println("hello from properties");
+		props.load(System.in);
+		props.put("foo", "bar");
+		props.list(System.out);
 	}
 }
 
