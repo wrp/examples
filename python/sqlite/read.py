5fdeb91734c97d1f17c04dbf83244fb00a69eaca
William Pursell <williamp@wepay.com>
2018-05-08 09:41:17 -0700

    Add show tables example in sqlite

--- python/sqlite/read.py
+++ python/sqlite/read.py
@@ -8,6 +8,9 @@ path = 'sqlite.example'
 db = sqlite3.connect(path)
 c = db.cursor()
 
+c.execute('SELECT name from sqlite_master WHERE type="table";')
+print c.fetchall()
+
 c.execute('SELECT * from ops')
 print c.fetchall()
 
