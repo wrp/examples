f77cf2fb661306ee93323b9994fa708d3bc1db54
William Pursell <williamp@wepay.com>
2018-08-27 09:28:58 -0600

    Add json-validate.py

---
+++ python/json-validate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# Validate json
+import sys
+import json
+d = json.load(sys.stdin)
+print d
+
+#k = {'foo':'bar'}
+
+#print json.dumps(k)
+
+#data = sys.stdin.read()
+#d = json.loads(data)
+
