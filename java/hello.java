5477ffc01da3840cc9e60ba08882240ccc8248c7
William Pursell <williamp@wepay.com>
2018-08-27 09:31:26 -0600

    Use %s vs {} in java format

--- java/hello.java
+++ java/hello.java
@@ -1,6 +1,6 @@
 
 public class hello {
 	public static void main(String args[]) {
-		System.out.println(String.format("hello {}", "world"));
+		System.out.println(String.format("hello %s", "world"));
 	}
 }
