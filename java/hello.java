5abfffc1f0800e851c1df13f0101f10dcbe63bf7
William Pursell <williamp@wepay.com>
2018-09-28 15:43:11 +0000

    Print info from the class loader

--- java/hello.java
+++ java/hello.java
@@ -2,11 +2,11 @@
 public class hello {
 	public static void main(String args[]) {
 		System.out.println(String.format("hello %s", "world"));
-/*		System.out.println(
-			hello.getClass()
+		System.out.println(
+			/* hello.getClass() */
+			hello.class
 			.getClassLoader()
 			.getResource("hello.class")
 		);
-		*/
 	}
 }
