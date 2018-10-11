82caf4d9a98b4d5a9a68edde305c6af85a66076d
William Pursell <williamp@wepay.com>
2018-10-11 12:50:10 -0700

    Demonstrate java write to stderr

--- java/hello.java
+++ java/hello.java
@@ -2,6 +2,7 @@
 public class hello {
 	public static void main(String args[]) {
 		System.out.println(String.format("hello %s", "world"));
+		System.err.println(String.format("error"));
 		System.out.println(
 			/* hello.getClass() */
 			hello.class
