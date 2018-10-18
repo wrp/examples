cb0d2502c2a5c005417ee6621bd9dd1d2c1e76eb
William Pursell <williamp@wepay.com>
2018-10-18 14:39:38 -0700

    Demonstrate use of FileInputStream

--- java/hello.java
+++ java/hello.java
@@ -1,13 +1,26 @@
 
+import java.io.FileInputStream;
+
 public class hello {
 	public static void main(String args[]) {
 		System.out.println(String.format("hello %s", "world"));
-		System.err.println(String.format("error"));
 		System.out.println(
 			/* hello.getClass() */
 			hello.class
 			.getClassLoader()
 			.getResource("hello.class")
 		);
+
+		/* Read name from a file */
+		try {
+			FileInputStream f = new FileInputStream(args[0]);
+			int c;
+			while(( c = f.read()) != -1) {
+				System.out.print((char)c);
+			}
+		}
+		catch( Exception e) {
+			System.err.print(e + "\n");
+		}
 	}
 }
