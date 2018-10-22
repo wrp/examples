cc345383baf996875b1acff86924df395955e440
William Pursell <williamp@wepay.com>
2018-10-22 14:26:53 -0600

    Documentation

--- java/log.java
+++ java/log.java
@@ -1,5 +1,12 @@
 /* Example using log4j */
 
+/* Check the log config in config/log4j for a sample that causes
+ * debug and info to go to stdout, warn and error go to stderr.
+ * Note that if we uncommend the BasicConfigurator.configure()
+ * invocation, cruft will still go to stdout.  There's probably
+ * something we could do to prevent that... maybe?
+ * */
+
 import org.apache.log4j.Logger;
 import org.apache.log4j.BasicConfigurator;
 
