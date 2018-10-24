3c0f32bee82fafde13a6ed6324febfeada3e8254
William Pursell <williamp@wepay.com>
2018-10-23 21:28:51 -0600

    Add java/map_example.java

---
+++ java/map_example.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+
+
+public class map_example {
+	static class cmdlinearg {
+		public String short_name;
+		public String description;
+		public Boolean required;
+
+		cmdlinearg(String short_name, String description, Boolean req) {
+			this.short_name = short_name;
+			this.description = description;
+			this.required = req;
+		}
+	}
+
+	public static void main(String args[]) {
+		Map<Integer, String> m = new HashMap<Integer, String>();
+		m.put(5, "five");
+		for(Map.Entry mp:m.entrySet()) {  
+			System.out.println(mp.getKey() + ": "  + mp.getValue());  
+		}  
+		System.out.println("direct access: " + m.get(5));
+
+		Map<String, cmdlinearg> v = new HashMap<String, cmdlinearg>();
+		v.put("test", new cmdlinearg("t", "test func", true));
+		for(Map.Entry mp:v.entrySet()) {  
+			System.out.println(mp.getKey() + ": "  + mp.getValue());  
+		}  
+		System.out.println("direct access: " + v.get("test").description);
+	}
+}
