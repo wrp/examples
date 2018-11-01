ac7652677ca63981c1a7b4eebfb668e7ea4c5623
William Pursell <williamp@wepay.com>
2018-11-01 21:02:16 +0000

    Write config file into code (log4j)

--- java/log.java
+++ java/log.java
@@ -8,7 +8,10 @@
  * */
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.Level;
+import org.apache.log4j.varia.LevelRangeFilter;
 
 public class log{
 
@@ -17,6 +20,40 @@ public class log{
    
 	public static void main(String[] args) {
 /*		BasicConfigurator.configure();*/
+/*
+log4j.appender.stdout = org.apache.log4j.ConsoleAppender
+log4j.appender.stdout.Threshold = TRACE
+log4j.appender.stdout.Target   = System.out
+log4j.appender.stdout.layout = org.apache.log4j.PatternLayout
+log4j.appender.stdout.layout.ConversionPattern = %-5p %d [%t][%F:%L] : %m%n
+log4j.appender.stdout.filter.filter1=org.apache.log4j.varia.LevelRangeFilter
+log4j.appender.stdout.filter.filter1.levelMin=TRACE
+log4j.appender.stdout.filter.filter1.levelMax=INFO
+
+log4j.appender.stderr = org.apache.log4j.ConsoleAppender
+log4j.appender.stderr.Threshold = WARN
+log4j.appender.stderr.Target   = System.err
+log4j.appender.stderr.layout = org.apache.log4j.PatternLayout
+log4j.appender.stderr.layout.ConversionPattern = %m%n
+*/
+		ConsoleAppender stdout = new ConsoleAppender();
+		stdout.setThreshold(Level.TRACE);
+		stdout.setLayout(new PatternLayout( new String ("%m%n")));
+		LevelRangeFilter infoFilter = new LevelRangeFilter();
+		infoFilter.setLevelMin(Level.TRACE);
+		infoFilter.setLevelMax(Level.INFO);
+		infoFilter.setAcceptOnMatch(true);
+		stdout.addFilter(infoFilter);
+		stdout.activateOptions();
+		Logger.getRootLogger().addAppender(stdout);
+
+		ConsoleAppender stderr = new ConsoleAppender();
+		stderr.setThreshold(Level.WARN);
+		stderr.setLayout(new PatternLayout( new String ("%m%n")));
+		stderr.setTarget("System.err");
+		stderr.activateOptions();
+		Logger.getRootLogger().addAppender(stderr);
+
 		log.debug("Hello this is a debug message");
 		log.info("Hello this is an info message");
 		log.warn("Hello this is an warn message");
