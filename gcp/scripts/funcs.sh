978a02c8b2840a70227f5086dd4df4342b643ed2
William <william.r.pursell@gmail.com>
2019-05-04 14:06:13 +0000

    Ignore white space by default when printing diff

--- gcp/scripts/funcs.sh
+++ gcp/scripts/funcs.sh
@@ -1,10 +1,12 @@
 
 die() { test -n "$*" && echo "$*" >&2; exit 1; }
 
+# Set IGNORE_WHITE to the empty string to suppress passing -w to git diff
 interactive_commit() {
 	local response
 	msg="$1"
-	if git diff --cached | grep .; then
+	if ! git diff-index --exit-code HEAD --; then # repo is changed
+		git diff ${IGNORE_WHITE--w} --cached
 		if test -z "$FORCE"; then
 			printf "%s ([n]yR)? " "Commit the above changes" > /dev/tty
 			read response < /dev/tty
