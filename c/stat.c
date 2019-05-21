d7b2524be496831812e007631b0e71fc29b6b5cb
William <william.r.pursell@gmail.com>
2019-05-21 16:15:43 +0000

    Autoconfiscate example dir
    To generate configure script, run "autoreconf -ivf"

--- c/stat.c
+++ c/stat.c
@@ -3,23 +3,27 @@
 #include <sys/types.h>
 #include <pwd.h>
 #include <unistd.h>
+#if 0
 #include <uuid/uuid.h>
+#endif
 
 
 void print_user_ID(char* filepath) {
     struct stat sb;
-    struct passwd pwent;  
+#if 0
+    struct passwd pwent;
     struct passwd *pwentp;
     char buf[_SC_GETPW_R_SIZE_MAX];
 
     if(stat(filepath, &sb) == -1) {
-        perror("stat"); 
+        perror("stat");
     }
 
-    if (!getpwuid_r(sb.st_uid, &pwent, buf, sizeof(buf), &pwentp))  
-        printf("%10s\n", pwent.pw_name);  
-    else  
-        printf("%7d\n", sb.st_uid);  
+    if (!getpwuid_r(sb.st_uid, &pwent, buf, sizeof(buf), &pwentp))
+        printf("%10s\n", pwent.pw_name);
+    else
+        printf("%7d\n", sb.st_uid);
+#endif
 }
 
 
