3650046a131549fa6f384d12c821cde76228f44e
William <william.r.pursell@gmail.com>
2019-06-01 15:17:02 +0000

    Handle decimal input

--- c/calc.c
+++ c/calc.c
@@ -5,6 +5,8 @@
 #define siz(x) ( (sizeof (x) / sizeof *(x)))
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 #include <ctype.h>
 
 struct state {
@@ -16,15 +18,42 @@ struct state {
 	int precision;
 };
 
+
+void
+append_digit(int c, struct state *S)
+{
+	assert( isdigit(c) || c == '.' || c == 'e' || c == 'E' || c == '_' );
+	if( c == '_' ) {
+		return;
+	}
+	*S->bp++ = (char)c;
+}
+
+void
+compute_value(struct state *S)
+{
+	char *end;
+	*S->bp = '\0';
+	if( S->bp == S->buf ) {
+		return;
+	}
+	*(++S->sp) = strtod(S->buf, &end);
+	if( end != S->bp ) {
+		fprintf(stderr, "Garbled: %s\n", S->buf);
+	}
+	S->bp = S->buf;
+}
+
+
 int
 main(int argc, char **argv)
 {
 	int c;
 	struct state S;
 	char fmt[32];
-	int new = 1;
 
 	S.sp = S.stack + 1;
+	S.bp = S.buf;
 	S.width = 6;
 	S.precision = 3;
 
@@ -33,8 +62,14 @@ main(int argc, char **argv)
 		case '0': case '1': case '2': case '3': case '4':
 		case '5': case '6': case '7': case '8': case '9':
 		case '.': case 'e': case 'E': case '_':
-			if( new ) *(++S.sp) = 0.0;
-			*S.sp = 10.0 * *S.sp + (c - '0'); break;
+			append_digit(c, &S); break;
+		default:
+			compute_value(&S);
+		}
+		switch(c) {
+		case '0': case '1': case '2': case '3': case '4':
+		case '5': case '6': case '7': case '8': case '9':
+		case '.': case 'e': case 'E': case '_': break;
 		case '*':
 			S.sp -= 1; S.sp[0] *= S.sp[1]; break;
 		case ' ': case '\n': case '\t': case ',':
@@ -58,7 +93,6 @@ main(int argc, char **argv)
 		case 'q':
 			goto end;
 		}
-		new = !isdigit(c);
 		if( S.sp == S.stack || S.sp - S.stack >= siz(S.stack)) {
 			fprintf(stderr, "%sflow\n", S.sp == S.stack ? "Under" : "Over");
 			goto fail;
