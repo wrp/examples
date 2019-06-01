ed9ad136a758aeeb03f1ce6074f74f8d32f52cc2
William <william.r.pursell@gmail.com>
2019-06-01 15:01:47 +0000

    Add a state structure

--- c/calc.c
+++ c/calc.c
@@ -7,41 +7,51 @@
 #include <stdio.h>
 #include <ctype.h>
 
+struct state {
+	double stack[1024];
+	double *sp;
+	char buf[32];
+	char *bp;
+	int width;
+	int precision;
+};
+
 int
 main(int argc, char **argv)
 {
 	int c;
-	double stack[1024];
-
-	double *sp = stack + 1;
-	int new=1;
-	int width = 6;
-	int precision = 3;
+	struct state S;
 	char fmt[32];
+	int new = 1;
+
+	S.sp = S.stack + 1;
+	S.width = 6;
+	S.precision = 3;
 
 	while( (c=getchar()) != EOF ) {
 		switch(c) {
 		case '0': case '1': case '2': case '3': case '4':
 		case '5': case '6': case '7': case '8': case '9':
-			if( new ) *(++sp) = 0.0;
-			*sp = 10.0 * *sp + (c - '0'); break;
+		case '.': case 'e': case 'E': case '_':
+			if( new ) *(++S.sp) = 0.0;
+			*S.sp = 10.0 * *S.sp + (c - '0'); break;
 		case '*':
-			sp -= 1; sp[0] *= sp[1]; break;
+			S.sp -= 1; S.sp[0] *= S.sp[1]; break;
 		case ' ': case '\n': case '\t': case ',':
 			break;
 		case '+':
-			sp -= 1; sp[0] += sp[1]; break;
+			S.sp -= 1; S.sp[0] += S.sp[1]; break;
 		case '/':
-			sp -= 1; sp[0] /= sp[1]; break;
+			S.sp -= 1; S.sp[0] /= S.sp[1]; break;
 		case '-':
-			sp -= 1; sp[0] -= sp[1]; break;
+			S.sp -= 1; S.sp[0] -= S.sp[1]; break;
 		case 'k':
-			precision = *sp--; break;
+			S.precision = *S.sp--; break;
 		case 'w':
-			width = *sp--; break;
+			S.width = *S.sp--; break;
 		case 'p':
-			snprintf(fmt, sizeof fmt, "%%%d.%dg\n", width, precision);
-			printf(fmt, sp[0]);
+			snprintf(fmt, sizeof fmt, "%%%d.%dg\n", S.width, S.precision);
+			printf(fmt, S.sp[0]);
 			break;
 		default:
 			fprintf(stderr, "Unrecognized value: %c\n", c);
@@ -49,8 +59,8 @@ main(int argc, char **argv)
 			goto end;
 		}
 		new = !isdigit(c);
-		if( sp == stack || sp - stack >= siz(stack)) {
-			fprintf(stderr, "%sflow\n", sp == stack ? "Under" : "Over");
+		if( S.sp == S.stack || S.sp - S.stack >= siz(S.stack)) {
+			fprintf(stderr, "%sflow\n", S.sp == S.stack ? "Under" : "Over");
 			goto fail;
 		}
 	}
