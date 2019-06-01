5d520fc5efccd2153cd1c570f1e99d39bfe9d9b2
William <william.r.pursell@gmail.com>
2019-06-01 23:35:46 +0000

    Move switch statment into function

--- c/calc.c
+++ c/calc.c
@@ -20,18 +20,33 @@ struct state {
 
 
 void
-compute_value(struct state *S)
+apply_operator(struct state *S, int c)
 {
 	char *end;
+	char fmt[32];
 	*S->bp = '\0';
-	if( S->bp == S->buf ) {
-		return;
+	if( S->bp != S->buf ) {
+		*(++S->sp) = strtod(S->buf, &end);
+		if( end != S->bp ) {
+			fprintf(stderr, "Garbled: %s\n", S->buf);
+		}
+		S->bp = S->buf;
 	}
-	*(++S->sp) = strtod(S->buf, &end);
-	if( end != S->bp ) {
-		fprintf(stderr, "Garbled: %s\n", S->buf);
+
+	switch(c) {
+	case '*': S->sp -= 1; S->sp[0] *= S->sp[1]; break;
+	case '+': S->sp -= 1; S->sp[0] += S->sp[1]; break;
+	case '/': S->sp -= 1; S->sp[0] /= S->sp[1]; break;
+	case '^': S->sp -= 1; S->sp[0] = pow(S->sp[0], S->sp[1]); break;
+	case '-': S->sp -= 1; S->sp[0] -= S->sp[1]; break;
+	case 'k': S->precision = *S->sp--; break;
+	case 'w': S->width = *S->sp--; break;
+	case 'p':
+		snprintf(fmt, sizeof fmt, "%%%d.%dg\n", S->width, S->precision);
+		printf(fmt, S->sp[0]);
+		break;
+	case 'q': exit(1);
 	}
-	S->bp = S->buf;
 }
 
 
@@ -39,36 +54,21 @@ int
 main(int argc, char **argv)
 {
 	int c;
-	struct state S;
-	char fmt[32];
+	struct state S[1];
 
-	S.sp = S.stack + 1;
-	S.bp = S.buf;
-	S.width = 6;
-	S.precision = 3;
+	S->sp = S->stack + 1;
+	S->bp = S->buf;
+	S->width = 6;
+	S->precision = 3;
 
 	while( (c=getchar()) != EOF ) {
 		if(strchr("*+/^-kwpq \t\n", c)) {
-			compute_value(&S);
+			apply_operator(&S,c);
 		} else {
-			*S.bp++ = (char)c;
+			*S->bp++ = (char)c;
 		}
-		switch(c) {
-		case '*': S.sp -= 1; S.sp[0] *= S.sp[1]; break;
-		case '+': S.sp -= 1; S.sp[0] += S.sp[1]; break;
-		case '/': S.sp -= 1; S.sp[0] /= S.sp[1]; break;
-		case '^': S.sp -= 1; S.sp[0] = pow(S.sp[0], S.sp[1]); break;
-		case '-': S.sp -= 1; S.sp[0] -= S.sp[1]; break;
-		case 'k': S.precision = *S.sp--; break;
-		case 'w': S.width = *S.sp--; break;
-		case 'p':
-			snprintf(fmt, sizeof fmt, "%%%d.%dg\n", S.width, S.precision);
-			printf(fmt, S.sp[0]);
-			break;
-		case 'q': goto end;
-		}
-		if( S.sp == S.stack || S.sp - S.stack >= siz(S.stack)) {
-			fprintf(stderr, "%sflow\n", S.sp == S.stack ? "Under" : "Over");
+		if( S->sp == S->stack || S->sp - S->stack >= siz(S->stack)) {
+			fprintf(stderr, "%sflow\n", S->sp == S->stack ? "Under" : "Over");
 			goto fail;
 		}
 	}
