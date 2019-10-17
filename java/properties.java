6760b94dd536ae3acb89cc97866de6363b377ba3
William <william.r.pursell@gmail.com>
2019-10-17 17:43:54 +0000

    Pass a property to java class at command line

--- java/properties.java
+++ java/properties.java
@@ -1,31 +1,17 @@
 
 package subdir;
 import java.util.Properties;
-import java.io.FileInputStream;
-import org.yaml.snakeyaml.Yaml;
-import java.util.Map;
-import java.util.HashMap;
 
 public class properties {
 	public static void main(String args[]) throws Exception {
-		Map<Object,Object> v = new HashMap<Object, Object>();
 		Properties props = new Properties();
 		props.load(System.in);
 		props.put("foo", "bar");
 		props.list(System.out);
+		System.out.println("-- End of properties listing --");
 
-		v.put(6,9);
-		System.out.println(v.get(6));
-
-		Yaml yaml = new Yaml();
-		String doc = "\nfoo: bar\nbaz: qux\n";
-		System.out.println(yaml.load(doc));
-
-		Map obya = (Map)yaml.load(doc);
-		System.out.println( obya.get("foo"));
-
-		Map obyak = (Map)yaml.load(new FileInputStream("sample.yaml"));
-		System.out.println( obyak.get("foo"));
+		String line = System.getProperty("banana");
+		System.out.println(line);
 	}
 }
 
