ecff3b49ce003899ffd736829cab8ffd18eaa3c7
William <william.r.pursell@gmail.com>
2019-10-20 02:13:58 +0000

    Trivial refactor

--- c/pyramid.c
+++ c/pyramid.c
@@ -1,24 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-/*
- *      0
- *     101
- *    21012
- *   3210123
- *  432101234
- * 54321012345
- */
-
-void write_line(int line, int size)
-{
-	char template[]="9876543210123456789";
-	char *s = template + (9 - size);
-	char *e = s + size + line;
-	e[1] = '\0';
-	memset(s, ' ', size - line);
-	puts(s);
-}
+/* Print a pyramid */
 
 int
 main(int argc, char **argv)
@@ -28,9 +11,11 @@ main(int argc, char **argv)
 		fprintf(stderr, "Invalid size\n");
 		return EXIT_FAILURE;
 	}
-	for(int line = 0; line <= size; line++ ) {
-		write_line(line, size);
+	for(int line = 0; line <= size; line++) {
+		char template[]="9876543210123456789";
+		char *s = template + 9 - size;
+		template[10 + line] = '\0';
+		memset(s, ' ', size - line);
+		puts(s);
 	}
 }
-
-
