1c5c0ca7c8ef48bbb7e924bf4bfcc912efd69868
William <william.r.pursell@gmail.com>
2019-11-07 17:14:42 +0000

    Add kaprekar.c

---
+++ c/kaprekar.c
@@ -0,0 +1,51 @@
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/* Take a 4 digit number.  Rearrange in ascending/descending order.
+ * Find the difference.  Repeat
+ */
+
+int
+compare_char( const void *a, const void *b )
+{
+	return *(char *)a > *(char *)b;
+}
+
+/* Reverse a 4 character string */
+void rev4(char *a) {
+	assert(strlen(a) == 4);
+	for(int i=0; i < 2; i++) {
+		char t = a[i];
+		a[i] = a[3-i];
+		a[3-i] = t;
+	}
+}
+
+int
+process(int x)
+{
+	int a, b;
+	if( x > 9999 ) {
+		fprintf(stderr, "Invalid entry: %d\n", x);
+		exit(EXIT_FAILURE);
+	}
+	char v[5];
+	snprintf(v, 5, "%04d", x);
+	assert(strlen(v) == 4);
+	qsort(v, 4, 1, compare_char);
+	a = strtol(v, NULL, 10);
+	rev4(v);
+	b = strtol(v, NULL, 10);
+	return b - a;
+}
+
+int
+main(int argc, char **argv)
+{
+	int v = argc > 1 ? strtol(argv[1], NULL, 0) : 1233;
+	while(v != 6174) {
+		printf("v = %d\n", v = process(v));
+	}
+	return 0;
+}
