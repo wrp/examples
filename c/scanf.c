e44e6ba145c27231311c4d8318afac6ae0eee651
William Pursell <william.r.pursell@gmail.com>
2019-11-14 14:43:07 +0000

    Try printing strings

--- c/scanf.c
+++ c/scanf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
+#include <ctype.h>
 
+int isstring(const char *s);
 int
 main(int argc, char **argv)
 {
@@ -16,8 +19,16 @@ main(int argc, char **argv)
 		c = scanf(*argv, a, b, s, s, s, s, s, s, s, s);
 		printf("%s\t: ", *argv);
 		printf("scanned %d item%s, ", c, c > 1 ? "s" : "");
-		printf("first is '%c' (%02x%02x%02x%02x), ",
-			a[0], a[0], a[1], a[2], a[3]);
+		printf("first item scanned: ");
+		if( isprint(*a) ) {
+			printf("'%c' ", *a);
+		}
+		a[1023] = '\0';
+		if(strlen(a) < 60 && isstring(a)) {
+			printf("'%s' ", a);
+		} else {
+			printf("(%02x%02x%02x%02x), ", a[0], a[1], a[2], a[3]);
+		}
 		ld = ftell(stdin);
 		if( ld != -1 ) {
 			printf("stream now at position %ld, ", ld);
@@ -31,4 +42,15 @@ main(int argc, char **argv)
 			break;
 		}
 	}
+}
+
+int
+isstring(const char *s)
+{
+	for( ; *s; s++ ) {
+		if( !isprint(*s)) {
+			return 0;
+		}
+	}
+	return 1;
 }
