bab6b4697650ff005a626735860f02a93e6e2454
William <william.r.pursell@gmail.com>
2020-02-29 18:04:44 +0000

    Demontrate passing a reference

--- rust/projects/hello/src/main.rs
+++ rust/projects/hello/src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
 	let mut s = String::from("hello");
 	s.push_str(", world!");
+	print_msg(&s);  // Pass reference to prevent move
+	println!("{}", s);  // Still valid
+}
+
+
+
+fn print_msg(s: &String) {
 	println!("{}", s);
 }
