84f78f5b67945eda181ecfc6de9f70f946efe939
William <william.r.pursell@gmail.com>
2020-03-04 00:01:12 +0000

    Cleanup scanf example

--- c/scanf.c
+++ c/scanf.c
@@ -4,31 +4,25 @@
 #include <string.h>
 #include <ctype.h>
 
+void show_buf(char *a, int);
 int isstring(const char *s);
+
 int
 main(int argc, char **argv)
 {
 	int c;
 	long ld;
-	char a[1024] = "XXXXXXXXXXXXXXXX....";
-	char b[1024];
-	char s[1024];
+	char a[7][1024];
 
 	(void)argc;
 	for(argv += 1; *argv; argv++) {
-		c = scanf(*argv, a, b, s, s, s, s, s, s, s, s);
-		printf("%s\t: ", *argv);
-		printf("scanned %d item%s, ", c, c > 1 ? "s" : "");
-		printf("first item scanned: ");
-		if( isprint(*a) ) {
-			printf("'%c' ", *a);
+		c = scanf(*argv, a[0], a[1], a[2], a[3], a[4], a[5], a[6]);
+		printf("********\n%s\n: ", *argv);
+		printf("scanned %d item%s\n", c, c > 1 ? "s" : "");
+		for(int i = 0; i < c && i < 6; i++) {
+			show_buf(a[i], i);
 		}
-		a[1023] = '\0';
-		if(strlen(a) < 60 && isstring(a)) {
-			printf("'%s' ", a);
-		} else {
-			printf("(%02x%02x%02x%02x), ", a[0], a[1], a[2], a[3]);
-		}
+
 		ld = ftell(stdin);
 		if( ld != -1 ) {
 			printf("stream now at position %ld, ", ld);
@@ -44,11 +38,28 @@ main(int argc, char **argv)
 	}
 }
 
+void
+show_buf(char *a, int idx)
+{
+	printf("item (%d): ", idx);
+	if( isprint(*a) ) {
+		printf("'%c' ", *a);
+	}
+	a[1023] = '\0';
+	if(strlen(a) < 60 && isstring(a)) {
+		printf("'%s' ", a);
+	} else {
+		printf("(%02x%02x%02x%02x), ", a[0], a[1], a[2], a[3]);
+	}
+	putchar('\n');
+}
+
 int
 isstring(const char *s)
 {
 	for( ; *s; s++ ) {
-		if( !isprint(*s)) {
+		if( !isprint(*s) && *s != '\n') {
+			printf("unprintable: %02x\n", *s);
 			return 0;
 		}
 	}
