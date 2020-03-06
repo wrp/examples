8b82e13c25c879ead8c09c22027326e92d71b1f1
William <william.r.pursell@gmail.com>
2020-03-06 23:22:22 +0000

    Implement xfread

--- c/diff16.c
+++ c/diff16.c
@@ -9,6 +9,8 @@
  */
 
 FILE * xfopen(const char *, const char *);
+size_t xfread(void *, FILE *, const char *);
+
 
 int
 main(int argc, char **argv)
@@ -22,14 +24,8 @@ main(int argc, char **argv)
 	fp[0] = xfopen(argv[1], "r");
 	fp[1] = xfopen(argv[2], "r");
 	do {
-		n[0] = fread(buf[0], 1, 16, fp[0]);
-		n[1] = fread(buf[1], 1, 16, fp[1]);
-		for(int i=0; i < 2; i++) {
-			if(n[i] == 0 && ferror(fp[i])) {
-				fprintf(stderr, "Error reading %s\n", argv[i+1]);
-				return 1;
-			}
-		}
+		n[0] = xfread(buf[0], fp[0], argv[1]);
+		n[1] = xfread(buf[1], fp[1], argv[2]);
 		if( n[0] != n[1] || (n[0] && memcmp(buf[0], buf[1], n[0]))) {
 			fprintf(stderr, "files differ in block %u\n", count);
 			return 1;
@@ -40,6 +36,16 @@ main(int argc, char **argv)
 	return 0;
 }
 
+size_t
+xfread(void *b, FILE *fp, const char *name)
+{
+	size_t n = fread(b, 1, 16, fp);
+	if(n == 0 && ferror(fp)) {
+		fprintf(stderr, "Error reading %s\n", name);
+		exit(EXIT_FAILURE);
+	}
+	return n;
+}
 
 FILE *
 xfopen(const char *path, const char *mode)
