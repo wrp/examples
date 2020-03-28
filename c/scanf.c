689256c42ac28f20deea0f42da00681d5e878087
William <william.r.pursell@gmail.com>
2020-03-28 19:28:39 +0000

    Add scanf examples

--- c/scanf.c
+++ c/scanf.c
@@ -6,6 +6,20 @@
 
 void show_buf(char *a, int);
 int isstring(const char *s);
+int scan(const char *input, const char *fmt, ...);
+
+
+void
+simple_examples(void)
+{
+	char buf[128] = "XXXXXXXXXXXXx";
+	int k;
+	scan("input string", "%3s", buf); /* Writes 4 chars: 'inp\0' */
+	scan("input string", "%7s", buf); /* Writes 6 chars; 'input\0' */
+	scan("input string\nline 2", "%[^\n]s", buf); /* Writes first line */
+	scan("input string\nline 2", "%7[^\n]s", buf); /* Write 8 chars */
+	scan("24", "%d", &k);
+}
 
 int
 main(int argc, char **argv)
@@ -13,6 +27,8 @@ main(int argc, char **argv)
 	int c;
 	long ld;
 	char a[7][1024];
+
+	simple_examples();
 
 	(void)argc;
 	for(argv += 1; *argv; argv++) {
@@ -64,4 +80,59 @@ isstring(const char *s)
 		}
 	}
 	return 1;
+}
+
+/* Handy wrapper
+ * Incredibly fragile (does not match `[]` accurately, etc.  Just
+ * designed to work with all the cases given here. )
+ */
+int
+scan(const char *input, const char *fmt, ...)
+{
+	va_list ap;
+	int rv;
+	int type;
+	const char *e;
+	union {
+		char *s;
+		int *d;
+	} buf;
+
+	e = fmt;
+match:
+	if( (e = strchr(e, '%')) != NULL ) {
+		e += 1;
+		if( *e == '%' ) {
+			e += 1;
+			goto match;
+		}
+		e += strspn(e, ".0123456789");
+		if( *e == '[' ) {
+			e = strchr(e, ']') + 1; /* HAndle NULL */
+		}
+		e += strspn(e, ".0123456789");
+	} else {
+		fprintf(stderr, "Invalid format string: %s\n", fmt);
+	}
+	type = *e;
+	va_start(ap, fmt);
+        rv = vsscanf(input, fmt, ap);
+	va_end(ap);
+
+	printf("On input '%s' with format '%s' (type %c): ", input, fmt, type);
+	va_start(ap, fmt);
+	switch(type) {
+	case 's':
+		buf.s = va_arg(ap, char *);
+		printf("'%s'", buf.s);
+		break;
+	case 'd':
+		buf.d = va_arg(ap, int *);
+		printf("'%d'", *buf.d);
+		break;
+	}
+	va_end(ap);
+	putchar('\n');
+
+	return rv;
 }
