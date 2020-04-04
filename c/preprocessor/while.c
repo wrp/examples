0bf274e27bbae0ee7df3b37a21cadf8066ba4318
William <william.r.pursell@gmail.com>
2020-04-04 13:19:08 +0000

    Invoke the while pre-processor

--- c/preprocessor/while.c
+++ c/preprocessor/while.c
@@ -1,5 +1,5 @@
-/*
 
+#if 0
 From stackoverflow.com/a/10542793/140750
 
 If you want to implement a while loop, you will need to use recursion in the preprocessor. The easiest way to do recursion is to use a deferred expression. A deferred expression is an expression that requires more scans to fully expand:
@@ -72,4 +72,62 @@ Then using the WHILE macro:
 
 M(EVAL(WHILE(PRED, OP, x, y, z))) //Expands to xyz
 Of course, any kind of predicate or operator can be passed to it.
-*/
+#endif
+
+
+#define EMPTY()
+#define DEFER(id) id EMPTY()
+#define OBSTRUCT(id) id DEFER(EMPTY)()
+#define EXPAND(...) __VA_ARGS__
+#define EVAL(...)  EVAL1(EVAL1(EVAL1(__VA_ARGS__)))
+#define EVAL1(...) EVAL2(EVAL2(EVAL2(__VA_ARGS__)))
+#define EVAL2(...) EVAL3(EVAL3(EVAL3(__VA_ARGS__)))
+#define EVAL3(...) EVAL4(EVAL4(EVAL4(__VA_ARGS__)))
+#define EVAL4(...) EVAL5(EVAL5(EVAL5(__VA_ARGS__)))
+#define EVAL5(...) __VA_ARGS__
+
+#define CAT(a, ...) PRIMITIVE_CAT(a, __VA_ARGS__)
+#define PRIMITIVE_CAT(a, ...) a ## __VA_ARGS__
+
+#define CHECK_N(x, n, ...) n
+#define CHECK(...) CHECK_N(__VA_ARGS__, 0,)
+
+#define NOT(x) CHECK(PRIMITIVE_CAT(NOT_, x))
+#define NOT_0 ~, 1,
+
+#define COMPL(b) PRIMITIVE_CAT(COMPL_, b)
+#define COMPL_0 1
+#define COMPL_1 0
+
+#define BOOL(x) COMPL(NOT(x))
+
+#define IIF(c) PRIMITIVE_CAT(IIF_, c)
+#define IIF_0(t, ...) __VA_ARGS__
+#define IIF_1(t, ...) t
+
+#define IF(c) IIF(BOOL(c))
+
+#define WHILE(pred, op, ...) \
+    IF(pred(__VA_ARGS__)) \
+    ( \
+        OBSTRUCT(WHILE_INDIRECT) () \
+        ( \
+            pred, op, op(__VA_ARGS__) \
+        ), \
+        __VA_ARGS__ \
+    )
+#define WHILE_INDIRECT() WHILE
+
+#define NARGS_SEQ(_1,_2,_3,_4,_5,_6,_7,_8,N,...) N
+#define NARGS(...) NARGS_SEQ(__VA_ARGS__, 8, 7, 6, 5, 4, 3, 2, 1)
+
+#define IS_1(x) CHECK(PRIMITIVE_CAT(IS_1_, x))
+#define IS_1_1 ~, 1,
+
+#define PRED(x, ...) COMPL(IS_1(NARGS(__VA_ARGS__)))
+
+#define OP(x, y, ...) CAT(x, y), __VA_ARGS__
+#define M(...) CAT(__VA_ARGS__)
+
+
+M(EVAL(WHILE(PRED, OP, x, y, z))) //Expands to xyz
