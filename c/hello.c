2cc6a7e9c6e62eca8cb9fbe1d60810528332f9fa
William Pursell <williamp@wepay.com>
2020-06-11 10:58:57 +0000

    Use char *const argv[]

--- c/hello.c
+++ c/hello.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int
-main(int argc, char **argv)
+main(int argc, char *const argv[])
 {
-	printf("Hello, World!\n");
+	char *defaults[] = { "World", NULL };
+	argv = argc > 1 ? argv + 1 : defaults;
+	printf("Hello, %s!\n", argv[0]);
 	return 0;
 }
