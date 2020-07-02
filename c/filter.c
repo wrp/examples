d8f026505ee44b6d3bb513e8c9af36dc59f3a3a6
William Pursell <williamp@wepay.com>
2020-07-02 18:31:34 +0000

    Trivial refactor

--- c/filter.c
+++ c/filter.c
@@ -25,20 +25,20 @@ main(int argc, char **argv)
 	size_t len = strlen(argv[1]);
 	int c;
 	char *buf = xmalloc(len);
-	char *e = buf + len;
 	char *s = buf;
+	char *m = argv[1];
 	while( (c = getchar()) != EOF ) {
-		if( s == e ) {
-			if( ! strcmp(buf, argv[1])) {
+		if( (*s++ = c) == *m ) {
+			if( *++m == '\0' ) {
 				fputs(argv[2], stdout);
 				s = buf;
-			} else {
-				fputc(*buf, stdout);
-				memmove(buf, buf + 1, len - 1);
-				s -= 1;
+				m = argv[1];
 			}
+		} else {
+			fwrite(buf, 1, s - buf, stdout);
+			s = buf;
+			m = argv[1];
 		}
-		*s++ = c;
 	}
 	if( s != buf ) {
 		fwrite(buf, 1, s - buf, stdout);
