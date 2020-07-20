4f8f6c522b9fbb7594420675170b7259a9dd1d02
William Pursell <william.r.pursell@gmail.com>
2020-07-20 01:12:28 +0000

    Demonstrate passing va_list as an argument

--- c/va_args.c
+++ c/va_args.c
@@ -12,13 +12,13 @@ die(const char *fmt, ... )
 	va_end(ap);
 }
 
-
 int
 foo(const char *fmt, ...)
 {
 	int sum = 0;
 	int c, d;
-	short s;
+	char *s;
+	short h;
 	long l;
 
 	va_list ap;
@@ -26,9 +26,40 @@ foo(const char *fmt, ...)
 	while(*fmt) switch(*fmt++) {
 	case 'c': c = va_arg(ap, int); printf("%c,", c); sum += c; break;
 	case 'd': d = va_arg(ap, int); printf("%d,", d); sum += d; break;
-	case 's': s = va_arg(ap, int); printf("%hd,", s); sum += s; break;
+	case 'h': h = va_arg(ap, int); printf("%hd,", h); sum += h; break;
+	case 's': s = va_arg(ap, char *); printf("%s,", s); break;
 	case 'l': l = va_arg(ap, long); printf("%ld,", l); sum += l; break;
 	}
+	va_end(ap);
+	putchar('\n');
+	return sum;
+}
+
+int
+qux(const char *fmt, va_list ap)
+{
+	int c, d;
+	char *s;
+	short h;
+	long l;
+	int sum = 0;
+	while(*fmt) switch(*fmt++) {
+	case 'c': c = va_arg(ap, int); printf("%c,", c); sum += c; break;
+	case 'd': d = va_arg(ap, int); printf("%d,", d); sum += d; break;
+	case 'h': h = va_arg(ap, int); printf("%hd,", h); sum += h; break;
+	case 's': s = va_arg(ap, char *); printf("%s,", s); break;
+	case 'l': l = va_arg(ap, long); printf("%ld,", l); sum += l; break;
+	}
+	return sum;
+}
+
+int
+bar(const char *fmt, ...)
+{
+	int sum = 0;
+	va_list ap;
+	va_start(ap, fmt);
+	sum = qux(fmt, ap);
 	va_end(ap);
 	putchar('\n');
 	return sum;
@@ -39,8 +70,10 @@ int
 main(void)
 {
 	long k = (long)INT_MAX + 35;
-	foo("dcsl", 5, 'k', 2, k);
-	foo("ddss", 5, 'k', 2, k);
+	foo("sdchl", "foo", 5, 'k', 2, k);
+	bar("sdchl", "bar", 5, 'k', 2, k);
+	foo("sddhh", "foo", 5, 'k', 2, k);
+	bar("sddhh", "bar", 5, 'k', 2, k);
 	die("%d banana %s\n", 5, "foo");
 	return 0;
 }
