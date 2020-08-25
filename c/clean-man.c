39b116227996df790dff5228a5922f4cce8e3617
William Pursell <william.r.pursell@gmail.com>
2020-08-25 16:24:52 +0000

    Naive stripping of control sequences

--- c/clean-man.c
+++ c/clean-man.c
@@ -1,7 +1,9 @@
 
 
 /*
- * Discard octet before 0x08 to clean up man pages
+ * Clean up man pages.
+ * Discard octet before 0x08.
+ * Discard all between 0x1b and 'm'
  */
 #include <stdio.h>
 #include <assert.h>
@@ -12,11 +14,18 @@ main( void )
 	int c;
 
 	int prev = 0x08;
+	int state = 1;
 	assert( EOF != 0x08 );
 	do {
 		c = getchar();
-		if( c != 0x08 && prev != 0x08 )
+		if( prev == 0x1b ) {
+			state = 0;
+		}
+		if( state && c != 0x08 && prev != 0x08 )
 			putchar(prev);
+		if( prev == 'm' ) {
+			state = 1;
+		}
 		prev = c;
 	} while( c != EOF );
 }
