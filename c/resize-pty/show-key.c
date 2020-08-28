6eb59080ecf2244cb34d60f3b14fec7387744a84
William Pursell <william.r.pursell@gmail.com>
2020-08-28 09:48:36 +0000

    Initial wrap/show

---
+++ c/resize-pty/show-key.c
@@ -0,0 +1,53 @@
+
+#define _POSIX_C_SOURCE 200809L
+#define _XOPEN_SOURCE 600
+#define _XOPEN_SOURCE_EXTENDED
+#define _DARWIN_C_SOURCE
+
+#include <err.h>
+#include <curses.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int
+main(void)
+{
+	int r;
+	wint_t w = 0;
+	unsetenv("COLUMNS");
+	unsetenv("LINES");
+	if( initscr() == NULL ) {
+		err(1, "initscr");
+	}
+	while( (r = get_wch(&w)) != ERR && w != 'q') {
+                char c[MB_LEN_MAX];
+		char s[64];
+		char *d = NULL;
+		if( r == KEY_CODE_YES ) switch(w) {
+		case KEY_RESIZE: d = "KEY_RESIZE"; break;
+		case KEY_HOME: d = "KEY_HOME"; break;
+		case KEY_END: d = "KEY_END"; break;
+		case KEY_PPAGE: d = "KEY_PPAGE"; break;
+		case KEY_NPAGE: d = "KEY_NPAGE"; break;
+		case KEY_BACKSPACE: d = "KEY_BACKSPACE"; break;
+		case KEY_DC: d = "KEY_DC"; break;
+		case KEY_IC: d = "KEY_IC"; break;
+		case KEY_BTAB: d = "KEY_BTAB"; break;
+		case KEY_ENTER: d = "KEY_ENTER"; break;
+		case KEY_UP: d = "KEY_UP"; break;
+		case KEY_DOWN: d = "KEY_DOWN"; break;
+		case KEY_RIGHT: d = "KEY_RIGHT"; break;
+		case KEY_LEFT: d = "KEY_LEFT"; break;
+		}
+		if( d != NULL ) {
+			sprintf(s, "(%s)", d);
+		} else if( wctomb(c, w) == 1 && isprint(w) ) {
+			sprintf(s, "%c", toupper(c[0]));
+		}
+		printw("%s", s);
+		doupdate();
+	}
+	endwin();
+	return 0;
+}
---
+++ c/resize-pty/wrappty.c
@@ -0,0 +1,82 @@
+#include <ctype.h>
+#include <err.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#ifdef __linux__
+# include <pty.h>
+# include <utmp.h>
+#else
+# include <util.h>
+#endif
+
+static ssize_t
+send_msg(int fd, const char *msg)
+{
+	char buf[1024];
+	if( msg ) {
+		write(fd, msg, strlen(msg));
+	}
+	ssize_t s = read(fd, buf, sizeof buf);
+	const char *t = buf;
+	for( ssize_t i = 0; i < s; i++, t++ ) {
+		const char *fmt = isprint(*t) || isspace(*t) ? "%c" : "0x%02x";
+		fprintf(stderr, fmt, *t);
+	}
+	fflush(stderr);
+	return s;
+}
+
+int
+main(int argc, char **argv)
+{
+	int primary, secondary, c;
+	ssize_t s;
+	struct winsize ws = { .ws_row = 24, .ws_col = 80 };
+
+	(void) argc;
+	if( openpty(&primary, &secondary, NULL, NULL, &ws) ) {
+		err(EXIT_FAILURE, "openpty");
+	}
+	switch( fork() ) {
+	case -1:
+		err(1, "fork");
+		break;
+	case 0:
+		if( close(primary) ) {
+			err(EXIT_FAILURE, "close");
+		}
+		if( login_tty(secondary) ) {
+			err(EXIT_FAILURE, "login_tty");
+		}
+		execvp(argv[1], argv + 1);
+		err(EXIT_FAILURE, "execvp %s", argv[1]);
+	}
+	/* Parent */
+	if( close(secondary) ) {
+		err(EXIT_FAILURE, "close");
+	}
+
+	send_msg(primary, NULL);
+	while( (c = getchar()) != EOF ) {
+		ws.ws_row = 30 + c;
+		ws.ws_col = 80 + c;
+		char b[2] = {0};
+		b[0] = c;
+		if( ioctl(primary, TIOCSWINSZ, &ws) ) {
+			err(EXIT_FAILURE, "ioctl");
+		}
+		send_msg(primary, b);
+		send_msg(primary, "\e[A");
+	}
+	while( send_msg(primary, NULL) > 0 )
+		;
+	fputc('\n', stderr);
+
+	return 0;
+}
