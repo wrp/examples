aa7baa32a1cb8c441f4e9e4401594071eecc0937
William Pursell <william.r.pursell@gmail.com>
2020-09-19 11:51:03 +0000

    Add verbosity to snprintf output

--- c/snprintf.c
+++ c/snprintf.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
-/* demonstrate that snprintf writes n-1 bytes of data plus a null byte.
- * eg, the first argument will be a properly terminated string
+/* Demonstrate that snprintf writes n-1 bytes of data plus a null byte.
+ * That is, the first argument will be a properly terminated string.
  */
 
 int
 main(void)
 {
 	char v[5];
+	int r;
 
-	snprintf(v, sizeof v, "abcdefghijk" );
+	r = snprintf(v, sizeof v, "abcdefghijk" );
+	printf("sizeof v == %zd\n", sizeof v);
+	printf("snprintf(v, sizeof v, \"abcdefghijk\" ); returns %d\n", r);
 	for(unsigned i = 0; i < sizeof v; i++ ) {
 		printf("v[%d] = '%c' == %02x\n", i, v[i], v[i]);
 	}
