70813a7b6000664bd33128b800dfbb756fe1b87f
William Pursell <william.r.pursell@gmail.com>
2020-09-25 12:07:46 +0000

    Add text re: PRI printf format specifiers

--- c/printf.c
+++ c/printf.c
@@ -1,8 +1,11 @@
 
-/* Use _ISOC99_SOURCE to enable positional parameters for width specification (*nn$) */
+/* Use _ISOC99_SOURCE to enable positional parameters
+ * for width specification (*nn$)
+ */
 #define _ISOC99_SOURCE
-#include <stdio.h>
+#include <inttypes.h>
 #include <locale.h>
+#include <stdio.h>
 
 
 int
@@ -20,12 +23,15 @@ main(void)
 
 	printf("%'10d\n", 103957395);
 	printf("%'10f\n", 1039573.95);
-	printf("%'10g\n", 1039);
+	printf("%'10g\n", 1039.9E3);
 
 	printf("%*s: %%zd  %zd\n", 15, "size_t", sizeof i);
 	printf("%*s: %%td  %td\n", 15, "ptrdiff_t", q - p);
 
-	/* Use positional arguments.  Mixing positional with non-positional is undefined */
+	/* Use %p to printf pointer.  PRIxPTR is here just for demonstration. */
+	printf("p = %" PRIxPTR " = %p\n", (uintptr_t)p, p);
+
+	/* Use positional arguments.  Mixing positional with non is UB */
 	printf("%2$15s: %%zd  %1$zd\n", sizeof i, "size_t");
 	printf("%2$*3$s: %%zd  %1$zd\n", sizeof i, "size_t", 15);
 	/* %2$*3$s uses the 3rd arg as the width to print the 2nd arg as %s */
@@ -306,4 +312,111 @@ SECURITY CONSIDERATIONS
            snprintf(buffer, sizeof(buffer), "%s", string);
 
 BSD                            December 2, 2009                            BSD
+#endif
+
+#if 0
+regarding format specifiers for fixed width types
+(from https://pubs.opengroup.org/onlinepubs/009696799/basedefs/inttypes.h.html)
+
+The <inttypes.h> header shall include the <stdint.h> header.
+
+The <inttypes.h> header shall include a definition of at least the
+following type: 'imaxdiv_t'  Structure type that is the type of the
+value returned by the imaxdiv() function.
+
+The following macros shall be defined. Each expands to a character
+string literal containing a conversion specifier, possibly modified
+by a length modifier, suitable for use within the format argument
+of a formatted input/output function when converting the corresponding
+integer type. These macros have the general form of PRI (character
+string literals for the fprintf() and fwprintf() family of functions)
+or SCN (character string literals for the fscanf() and fwscanf()
+family of functions), followed by the conversion specifier, followed
+by a name corresponding to a similar type name in <stdint.h>. In
+these names, N represents the width of the type as described in
+<stdint.h>. For example, PRIdFAST32 can be used in a format string
+to print the value of an integer of type int_fast32_t.
+
+The fprintf() macros for signed integers are:
+
+PRIdN
+PRIdLEASTN
+PRIdFASTN
+PRIdMAX
+PRIdPTR
+PRIiN
+PRIiLEASTN
+PRIiFASTN
+PRIiMAX
+PRIiPTR
+
+The fprintf() macros for unsigned integers are:
+
+PRIoN
+PRIoLEASTN
+PRIoFASTN
+PRIoMAX
+PRIoPTR
+PRIuN
+PRIuLEASTN
+PRIuFASTN
+PRIuMAX
+PRIuPTR
+PRIxN
+PRIxLEASTN
+PRIxFASTN
+PRIxMAX
+PRIxPTR
+PRIXN
+PRIXLEASTN
+PRIXFASTN
+PRIXMAX
+PRIXPTR
+
+The fscanf() macros for signed integers are:
+
+SCNdN
+SCNdLEASTN
+SCNdFASTN
+SCNdMAX
+SCNdPTR
+SCNiN
+SCNiLEASTN
+SCNiFASTN
+SCNiMAX
+SCNiPTR
+
+The fscanf() macros for unsigned integers are:
+
+SCNoN
+SCNoLEASTN
+SCNoFASTN
+SCNoMAX
+SCNoPTR
+SCNuN
+SCNuLEASTN
+SCNuFASTN
+SCNuMAX
+SCNuPTR
+SCNxN
+SCNxLEASTN
+SCNxFASTN
+SCNxMAX
+SCNxPTR
+
+For each type that the implementation provides in <stdint.h>, the
+corresponding fprintf() and fwprintf() macros shall be defined and
+the corresponding fscanf() and fwscanf() macros shall be defined
+unless the implementation does not have a suitable modifier for the
+type.
+
+The following shall be declared as functions and may also be defined
+as macros. Function prototypes shall be provided.
+
+intmax_t  imaxabs(intmax_t);
+imaxdiv_t imaxdiv(intmax_t, intmax_t);
+intmax_t  strtoimax(const char *restrict, char **restrict, int);
+uintmax_t strtoumax(const char *restrict, char **restrict, int);
+intmax_t  wcstoimax(const wchar_t *restrict, wchar_t **restrict, int);
+uintmax_t wcstoumax(const wchar_t *restrict, wchar_t **restrict, int);
 #endif
