ad67da0b1d518688ed6409997417638c79c9fda5
William Pursell <william.r.pursell@gmail.com>
2020-10-07 15:05:37 +0000

    Trivial reformat

--- c/stat.c
+++ c/stat.c
@@ -8,23 +8,25 @@
 #include <stdlib.h>
 
 int
-main( int argc, char **argv )
+main(int argc, char **argv)
 {
 	struct stat sb;
 	struct passwd pwent;
 	struct passwd *pwentp;
-	char buf[ _SC_GETPW_R_SIZE_MAX ];
+	char buf[_SC_GETPW_R_SIZE_MAX];
 
-	char *filepath = argc > 1 ? argv[1] : "foo";
+	char *filepath = argc > 1 ? argv[1] : argv[0];
 
-	if( stat( filepath, &sb ) == -1 ) {
-		perror( "stat" );
+	if( stat(filepath, &sb) == -1 ) {
+		perror(filepath);
 		return EXIT_FAILURE;
 	}
 
-	if( !getpwuid_r( sb.st_uid, &pwent, buf, sizeof buf, &pwentp )) {
-		printf( "%s\n", pwent.pw_name );
+	printf("%s is owned by ", filepath);
+	if( !getpwuid_r(sb.st_uid, &pwent, buf, sizeof buf, &pwentp) ) {
+		printf("user %s\n", pwent.pw_name);
 	} else {
-		printf( "%d\n", sb.st_uid );
+		perror("getpwuid_r");
+		printf("uid %d\n", sb.st_uid);
 	}
 }
