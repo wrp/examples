b0af8bfa4bee952105068135b9cdd49c1c7196a6
William Pursell <william.r.pursell@gmail.com>
2020-10-15 03:47:25 +0000

    Display multiple base conversions

--- c/strtol.c
+++ c/strtol.c
@@ -3,24 +3,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-int
-main(int argc, char **argv)
+static void
+display(const char *arg, int base)
 {
 	char *end;
 	long n;
 	int i;
+	i = n = strtol( arg, &end, base);
+	printf("%s parses in base %d as %ld and %d", arg, base, n, i);
+	if(*end) {
+		printf(", ending at %c", *end);
+	}
+	putchar('\n');
+}
+
+int
+main(int argc, char **argv)
+{
 	(void) argc;
 
 	printf("longmax = %ld\n", LONG_MAX);
 	printf("ulongmax = %lu\n", ULONG_MAX);
 	for( argv += 1; *argv; argv += 1 ) {
-		i = n = strtol( *argv, &end, 10);
-		printf("%s parses as %ld and %d", *argv, n, i);
-		if(*end) {
-			printf(", ending at %c", *end);
-		}
-		putchar('\n');
+		display(*argv, 8);
+		display(*argv, 10);
+		display(*argv, 16);
 	}
 	return 0;
 }
