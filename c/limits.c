1868970ecba57a44a4efee76183867c2cd2335fb
William Pursell <williamp@wepay.com>
2020-10-29 18:49:37 +0000

    Tweak sizeof in limits

--- c/limits.c
+++ c/limits.c
@@ -5,10 +5,13 @@
 int
 main(void)
 {
+	long x = 5;
+	char buf[ sizeof(long) * CHAR_BIT ];
 	printf("SIZE_MAX  = %zu\n", SIZE_MAX);
 	printf("INT_MAX   = %d\n", INT_MAX);
 	printf("UINT_MAX  = %u\n", UINT_MAX);
 	printf("LONG_MAX  = %ld\n", LONG_MAX);
 	printf("ULONG_MAX = %lu\n", ULONG_MAX);
+	printf(" x == %ld is of size %zd\n", x, sizeof x);
 	return 0;
 }
