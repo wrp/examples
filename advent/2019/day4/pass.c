cd2be4a205854a5e5c41d4b5719761ae08a8a805
William Pursell <william.r.pursell@gmail.com>
2020-12-03 16:39:21 +0000

    Dissallow matching runs

--- advent/2019/day4/pass.c
+++ advent/2019/day4/pass.c
@@ -19,6 +19,20 @@ Other than the range rule, the following are true:
 123789 does not meet these criteria (no double).
 How many different passwords within the range given in your puzzle input meet these criteria?
 
+An Elf just remembered one more important detail: the two adjacent
+matching digits are not part of a larger group of matching digits.
+
+Given this additional criterion, but still ignoring the range rule,
+the following are now true:
+
+112233 meets these criteria because the digits never decrease and
+all repeated digits are exactly two digits long.  123444 no longer
+meets the criteria (the repeated 44 is part of a larger group of
+444).  111122 meets the criteria (even though 1 is repeated more
+than twice, it still contains a double 22).  How many different
+passwords within the range given in your puzzle input meet all of
+the criteria?
+
 input: 246540-787419
 */
 
@@ -38,7 +52,10 @@ valid(int i)
 		errx(1, "Invalid input\n");
 	}
 	for( int i = 0; i < c - 1; i++ ){
-		if( buf[i] == buf[i+1] ){
+		if( buf[i] == buf[i+1]
+			&& (i == 0 || buf[i] != buf[ i - 1])
+			&& (i == c-2 || buf[i] != buf[i+2])
+		){
 			pair = 1;
 		}
 		if( buf[i] > buf[i+1] ){
