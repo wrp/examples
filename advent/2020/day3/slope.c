a8cc0468857f80c9729ed2246b681b65a5210582
William Pursell <william.r.pursell@gmail.com>
2020-12-03 12:30:09 +0000

    Tweak to compute 2nd half

--- advent/2020/day3/slope.c
+++ advent/2020/day3/slope.c
@@ -1,5 +1,6 @@
 #include <err.h>
 #include <stdio.h>
+#include <stdlib.h>
 /*
 ..#...##...###.........#..#..#.
 #.###........#..##.#......#...#
@@ -7,21 +8,25 @@
 */
 
 int
-main(void)
+main(int argc, char **argv)
 {
+	int right = argc > 1 ? strtol(argv[1], NULL, 0) : 3;
+	int down = argc > 2 ? strtol(argv[2], NULL, 0) : 1;
 	char buf[33];
 	int x = 0;
-	int line = 1;
+	int line = 0;
 	int count = 0;
 	while( fgets(buf, sizeof buf, stdin) != NULL ){
+		if( line++ % down ){
+			continue;
+		}
 		if( buf[31] != '\n' ){
 			errx(1, "Unexpected input in line %d: %s", line, buf);
 		}
-		line += 1;
 		if( buf[x] == '#' ){
 			count += 1;
 		}
-		x = (x + 3) % 31;
+		x = (x + right) % 31;
 	}
 	printf("%d\n", count);
 	return 0;
