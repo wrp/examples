df95007b6e4120609c8bc976337f80124f4a000d
William Pursell <william.r.pursell@gmail.com>
2020-12-19 15:23:53 +0000

    Add part two

--- advent/2020/12/solve.c
+++ advent/2020/12/solve.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <err.h>
-/*
-F35
-L90
-S5
-F4
-R90
-Action N means to move north by the given value.
-Action S means to move south by the given value.
-Action E means to move east by the given value.
-Action W means to move west by the given value.
-Action L means to turn left the given number of degrees.
-Action R means to turn right the given number of degrees.
-Action F means to move forward by the given value in the direction the ship is currently facing.
-*/
 
 struct pos { int y; int x; };
 
 static void
-move(char dir, int count, struct pos *p)
+move(int count, struct pos *p, const struct pos *w)
+{
+	p->y += w->y * count;
+	p->x += w->x * count;
+}
+
+static void
+move_way(char dir, int count, struct pos *p)
 {
 	switch( dir ){
 	case 'N': p->y -= count; break;
@@ -28,18 +22,29 @@ move(char dir, int count, struct pos *p)
 	}
 }
 
+
 static void
-rotate(int d, int count, int *o)
+rotate(int count, struct pos *w)
 {
-	if( d == 1 ){
-		*o -= count;
-	} else {
-		*o += count;
+	int wy = w->y;
+	int wx = w->x;
+	switch( count ){
+	case 90:
+	case -270:
+		w->y =  wx;
+		w->x = -wy;
+		break;
+	case 180:
+	case -180:
+		w->y = -wy;
+		w->x = -wx;
+		break;
+	case -90:
+	case 270:
+		w->y = -wx;
+		w->x =  wy;
+		break;
 	}
-	while( *o < 0 ){
-		*o += 360;
-	}
-	*o %= 360;
 }
 
 int
@@ -47,28 +52,27 @@ main(void)
 {
 	int count;
 	char c;
-	int orientation = 90;
 	struct pos p = { 0, 0 };
+	struct pos w = { -1, 10 }; /* 10 east, 1 north */
 	while( scanf("%c%d ", &c, &count) == 2 ){
 		switch( c ){
 		case 'F':
-			switch( orientation ){
-			case  90: c = 'E'; break;
-			case 180: c = 'S'; break;
-			case 270: c = 'W'; break;
-			case   0: c = 'N'; break;
-			default: errx(1, "Bad orientation: %d", orientation);
-			}
+			move(count, &p, &w);
+			break;
 		case 'N':
 		case 'S':
 		case 'E':
 		case 'W':
-			move(c, count, &p);
+			move_way(c, count, &w);
 			break;
-		case 'L': rotate(1, count, &orientation); break;
-		case 'R': rotate(0, count, &orientation); break;
+		case 'L': rotate(-count, &w); break;
+		case 'R': rotate(count, &w); break;
 		}
+		#ifdef DEBUG
+		printf("%c: %d\tship@(%3d,%3d)\tway@(%3d,%3d)\n",
+			c, count, p.y, p.x, w.y, w.x);
+		#endif
 	}
-	printf("pos = %d, %d\n", p.x, p.y);
+	printf("pos = %d, %d  distance = %d\n", p.y, p.x, abs(p.y) + abs(p.x));
 	return 0;
 }
