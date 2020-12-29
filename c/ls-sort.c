ebc83468f9dbcd62f6882d21267da41768ff3663
William Pursell <william.r.pursell@gmail.com>
2020-12-29 18:40:41 +0000

    Refactor

--- c/ls-sort.c
+++ c/ls-sort.c
@@ -10,33 +10,42 @@ static void xpipe(int *fd) { if( pipe(fd) == -1 ){ err(1, "pipe"); } }
 static void xdup2(int a, int b) { if( dup2(a, b) == -1 ){ err(1, "dup2"); } }
 static void xclose(int fd) { if( close(fd) == -1 ){ err(1, "close"); } }
 
+static void
+execute(int *fd, char **cmd, int w)
+{
+	switch( fork() ){
+	case -1:
+		err(EXIT_FAILURE, "fork");
+	case 0:
+		xdup2(fd[w], w);
+		xclose(fd[0]);
+		xclose(fd[1]);
+		execvp(cmd[0], cmd);
+		perror("execvp");
+		exit(EXIT_FAILURE);
+	}
+}
+
 int
 main(void)
 {
+	int rv = EXIT_SUCCESS;
 	char *ls[] = { "ls", NULL };
 	char *sort[] = { "sort", "-r", NULL };
 	int fd[2];
 	xpipe(fd);
-	switch( fork() ){
-	case -1: perror("fork"); return EXIT_FAILURE;
-	case 0:
-		xdup2(fd[1], STDOUT_FILENO);
-		xclose(fd[0]);
-		xclose(fd[1]);
-		execvp(ls[0], ls);
-		perror("execvp");
-		return EXIT_FAILURE;
+	execute(fd, ls, 1);
+	execute(fd, sort, 0);
+	xclose(fd[0]);
+	xclose(fd[1]);
+	for( int i = 0; i < 2; i++ ){
+		int status;
+		wait(&status);
+		if( ! WIFEXITED(status) || WEXITSTATUS(status) ){
+			rv = EXIT_FAILURE;
+		}
 	}
-	switch( fork() ){
-	case -1: perror("fork"); return EXIT_FAILURE;
-	case 0:
-		xdup2(fd[0], STDIN_FILENO);
-		xclose(fd[0]);
-		xclose(fd[1]);
-		execvp(sort[0], sort);
-		perror("execvp");
-		return EXIT_FAILURE;
-	}
+	return rv;
 }
 
 
