f690c2b4df9f7ba502713502a68ec66e761c2e90
William Pursell <william.r.pursell@gmail.com>
2021-04-07 13:15:17 +0000

    Add precision specifier

--- rust/projects/rbe/src/print.rs
+++ rust/projects/rbe/src/print.rs
@@ -38,5 +38,7 @@ fn main() {
 	// handling. This will not work.
 	// println!("This struct `{}` won't print...", Structure(3));
 	// FIXME ^ Comment out this line.
+
+	println!("Pi is approximately {:.3}", 3.141592);
 }
 
