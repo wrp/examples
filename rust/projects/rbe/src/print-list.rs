aeffb44759cfb755a74e5544d70e7e436ed194f9
William Pursell <william.r.pursell@gmail.com>
2021-04-07 15:07:13 +0000

    Print indices

--- rust/projects/rbe/src/print-list.rs
+++ rust/projects/rbe/src/print-list.rs
@@ -16,8 +16,10 @@ impl fmt::Display for List {
 		for (count, v) in vec.iter().enumerate() {
 			// For every element except the first, add a comma.
 			// Use the ? operator to return on errors.
-			if count != 0 { write!(f, ", ")?; }
-			write!(f, "{}", v)?;
+			if count != 0 {
+				    write!(f, ", ")?;
+			}
+			write!(f, "{}: {}", count, v)?;
 		}
 
 		// Close the opened bracket and return a fmt::Result value.
