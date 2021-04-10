7b9ea52808dfb67b79b197ed7a322937d9877578
William Pursell <william.r.pursell@gmail.com>
2021-04-10 14:09:17 +0000

    Add macro sample

--- rust/projects/rbe/src/ternary.rs
+++ rust/projects/rbe/src/ternary.rs
@@ -7,6 +7,14 @@
  * cond ? val1 : val2, but see https://github.com/rust-lang/rust/issues/1698
  * There are some reasonable arguments there that could persuade me.
  */
+
+// From https://users.rust-lang.org/t/ternary-operator/40330/2
+macro_rules! either {
+	($test:expr => $true_expr:expr; $false_expr:expr) => {
+		if $test { $true_expr } else { $false_expr }
+	}
+}
+
 fn main() {
 
 	let mut r = 5;
@@ -15,4 +23,7 @@ fn main() {
 	println!("sign of {}: {}", r, (r >= 0).then(|| 1).unwrap_or(-1));
 
 	println!("sign of {}: {}", r, if r >= 0 { 1 } else { -1 } );
+
+	either!(true => println!("it is true"); println!("it is false"));
+	either!(false => println!("it is true"); println!("it is false"));
 }
