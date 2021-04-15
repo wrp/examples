78a82199b476efab7d08fc70ccabbedf87166bb4
William Pursell <william.r.pursell@gmail.com>
2021-04-15 13:32:47 +0000

    Add rust/projects/rbe/src/tryinto.rs

---
+++ rust/projects/rbe/src/tryinto.rs
@@ -0,0 +1,34 @@
+use std::convert::TryFrom;
+use std::convert::TryInto;
+
+#[derive(Debug, PartialEq)]
+struct EvenNumber(i32);
+
+impl TryFrom<i32> for EvenNumber {
+	type Error = ();
+
+	fn try_from(value: i32) -> Result<Self, Self::Error> {
+		if value % 2 == 0 {
+			Ok(EvenNumber(value))
+		} else {
+			Err(())
+		}
+	}
+}
+
+fn main() {
+	// TryFrom
+
+	assert_eq!(EvenNumber::try_from(8), Ok(EvenNumber(8)));
+	assert_eq!(EvenNumber::try_from(5), Err(()));
+
+	// TryInto
+
+	let result: Result<EvenNumber, ()> = 8i32.try_into();
+	println!("1: {:?}", result);
+	assert_eq!(result, Ok(EvenNumber(8)));
+	let result: Result<EvenNumber, ()> = 5i32.try_into();
+	println!("2: {:?}", result);
+	assert_eq!(result, Err(()));
+}
+
