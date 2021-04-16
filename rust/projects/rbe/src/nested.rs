09424ebd1c1ba612a2c04c37361f49bfda84a23d
William Pursell <william.r.pursell@gmail.com>
2021-04-16 14:43:27 +0000

    Add rust/projects/rbe/src/nested.rs

---
+++ rust/projects/rbe/src/nested.rs
@@ -0,0 +1,25 @@
+//https://doc.rust-lang.org/stable/rust-by-example/flow_control/loop/nested.html
+
+#![allow(unreachable_code)]
+#[allow(unused_labels)]
+
+fn main() {
+	'outer: loop {
+		println!("Entered the outer loop");
+
+		'inner: loop {
+			println!("Entered the inner loop");
+
+			// This would break only the inner loop
+			//break;
+
+			// This breaks the outer loop
+			break 'outer;
+		}
+
+		println!("This point will never be reached");
+	}
+
+	println!("Exited the outer loop");
+}
+
