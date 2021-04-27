85287d52a19dfb5fa937d76ed3eeae3a099c4d7b
William Pursell <william.r.pursell@gmail.com>
2021-04-27 15:33:31 +0000

    Add 2nd if-let example

--- rust/projects/rbe/src/if-let.rs
+++ rust/projects/rbe/src/if-let.rs
@@ -33,5 +33,41 @@ fn main() {
 		// The condition evaluated false. This branch is the default:
 		eprintln!("I don't like letters. Let's go with an emoticon :)!");
 	}
+	main2()
 }
 
+#[derive(Debug)]
+enum Foo {
+	Bar,
+	Baz,
+	Qux(u32)
+}
+
+fn main2() {
+	// Create example variables
+	let a = Foo::Bar;
+	let b = Foo::Baz;
+	let c = Foo::Qux(100);
+
+	// Variable a matches Foo::Bar
+	if let Foo::Bar = a {
+		println!("a is foobar");
+	}
+
+	// Variable b does not match Foo::Bar
+	// So this will print nothing
+	if let Foo::Bar = b {
+		println!("b is foobar");
+	}
+
+	// Variable c matches Foo::Qux which has a value
+	// Similar to Some() in the previous example
+	if let Foo::Qux(value) = c {
+		println!("c is {:?} value is {}", c, value);
+	}
+
+	// Binding also works with `if let`
+	if let Foo::Qux(value2 @ 100) = c {
+		println!("value2 is {}", value2);
+	}
+}
