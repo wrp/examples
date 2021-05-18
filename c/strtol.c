bd058b6b590fd972b2ac2ac397a208ba71c5b516
William Pursell <william.r.pursell@gmail.com>
2021-05-18 02:50:20 +0000

    Check for overflow in strtol.c

--- c/strtol.c
+++ c/strtol.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,10 +10,33 @@ display(const char *arg, int base)
 	char *end;
 	long n;
 	int i;
-	i = n = strtol( arg, &end, base);
-	printf("%s parses in base %d as %ld and %d", arg, base, n, i);
-	if(*end) {
+	errno = 0;
+	i = n = strtol(arg, &end, base);
+	int errno_save = errno;
+	/*
+
+     The strtol(), strtoll(), strtoimax(), and strtoq() functions
+     return the result of the conversion, unless the value would
+     underflow or overflow.  If no conversion could be performed,
+     0 is returned and the global variable errno is set to EINVAL
+     (the last feature is not portable across all platforms).  If
+     an overflow or underflow occurs, errno is set to ERANGE and
+     the function return value is clamped according to the following
+     table.
+
+           Function       underflow     overflow
+           strtol()       LONG_MIN      LONG_MAX
+           strtoll()      LLONG_MIN     LLONG_MAX
+           strtoimax()    INTMAX_MIN    INTMAX_MAX
+           strtoq()       LLONG_MIN     LLONG_MAX
+
+	*/
+	printf("%s in base %d as (long)%ld and (int)%d", arg, base, n, i);
+	if( *end ){
 		printf(", ending at %c", *end);
+	}
+	if( errno_save ){
+		printf("; err: %s", strerror(errno_save));
 	}
 	putchar('\n');
 }
@@ -24,7 +48,7 @@ main(int argc, char **argv)
 
 	printf("longmax = %ld\n", LONG_MAX);
 	printf("ulongmax = %lu\n", ULONG_MAX);
-	for( argv += 1; *argv; argv += 1 ) {
+	for( argv += 1; *argv; argv += 1 ){
 		display(*argv, 8);
 		display(*argv, 10);
 		display(*argv, 16);
