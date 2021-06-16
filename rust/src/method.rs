5ff080549fbc377358b25f3d502b6e1453daddad
William Pursell <williamp@wepay.com>
2021-06-16 18:46:15 +0000

    Add rust/src/method.rs

---
+++ rust/src/method.rs
@@ -0,0 +1,30 @@
+// Simple example of an implementation block defining a method
+// From the book: ch05-03-method-syntax.html
+
+use std::env;
+
+#[derive(Debug)]
+struct Rectangle {
+	width: u32,
+	height: u32,
+}
+
+impl Rectangle {
+	fn area(&self) -> u32 {
+		self.width * self.height
+	}
+}
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+	let width = args[1].parse::<u32>().unwrap();
+	let height = args[2].parse::<u32>().unwrap();
+
+	let rect1 = Rectangle {
+		width: width,
+		height: height,
+	};
+
+	println!("The area of the rectangle is {} square pixels.", rect1.area());
+}
+
