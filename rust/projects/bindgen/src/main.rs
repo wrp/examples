899af0767b247099cfcbc0cc3a4a2adbff47d80b
William Pursell <william.r.pursell@gmail.com>
2021-06-25 16:08:44 +0000

    Stop building a library
    Apparently, "cargo:rustc-link-lib=wrp-bindgen" will only cause
    the appropriate -l flags to be passed when compiling the
    builtin libary, and not the executable.  The flags are passed
    to the executable only if there is no library being built.

--- rust/projects/bindgen/src/main.rs
+++ rust/projects/bindgen/src/main.rs
@@ -1,3 +1,26 @@
+
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_sum_2() {
+		unsafe {
+			let result = sum_2_int(3, 8);
+			assert_eq!(result, 11);
+		}
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+	unsafe {
+		println!("sum of 3 and 5 is {}", sum_2_int(3,5));
+	}
 }
