7b741aaad5e633e3eeae3d620f6a701f4625e06c
William Pursell <william.r.pursell@gmail.com>
2021-06-30 20:23:41 +0000

    Use dict in format string

--- python/print.py
+++ python/print.py
@@ -8,6 +8,13 @@ h = 'hello'
 w = 'world'
 p=3.14159265
 print(f'{h}, {w}!')
-print(f'{h}, {w:20}!')
+print(f'{h}, {w:20}!')  # 20 wide
+print(f'{h!a}, {w!r}!')   # Apply ascii() and repr()
 print(f'pi =~ {p:6.2f}!')
-print('{}, {}!'.format(h, w))
+print('numbered: {1}, {0}!'.format(h, w))  # number the args
+print('named: {foo}, {bar}!'.format(foo=h, bar=w))  # named args
+
+a = {'foo': 45.12345, 'bar': 12.34567}
+b = {'baz': 45.12345, 'qux': 12.34567}
+print('{0[foo]:.2f} (45.12) {1[qux]:.2f} (12.35)'.format(a, b))
+
