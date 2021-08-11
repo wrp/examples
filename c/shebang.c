2a0534c704ed3f7d3a51b1396c347a28e4a30f72
William Pursell <william.r.pursell@gmail.com>
2021-08-11 15:17:34 +0000

    Add c/shebang.c

---
+++ c/shebang.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ When a file has a shebang:
+
+ #!path1 arg1 arg2
+
+and is invoked with "path2 arg3 arg4",
+the file at path1 will be executed with arguments:
+
+path1 arg1 arg2 path2 arg3 arg4
+
+ */
+int
+main(int argc, char **argv)
+{
+	printf("argc = %d\n", argc);
+	for( ; *argv; argv++ ){
+		printf("arg: %s\n", *argv);
+	}
+}
