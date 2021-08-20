41e3c4a809dfac715c3b6ba244734d31f3b98a7a
William Pursell <william.r.pursell@gmail.com>
2021-08-20 17:33:05 +0000

    Add cxx/cin.cc

---
+++ cxx/cin.cc
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/* If there is a conversion error reading, then subsequent
+ * attempts to read will fail until cin.clear() is called.
+ * eg: int x; cin >> x; will fail if the stream cannot be
+ * converted.
+ *
+ * How this is considered better than scanf is beyond me
+ */
+int main()
+{
+	int i;
+	char c;
+	if( cin.good() ){
+		cin >> i >> c;
+		cout << "i is " << i << endl;
+		cout << "c is " << c << endl;
+	}
+	cout << "cin.bad " << cin.bad() << endl;
+	cout << "cin.good" << cin.good() << endl;
+	cout << "cin.eof " << cin.eof() << endl;
+	cout << "cin.fail " << cin.eof() << endl;
+	if( cin.rdstate() ){
+		cout << "cin in state: " << cin.rdstate() << endl;
+		cin.clear();
+	}
+	cin >> i >> c;
+	cout << "i is " << i << endl;
+	cout << "c is " << c << endl;
+}
