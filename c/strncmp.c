525add1d9514f630ba2c523c3223b25e7a7fabf5
William Pursell <william.r.pursell@gmail.com>
2021-09-15 13:45:36 +0000

    Whitespace

--- c/strncmp.c
+++ c/strncmp.c
@@ -9,7 +9,7 @@ main(int argc, char **argv)
 {
 	const char *cmp;
 	size_t n;
-	if(argc < 3) {
+	if( argc < 3 ){
 		printf("usage: %s N cmp [string...]\n", basename(argv[0]));
 		puts("\nShow the result of strncmp(cmp, string, N) on each of the strings");
 		return 0;
@@ -17,7 +17,7 @@ main(int argc, char **argv)
 	n = strtol(argv[1], NULL, 10);
 	cmp = argv[2];
 	argv += 2;
-	for(; *argv; argv += 1) {
+	for( ; *argv; argv += 1 ){
 		printf("strncmp(\"%s\", \"%s\", %zu) = %d\n",
 			cmp, *argv, n, strncmp(cmp, *argv, n));
 	}
