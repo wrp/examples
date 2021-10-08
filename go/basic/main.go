f7ac82358006416b50b7b3fefe52806834dacf4e
William Pursell <william.r.pursell@gmail.com>
2021-10-08 19:27:42 +0000

    Play with formatting

--- go/basic/main.go
+++ go/basic/main.go
@@ -10,9 +10,7 @@ func main() {
 }
 
 func maps() {
-	type Vertex struct {
-		Lat, Long float64
-	}
+	type Vertex struct { Lat, Long float64 }
 	var m = map[string]Vertex{
 		"Bell Labs": Vertex{ 40.68433, -74.39967, },
 		"Google": Vertex{ 37.42202, -122.08408, },
