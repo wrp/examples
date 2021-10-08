6cd7fc3384d22d04c31a2a0df94c5486bc9f0100
William Pursell <william.r.pursell@gmail.com>
2021-10-08 21:58:11 +0000

    Explode a slice

--- go/basic/variadic.go
+++ go/basic/variadic.go
@@ -13,4 +13,9 @@ func sum(floats ...float32) (ret float32) {
 
 func variadic() {
 	fmt.Println("sum is", sum(5.2, 2.9, -4e-5))
+
+	/* Explode the slice to call the variadic function */
+	a := []float32{2.3, 1.0, 5}
+	s := sum(a...)
+	fmt.Println("sum is", s)
 }
