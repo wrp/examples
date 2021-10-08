85f03fb5779ed1bc04091aa27854810e2f4b556b
William Pursell <william.r.pursell@gmail.com>
2021-10-08 17:53:14 +0000

    Demonstrate [...] in array initialization

--- go/basic/main.go
+++ go/basic/main.go
@@ -15,6 +15,7 @@ func slices() {
 
 	_, _, _ = b, c, d
 
+	fmt.Println("Slices");
 	fmt.Printf("b=%v\n", b);
 	fmt.Printf("b[0:]=%v\n", b[0:]);
 	fmt.Printf("b[len(b)-2:]=%v\n", b[len(b)-2:]);
@@ -23,10 +24,14 @@ func slices() {
 }
 
 func arrays() {
-	var a [5]int           /* An array of 5 integers, initialized to 0 */
-	b := [5]int{1, 2, 3,}  /* An initialized array, unset values go to 0 */
+	var a [5]int             /* An array of 5 integers, initialized to 0 */
+	b := [5]int{1, 2, 3,}    /* An initialized array, unset values go to 0 */
+	e := [...]int{1, 2, 3,}  /* An initialized array */
 	var c [5]int = [5]int{6, 7, 8, 9, 10} /* An initialized array */
 	var d [2][3] int = [2][3]int{{1,2,3}, {4,5,6}}
-
 	_, _, _, _ = a, b, c, d
+
+	fmt.Println("Arrays");
+	fmt.Printf("%v: lcn=%d cap=%d\n", b, len(b), cap(b))
+	fmt.Printf("%v: lcn=%d cap=%d\n", e, len(e), cap(e))
 }
