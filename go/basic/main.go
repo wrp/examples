23806514d5bb4dc3a364869fd7a3362ba5ca027f
William Pursell <william.r.pursell@gmail.com>
2021-10-08 17:44:54 +0000

    Add slice example

--- go/basic/main.go
+++ go/basic/main.go
@@ -4,13 +4,26 @@ package main
 import "fmt"
 
 func main() {
+	slices()
+	arrays()
+}
+
+func slices() {
+	b := []int{1, 2, 3,}  /* An initialized slice */
+	var c []int = []int{6, 7, 8, 9, 10} /* An initialized array */
+	var d [][]int = [][]int{{1,2,3}, {4,5,6}}
+
+	_, _, _ = b, c, d
+
+	fmt.Printf("len=%d cap=%d %v\n", len(c), cap(c), c)
+	fmt.Printf("len=%d cap=%d %v\n", len(d), cap(d), d) /* len == cap == 2 */
+}
+
+func arrays() {
 	var a [5]int           /* An array of 5 integers, initialized to 0 */
 	b := [5]int{1, 2, 3,}  /* An initialized array, unset values go to 0 */
 	var c [5]int = [5]int{6, 7, 8, 9, 10} /* An initialized array */
 	var d [2][3] int = [2][3]int{{1,2,3}, {4,5,6}}
 
-	_, _, _ = a, b, c
-
-	fmt.Println("d:", d)
-
+	_, _, _, _ = a, b, c, d
 }
