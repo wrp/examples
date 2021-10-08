226952a3c31165366c0f1a5fa1408887715a810d
William Pursell <william.r.pursell@gmail.com>
2021-10-08 16:13:47 +0000

    Play around with formatting

--- go/switch/main.go
+++ go/switch/main.go
@@ -11,19 +11,17 @@ func main() {
 	i := 2
 	fmt.Print("Write ", i, " as ")
 	switch i {
-	case 1:
-		fmt.Println("one")
-	case 2:
-		fmt.Println("two")
-	case 3:
-		fmt.Println("three")
+	case 1: fmt.Println("one")
+	case 2: fmt.Println("two")
+	case 3: fmt.Println("three")
 	}
 
 	switch time.Now().Weekday() {
-	case time.Saturday, time.Sunday:
-		fmt.Println("It's the weekend")
-	default:
-		fmt.Println("It's a weekday")
+	case
+		time.Saturday,
+		time.Sunday:
+			fmt.Println("It's the weekend")
+	default: fmt.Println("It's a weekday")
 	}
 
 	t := time.Now()
@@ -36,12 +34,9 @@ func main() {
 
 	whatAmI := func(i interface{}) {
 		switch t := i.(type) {
-		case bool:
-			fmt.Println("I'm a bool")
-		case int:
-			fmt.Println("I'm an int")
-		default:
-			fmt.Printf("Don't know type %T\n", t)
+		case bool: fmt.Println("I'm a bool")
+		case int: fmt.Println("I'm an int")
+		default: fmt.Printf("Don't know type %T\n", t)
 		}
 	}
 	whatAmI(true)
