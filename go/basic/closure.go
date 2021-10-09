ef8ccf0ada049760417a3b6835a9f77289e4ed9e
William Pursell <william.r.pursell@gmail.com>
2021-10-09 06:59:36 -0600

    Demonstrate recursion with a closure

--- go/basic/closure.go
+++ go/basic/closure.go
@@ -22,4 +22,38 @@ func closure(){
 
 	newInts := intSeq()
 	fmt.Printf("%d\n", newInts())
+
+	fmt.Println(fact(3))
+
+	// Closures can also be recursive, but this requires the
+	// closure to be declared with a typed `var` explicitly
+	// before it's defined.
+	var fib func(n int) int
+
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
+		return fib(n-1) + fib(n-2)
+
+		// Since `fib` was previously declared in `main`, Go
+		// knows which function to call with `fib` here.
+	}
+
+	fmt.Println(fib(3))
+}
+
+
+// Go supports
+// <a href="http://en.wikipedia.org/wiki/Recursion_(computer_science)"><em>recursive functions</em></a>.
+// Here's a classic example.
+
+
+// This `fact` function calls itself until it reaches the
+// base case of `fact(0)`.
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
 }
