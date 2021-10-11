a9082c0dfb5e370948f63a8992a996737a1b3fc3
William Pursell <william.r.pursell@gmail.com>
2021-10-11 13:44:36 +0000

    Delete some newlines
    gofmt continues to exhibit stupid behavior.  There is no
    need to insert these blank lines, and it is ridiculous
    ...just leave it at that.  The whole concept is flawed.
    Rather than reducing any flame wars about style, the language
    designers are just imposing their own flawed stylistic
    judgments on everyone else and effectively inviting criticism.
    Let me put whitespace around operators, and omit blank lines
    where they are fugly.  A static style guide cannot be as
    effective as human intuition, and the language's attempt
    to do so is extreme hubris.

--- go/basic/interface.go
+++ go/basic/interface.go
@@ -1,10 +1,10 @@
-
 package main
 
 import (
 	"fmt"
 	"math"
 )
+
 type geo3d interface {
 	geo2d
 	volume() float64
@@ -15,44 +15,21 @@ type geo2d interface {
 	perim() float64
 }
 
+type rect struct{ width, height float64 }
 
-type rect struct {
-	width, height float64
-}
-func (r rect) area() float64 {
-	return r.width * r.height
-}
-func (r rect) perim() float64 {
-	return 2*r.width + 2*r.height
-}
+func (r rect) area() float64  { return r.width * r.height }
+func (r rect) perim() float64 { return 2*r.width + 2*r.height }
 
+type circle struct{ radius float64 }
 
-type circle struct {
-	radius float64
-}
+func (c circle) area() float64  { return math.Pi * c.radius * c.radius }
+func (c circle) perim() float64 { return 2 * math.Pi * c.radius }
 
-func (c circle) area() float64 {
-	return math.Pi * c.radius * c.radius
-}
-func (c circle) perim() float64 {
-	return 2 * math.Pi * c.radius
-}
+type cube struct{ side float64 }
 
-
-
-type cube struct {
-	side float64
-}
-func (c cube) area() float64 {
-	return c.side * c.side * 6
-}
-func (c cube) perim() float64 {
-	return c.side * 12
-}
-func (c cube) volume() float64 {
-	return c.side * c.side * c.side
-}
-
+func (c cube) area() float64   { return c.side * c.side * 6 }
+func (c cube) perim() float64  { return c.side * 12 }
+func (c cube) volume() float64 { return c.side * c.side * c.side }
 
 func measure(g geo2d) {
 	fmt.Println(g)
@@ -67,7 +44,7 @@ func interface_examples() {
 	var w geo2d
 	var y geo3d
 	w = z
-	y = w.(geo3d)  // type assertion
+	y = w.(geo3d) // type assertion
 
 	measure(r)
 	measure(c)
