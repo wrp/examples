7c00f5d094cbd019ac52a428d7f9afb515cc9d85
William Pursell <william.r.pursell@gmail.com>
2021-10-12 22:05:32 +0000

    Demonstrate equivalence of new(t) and &t{}

--- go/basic/struct.go
+++ go/basic/struct.go
@@ -5,19 +5,33 @@ import (
 	"reflect"
 )
 
+type T struct {
+	A int
+	B string
+	C float64
+}
+func (t T) String() string {
+	return fmt.Sprintf("A=%v, B=%v, C=%v", t.A, t.B, t.C)
+}
+
 func structs() {
-	type T struct {
-		A int
-		B string
-		C float64
-	}
 	t := T{23, "skidoo", 3.5}
 	s := reflect.ValueOf(&t).Elem()
 	typeOfT := s.Type()
+
 	// Iterate over the members of a struct, printing their names/values
 	for i := 0; i < s.NumField(); i++ {
 		f := s.Field(i)
 		fmt.Printf("%d: %s %s = %v\n", i,
 			typeOfT.Field(i).Name, f.Type(), f.Interface())
 	}
+
+	// new(T) is exactly the same as &T{}
+	v := new(T)
+	w := &T{}
+	x := &T{A:5, B:"foo", C: 3}
+
+	fmt.Println(v)
+	fmt.Println(w)
+	fmt.Println(x)
 }
