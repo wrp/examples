7d8e7e7fb997f453faa00f6c7988a8492d206721
William Pursell <william.r.pursell@gmail.com>
2021-10-12 19:31:45 +0000

    Add go/basic/struct.go

---
+++ go/basic/struct.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func structs() {
+	type T struct {
+		A int
+		B string
+		C float64
+	}
+	t := T{23, "skidoo", 3.5}
+	s := reflect.ValueOf(&t).Elem()
+	typeOfT := s.Type()
+	for i := 0; i < s.NumField(); i++ {
+		f := s.Field(i)
+		fmt.Printf("%d: %s %s = %v\n", i,
+			typeOfT.Field(i).Name, f.Type(), f.Interface())
+	}
+}
