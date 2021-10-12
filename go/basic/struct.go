d350e35ea28493d5633d51f532d02d62b47b138e
William Pursell <william.r.pursell@gmail.com>
2021-10-12 19:33:21 +0000

    Documentation

--- go/basic/struct.go
+++ go/basic/struct.go
@@ -14,6 +14,7 @@ func structs() {
 	t := T{23, "skidoo", 3.5}
 	s := reflect.ValueOf(&t).Elem()
 	typeOfT := s.Type()
+	// Iterate over the members of a struct, printing their names/values
 	for i := 0; i < s.NumField(); i++ {
 		f := s.Field(i)
 		fmt.Printf("%d: %s %s = %v\n", i,
