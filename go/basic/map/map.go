302bb2ca20e94bff2ae5d3f60e0d4b4b8d28ecbf
William Pursell <william.r.pursell@gmail.com>
2021-10-13 20:35:55 +0000

    Do not export Vertex

--- go/basic/map/map.go
+++ go/basic/map/map.go
@@ -18,7 +18,7 @@ import (
 	"fmt"
 )
 
-type Vertex struct{ Lat, Long float64 }
+type vertex struct{ Lat, Long float64 }
 
 func map_incr(x map[int]int) {
 	for k := range x {
@@ -27,9 +27,9 @@ func map_incr(x map[int]int) {
 }
 
 func Example() {
-	var m = map[string]Vertex{
-		"Bell Labs": Vertex{40.68433, -74.39967},
-		"Google":    Vertex{37.42202, -122.08408},
+	var m = map[string]vertex{
+		"Bell Labs": vertex{40.68433, -74.39967},
+		"Google":    vertex{37.42202, -122.08408},
 	}
 	fmt.Println(m["Google"])
 	k := map[int]int{
