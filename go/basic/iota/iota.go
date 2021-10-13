54ef3c2257cb44c6569037d71c43a914419153b9
William Pursell <william.r.pursell@gmail.com>
2021-10-13 21:47:34 +0000

    Add go/basic/iota/iota.go

---
+++ go/basic/iota/iota.go
@@ -0,0 +1,21 @@
+
+package iota
+import "fmt"
+
+type ByteSize float64
+
+const (
+    _           = iota // ignore first value by assigning to blank identifier
+    KiB ByteSize = 1 << (10 * iota)
+    MiB
+    GiB
+    TiB
+    PiB
+    EiB
+    ZiB
+    YiB
+)
+
+func Example() {
+	fmt.Println("iota: ", KiB, MiB, GiB, TiB, PiB)
+}
