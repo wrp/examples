f2bbc0f028c0dc008eca794c7ade6b52fa947866
William Pursell <william.r.pursell@gmail.com>
2021-12-21 17:08:58 -0700

    s/rary// and add tab

--- rust/src/misc.rs
+++ rust/src/misc.rs
@@ -4,11 +4,11 @@ pub fn public_function() {
 }
 
 fn private_function() {
-    println!("called miscrary's `private_function()`");
+    println!("called misc's `private_function()`");
 }
 
 pub fn indirect_access() {
-    print!("called miscrary's `indirect_access()`, that\n> ");
+    print!("called misc's `indirect_access()`, that\n\t> ");
 
     private_function();
 }
