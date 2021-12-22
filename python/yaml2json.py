2a111fdd8bc76edd132d0c9cabb104da485c8064
William Pursell <william.r.pursell@gmail.com>
2021-12-21 17:00:03 -0700

    Read yaml from /dev/stdin by default
    All the yaml examples are now in a different directory, and having this
    embedded example makes no sense.

--- python/yaml2json.py
+++ python/yaml2json.py
@@ -20,25 +20,8 @@ def main(args):
                 raise e
 
 
-data = r'''
-foo:
-    bar: value
-    scalar: >
-        Scalar content can be written in block notation, using a '>'
-    nospace: "this string has\
-        no space between 'has' and 'no'"
-view:
-    query: >
-        SELECT  * EXCEPT (foo)
-    "query2": >
-        "SELECT  * EXCEPT (foo)
-    "query3":
-        "SELECT  * EXCEPT (foo)
-    useLegacySql: false
-'''
-
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         main(sys.argv[1:])
     else:
-        main([data])
+        main(['/dev/stdin/'])
