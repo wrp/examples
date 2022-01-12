f13917da91c72661a07a2ade8e87c29bde458a89
William Pursell <william.r.pursell@gmail.com>
2022-01-12 09:33:10 -0700

    Add usage statement and iteration over files

--- c/clean-man.c
+++ c/clean-man.c
@@ -1,31 +1,48 @@
-
-
 /*
  * Clean up man pages.
  * Discard octet before 0x08.
  * Discard all between 0x1b and 'm'
  */
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include "xutil.h"
 
 int
-main( void )
+main( int argc, char **argv )
 {
 	int c;
+	char *defaults[] = { "-", NULL };
 
-	int prev = 0x08;
-	int state = 1;
-	assert( EOF != 0x08 );
-	do {
-		c = getchar();
-		if( prev == 0x1b ) {
-			state = 0;
-		}
-		if( state && c != 0x08 && prev != 0x08 )
-			putchar(prev);
-		if( prev == 'm' ) {
-			state = 1;
-		}
-		prev = c;
-	} while( c != EOF );
+	if( argc > 1 && strcmp(argv[1], "-h") == 0 ){
+		char *base = strrchr(argv[0], '/');
+		printf("usage: %s [file ... ]", base ? base + 1 : argv[0]);
+		puts("\n\nRemove all bytes preceding 0x08 and all between");
+		puts("0x1b and the next 'm'.  Useful for naive clean up of");
+		puts("manpages.");
+		return 0;
+	}
+	char **args = argc > 1 ? ++argv : defaults;
+
+	for( char **arg = args; *arg; arg++ ){
+		FILE *ifp = xfopen(*arg, "r");
+
+		int prev = 0x08;
+		int state = 1;
+		assert( EOF != 0x08 );
+		do {
+			c = getc(ifp);
+			if( prev == 0x1b ) {
+				state = 0;
+			}
+			if( state && c != 0x08 && prev != 0x08 ){
+				putc(prev, stdout);
+			}
+			if( prev == 'm' ){
+				state = 1;
+			}
+			prev = c;
+		} while( c != EOF );
+		fclose(ifp);
+	}
 }
