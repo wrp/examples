b36ab3d27c773b85b471df31852de67fa3b9bfec
William Pursell <=william.r.pursell@gmail.com>
2022-01-18 16:21:23 -0700

    Use multiple generic types in struct

--- rust/src/generic.rs
+++ rust/src/generic.rs
@@ -4,10 +4,17 @@ struct Point<T> {
 	y: T,
 }
 
+struct Point2<T1, T2> {
+	x: T1,
+	y: T2,
+}
+
 fn main() {
 	let i = Point { x: 5, y: 10 };
 	let f = Point { x: 1.0, y: 4.0 };
+	let z = Point2 { x: 1.0, y: 4 };
 
-	println!("integer = ({},{})", i.x, i.y);
-	println!("float = ({0:.3},{1:.3})", f.x, f.y);
+	println!("({},{})", i.x, i.y);
+	println!("({0:.3},{1:.3})", f.x, f.y);
+	println!("({},{})", z.x, z.y);
 }
