92deb1d4083fe19096a0d66d7f4c3c2568d622ea
William Pursell <=william.r.pursell@gmail.com>
2022-01-18 20:05:33 -0700

    Implement display for Point<i32>

--- rust/src/generic.rs
+++ rust/src/generic.rs
@@ -1,4 +1,6 @@
 
+use std::fmt::{self, Formatter, Display};
+
 struct Point<T> {
 	x: T,
 	y: T,
@@ -9,12 +11,22 @@ struct Point2<T1, T2> {
 	y: T2,
 }
 
+impl Display for Point<i32> {
+        // `f` is a buffer, and this method must write the formatted string into it
+        fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+                // `write!` is like `format!`, but it will write the formatted string
+                // into a buffer (the first argument)
+                write!(f, "({},{})", self.x, self.y)
+        }
+}
+
+
 fn main() {
 	let i = Point { x: 5, y: 10 };
 	let f = Point { x: 1.0, y: 4.0 };
 	let z = Point2 { x: 1.0, y: 4 };
 
-	println!("({},{})", i.x, i.y);
+	println!("{}", i);
 	println!("({0:.3},{1:.3})", f.x, f.y);
 	println!("({},{})", z.x, z.y);
 }
