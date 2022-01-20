97c6e0333f6623f18b3f75f7295935ddb6b57fda
William Pursell <=william.r.pursell@gmail.com>
2022-01-20 16:00:02 -0700

    Use a generic to implement trait for Point<float>

--- rust/src/generic.rs
+++ rust/src/generic.rs
@@ -1,22 +1,22 @@
 
 use std::fmt::{self, Formatter, Display};
 
-struct Point<T> {
+struct Point<T: Display> {
 	x: T,
 	y: T,
 }
 
-struct Point2<T1, T2> {
+struct Point2<T1: Display, T2: Display> {
 	x: T1,
 	y: T2,
 }
 
-impl Display for Point<i32> {
+impl <T: Display> Display for Point <T> {
         // `f` is a buffer, and this method must write the formatted string into it
         fn fmt(&self, f: &mut Formatter) -> fmt::Result {
                 // `write!` is like `format!`, but it will write the formatted string
                 // into a buffer (the first argument)
-                write!(f, "({},{})", self.x, self.y)
+                write!(f, "({0:.3},{1:.3})", self.x, self.y)
         }
 }
 
@@ -27,6 +27,6 @@ fn main() {
 	let z = Point2 { x: 1.0, y: 4 };
 
 	println!("{}", i);
-	println!("({0:.3},{1:.3})", f.x, f.y);
+	println!("{}", f);
 	println!("({},{})", z.x, z.y);
 }
