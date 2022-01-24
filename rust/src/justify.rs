03c3d246dc4c65271a47446fe9598e3b96135914
William Pursell <william.r.pursell@gmail.com>
2022-01-24 11:04:17 -0700

    Use String::from

--- rust/src/justify.rs
+++ rust/src/justify.rs
@@ -8,13 +8,7 @@ use std::{
 /* Read text and justify on the given width */
 fn main() {
 	let args: Vec<String> = env::args().collect();
-//	let word = &*args.get(4).unwrap_or(&"80".to_string());
-
-//	let width = match &args.get(1) {
-//		Some(&s) => s.parse::<i32>().unwrap(),
-//		None => 80,
-//	};
-	let width = args.get(1).unwrap_or(&"80".to_string()).parse::<i32>().unwrap();
+	let width = args.get(1).unwrap_or(&String::from("80")).parse::<i32>().unwrap();
 
 	println!("w = {}", width);
 
