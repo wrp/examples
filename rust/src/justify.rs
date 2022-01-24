a9461f90faccd78a20b23133df907956a8c9328c
William Pursell <william.r.pursell@gmail.com>
2022-01-24 10:05:13 -0700

    Read command line arg as default int

--- rust/src/justify.rs
+++ rust/src/justify.rs
@@ -8,10 +8,13 @@ use std::{
 /* Read text and justify on the given width */
 fn main() {
 	let args: Vec<String> = env::args().collect();
-	let width = match args.get(1) {
-		Some(&s) => s.parse::<i32>().unwrap(),
-		None => 80,
-	};
+//	let word = &*args.get(4).unwrap_or(&"80".to_string());
+
+//	let width = match &args.get(1) {
+//		Some(&s) => s.parse::<i32>().unwrap(),
+//		None => 80,
+//	};
+	let width = args.get(1).unwrap_or(&"80".to_string()).parse::<i32>().unwrap();
 
 	println!("w = {}", width);
 
