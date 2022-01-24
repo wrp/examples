77406d46d5421d5bce84dc399d362141eb21d5be
William Pursell <william.r.pursell@gmail.com>
2022-01-24 09:40:29 -0700

    Invalid borrow in match

--- rust/src/justify.rs
+++ rust/src/justify.rs
@@ -8,7 +8,10 @@ use std::{
 /* Read text and justify on the given width */
 fn main() {
 	let args: Vec<String> = env::args().collect();
-	let width = args[1].parse::<i32>().unwrap();
+	let width = match args.get(1) {
+		Some(&s) => s.parse::<i32>().unwrap(),
+		None => 80,
+	};
 
 	println!("w = {}", width);
 
