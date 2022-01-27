d72d4bab0c3f722d7cec25a46617e4ef290e7cc2
William Pursell <william.r.pursell@gmail.com>
2022-01-27 13:43:22 -0700

    Add a few more types

--- rust/src/sizeof.rs
+++ rust/src/sizeof.rs
@@ -1,12 +1,14 @@
 
 use std::{io,mem};
 
-fn print_type_of<T>(x: &T) {
+fn print_type_size<T>(x: &T) {
 	println!("size of {} is {}", std::any::type_name::<T>(), mem::size_of_val(x));
 }
 
 fn main() -> io::Result<()> {
 	let i = 32;
-	print_type_of(&i);
+	print_type_size(&i);
+	print_type_size(&2);
+	print_type_size(&32_u64);
 	Ok(())
 }
