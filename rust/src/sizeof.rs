ceac3f53ef594ec57d62e33170d3608148ed3d6d
William Pursell <william.r.pursell@gmail.com>
2022-01-27 13:13:55 -0700

    Add type name to ouput

--- rust/src/sizeof.rs
+++ rust/src/sizeof.rs
@@ -1,11 +1,12 @@
 
-
-
-
 use std::{io,mem};
+
+fn print_type_of<T>(x: &T) {
+	println!("size of {} is {}", std::any::type_name::<T>(), mem::size_of_val(x));
+}
 
 fn main() -> io::Result<()> {
 	let i = 32;
-	println!("size of i is {}", mem::size_of_val(&i));
+	print_type_of(&i);
 	Ok(())
 }
