001a1bdd8727db93d50365f04231181e029679e9
William Pursell <william.r.pursell@gmail.com>
2022-02-17 10:31:06 -0700

    Return a value with break

--- rust/src/labeled-loop.rs
+++ rust/src/labeled-loop.rs
@@ -1,21 +1,21 @@
 fn main() {
 	let mut count = 0;
-	'foo: loop {
+	let a = 'foo: loop {
 		println!("count = {}", count);
 		let mut remaining = 10;
 		loop {
 			println!("remaining = {}", remaining);
 				if remaining == 9 {
-					break;  // Branch to A
+					break 5;  // Branch to A
 			}
 			if count == 2 {
-				break 'foo;  // Branch to B
+				break 'foo count;  // Branch to B, bind count to a
 			}
 			remaining -= 1;
-		}
+		};
 		// A
 		count += 1;
-	}
+	};
 	// B
-	println!("End count = {}", count);
+	println!("End count = {}, a = {a}", count);
 }
