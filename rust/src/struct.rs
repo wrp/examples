55c8c18fe133e136d796cfd74f916ca827becfaa
William Pursell <william.r.pursell@gmail.com>
2022-02-21 10:29:20 -0700

    Demonstrate field init shorthand for struct

--- rust/src/struct.rs
+++ rust/src/struct.rs
@@ -7,9 +7,15 @@ struct Rectangle {
     height: u32,
 }
 
+// Use "Field Init Shorthand
+fn demo_field_init( height: u32, width: u32, ) -> Rectangle { Rectangle { height, width } }
+
+
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = demo_field_init(5, 10);
 
     println!("rect1 is {:?}", rect1);
-    println!("rect1 is {:#?}", rect1);
+    println!("rect1 is {:?}", rect2);
+    println!("height is {:#?}", rect1.height);
 }
