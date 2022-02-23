74c799a7dfc36e909987f9ee71d6739cc9ccc729
William Pursell <william.r.pursell@gmail.com>
2022-02-23 12:27:46 -0700

    Use unwrap_or_else to provide good open error

--- rust/src/open.rs
+++ rust/src/open.rs
@@ -1,25 +1,12 @@
-// Todo: look at std::fs::OpenOptions;
 
-use std::{env,fs::File};
-use anyhow::{Context, Result};
+use std::{env, fs::File, process};
 
-fn count_words<R: Read>(input: &mut R) -> Result<u32, Box<dyn Error>> {
-	let reader = BufReader::new(input);
-	let mut wordcount = 0;
-	for line in reader.lines() {
-		for _word in line?.split_whitespace() {
-			wordcount += 1;
-		}
-	}
-	Ok(wordcount)
-}
-
-fn main() -> Result<()> {
+fn main() {
     for filename in env::args().skip(1).collect::<Vec<String>>() {
-        let mut reader = File::open(&filename).context(format!("unable to open '{}'", filename))?;
-        let wordcount =
-            count_words(&mut reader).context(format!("unable to count words in '{}'", filename))?;
-        println!("{} {}", wordcount, filename);
+        let mut _reader = File::open(&filename).unwrap_or_else(|err| {
+		println!("unable to open '{}': {}", filename,err);
+		process::exit(1);
+	});
+        println!("succesfully opened {}", filename);
     }
-    Ok(())
 }
