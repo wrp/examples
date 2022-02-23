26709b6517a5e824031826b3eeea6c7c84021cac
William Pursell <william.r.pursell@gmail.com>
2022-02-23 14:00:34 -0700

    Whitespace

--- rust/src/open.rs
+++ rust/src/open.rs
@@ -2,11 +2,11 @@
 use std::{env, fs::File, process};
 
 fn main() {
-    for filename in env::args().skip(1).collect::<Vec<String>>() {
-        let mut _reader = File::open(&filename).unwrap_or_else(|err| {
-		println!("unable to open '{}': {}", filename,err);
-		process::exit(1);
-	});
-        println!("succesfully opened {}", filename);
-    }
+	for filename in env::args().skip(1).collect::<Vec<String>>() {
+		let mut _reader = File::open(&filename).unwrap_or_else(|err| {
+			println!("unable to open '{}': {}", filename, err);
+			process::exit(1);
+		});
+		println!("succesfully opened {}", filename);
+	}
 }
