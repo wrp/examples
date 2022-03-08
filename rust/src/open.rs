fac6a809ec2e68876d4fcaee45da2849398d1834
William Pursell <william.r.pursell@gmail.com>
2022-03-08 16:56:23 -0700

    Add example using BufReader to read file

--- rust/src/open.rs
+++ rust/src/open.rs
@@ -1,12 +1,17 @@
 
-use std::{env, fs::File, process};
+use std::{env, fs::File, process, io::BufReader, io::BufRead};
 
 fn main() {
 	for filename in env::args().skip(1).collect::<Vec<String>>() {
-		let mut _reader = File::open(&filename).unwrap_or_else(|err| {
+		let r = File::open(&filename).unwrap_or_else(|err| {
 			println!("unable to open '{}': {}", filename, err);
 			process::exit(1);
 		});
+		let r = BufReader::new(r);
 		println!("succesfully opened {}", filename);
+		println!("contents:");
+		for line in r.lines() {
+			println!("{}", line.unwrap());
+		}
 	}
 }
