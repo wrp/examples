4b167beb94da98e2c8cfef7227d39b9791b53069
William Pursell <william.r.pursell@gmail.com>
2022-03-23 13:49:52 -0600

    Add python/dis-example.py

---
+++ python/dis-example.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from dis import dis
+def my_list():
+    x = [10, 20, 30, 'abc']
+    y = x[0]
+def my_tuple():
+    x = (10, 20, 30, 'abc')
+    y = x[0]
+dis(my_list)
+dis(my_tuple)
