3e96f2a63437d980dc3cab6c8aa2c0cbae15ff2c
William Pursell <william.r.pursell@gmail.com>
2022-03-31 10:38:42 -0400

    Format all python with black

--- python/with.py
+++ python/with.py
@@ -2,6 +2,6 @@
 
 # Sample using complex with:
 
-with open('/tmp/foo','w') as f, open('/tmp/bar','w') as g:
-    f.write('go foo\n')
-    g.write('go bar\n')
+with open("/tmp/foo", "w") as f, open("/tmp/bar", "w") as g:
+    f.write("go foo\n")
+    g.write("go bar\n")
--- python/yaml2json.py
+++ python/yaml2json.py
@@ -12,16 +12,20 @@ def main(args):
 
     for x in args:
         try:
-            json.dump(yaml.load(open(x,'r').read(), Loader=yaml.SafeLoader), sys.stdout, indent = 8)
+            json.dump(
+                yaml.load(open(x, "r").read(), Loader=yaml.SafeLoader),
+                sys.stdout,
+                indent=8,
+            )
         except IOError as e:
-            if e.errno in [2,63]:
-                json.dump(yaml.load(x), sys.stdout, indent = 8)
+            if e.errno in [2, 63]:
+                json.dump(yaml.load(x), sys.stdout, indent=8)
             else:
                 raise e
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) > 1:
         main(sys.argv[1:])
     else:
-        main(['/dev/stdin/'])
+        main(["/dev/stdin/"])
