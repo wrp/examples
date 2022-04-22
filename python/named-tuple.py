ef4b8be9d23d95d12bd2e1991f6019291b257af3
William Pursell <william.r.pursell@gmail.com>
2022-04-22 15:36:19 -0600

    Reference space separated list in namedtuple

--- python/named-tuple.py
+++ python/named-tuple.py
@@ -3,11 +3,19 @@
 from collections import namedtuple
 
 Thing = namedtuple('Thingy', ['one', 'two'])
-
 a = Thing('foo','bar')
-
 c, b = a
-print "a=", a  # A is a Thingy
+
+print "a=", a  # a is a Thingy
 print "b=", b  # b is 'bar'
 print "c=", c  # c is 'foo'
+print "a.one = ", a.one
+print "a.two = ", a.two
 
+
+
+# Note that the naming of the fields can also done with a
+# string containing a space or comma separated list
+SThing = namedtuple('Thingy', 'one, two')
+a = SThing(5, 6)
+print('a.one = {}'.format(a.one))
