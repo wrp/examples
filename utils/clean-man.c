30c8753328c44526c1a4eef3c78b00eff6c872b8
William Pursell <william.r.pursell@gmail.com>
2022-07-21 07:01:16 -0600

    Copy-paste the xutils
    Need to think about this: do I want to shift the xutils from
    ../c and make a convenience library?   It is not clear if
    this package will ever be used, and it is useful to have that
    code directly in ../c.  Maybe consider having a different
    Makefile to install only things I actually want and keep
    everything in ../c?

--- utils/clean-man.c
+++ utils/clean-man.c
@@ -2,15 +2,21 @@
  * Clean up man pages.
  * Discard octet before 0x08.
  * Discard all between 0x1b and 'm'
- * This is pointless: use col -bx
  */
 #include "config.h"
 #include <assert.h>
 #include <errno.h>
 #include <limits.h>
+#include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include "xutil.h"
+
+FILE * xfopen(const char *path, const char *mode);
+FILE * xtmpfile(char *tempname, size_t siz, const char *mode);
+int die(const char *fmt, ...);
+void xrename(const char *old, const char *new);
+
 
 int
 main(int argc, char **argv)
@@ -61,5 +67,68 @@ main(int argc, char **argv)
 			fclose(ofp);
 			xrename(tmpname, *arg);
 		}
+	}
+}
+
+FILE *
+xfopen(const char *path, const char *mode)
+{
+	FILE *fp = path[0] != '-' || path[1] != '\0' ? fopen(path, mode) :
+		*mode == 'r' ? stdin : stdout;
+	if( fp == NULL ){
+		perror(path);
+		exit(EXIT_FAILURE);
+	}
+	return fp;
+}
+
+FILE *
+xtmpfile(char *tempname, size_t siz, const char *mode)
+{
+	const char *template = "xutil.XXXXXXXXX";
+	int fd;
+	char *tmpdir = getenv("TMPDIR");
+	int length = 0;
+
+	if( tmpdir ){
+		length = snprintf(tempname, siz - 1, "%s", tmpdir);
+		if( length < siz - 1 && tempname[length - 1] != '/' ){
+			tempname[length++] = '/';
+		}
+	}
+	tempname[length] = '\0';
+
+	strncat(tempname, template, siz - length);
+
+	if( (fd = mkstemp(tempname)) == -1 ){
+		perror(tempname);
+		exit(EXIT_FAILURE);
+	}
+	FILE *fp = fdopen(fd, mode);
+	if( fp == NULL ){
+		perror(tempname);
+		exit(EXIT_FAILURE);
+	}
+	return fp;
+}
+
+int
+die(const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+	exit(EXIT_FAILURE);
+}
+
+void
+xrename(const char *old, const char *new)
+{
+	if( rename(old, new) ){
+		int errno_sav = errno;
+		fprintf(stderr, "rename: %s -> ", old);
+		perror(new);
+		exit(EXIT_FAILURE);
 	}
 }
