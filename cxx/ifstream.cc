65ebcc139ee1817c1c92005062a2ad6326c58590
William Pursell <william.r.pursell@gmail.com>
2022-07-28 12:25:52 -0600

    Use std::perror

--- cxx/ifstream.cc
+++ cxx/ifstream.cc
@@ -8,10 +8,8 @@ main(int argc, char **argv)
 {
 	const char *path = argc > 1 ? argv[1] : "foo";
 	std::ifstream f(path);
-	if( f.is_open() ){
-		std::cout << "open" << std::endl;
-	} else {
-		std::cerr << path << ": " << strerror(errno) << std::endl;
+	if( ! f.is_open() ){
+		std::perror(path);
 	}
 	return 0;
 }
