210143a82ee6cca567edbc2acb4d3acffbc9db7a
William Pursell <william.r.pursell@gmail.com>
2022-08-04 06:48:59 -0600

    Clean up output of complex

--- c/complex.c
+++ c/complex.c
@@ -1,20 +1,68 @@
 
 #include <complex.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+static void
+show(double complex z)
+{
+	double imag = cimag(z);
+	printf("%lg %c I * %lg", creal(z), imag < 0 ? '-' : '+', fabs(imag));
+}
+
+static void
+display(double complex z, double complex c, double d, const char *msg, int t)
+{
+	printf("%10s ", msg);
+	show(z);
+	fputs(" = ", stdout);
+	if(t)
+		show(c);
+	else
+		printf("%lg", d);
+	putchar('\n');
+}
 
 int
-main(void)
+main(int argc, char **argv)
 {
-	double complex z = 1.0 + 1.0 * I;  // z = 1 + i
-	float complex a;
-	double b = 1.0;
+	double complex z = 1.0 + 1.0 * I;
+	if( argc > 1 ){
+		double r = strtod(argv[1], NULL);
+		z = CMPLX(r, cimag(z));
+	}
+	if( argc > 2 ){
+		double r = strtod(argv[1], NULL);
+		z = CMPLX(creal(z), r);
+	}
 
-	a = (float complex) b; /* sets imaginary part to 0 */
-	printf("a = %f + %fi\n", crealf(a), cimagf(a));
-	printf("z = %lf + %lfi\n", creal(z), cimag(z));
-	printf("|z| = %lf\n", cabs(z));
+#define DOD(X) display(z, 0, X(z), #X, 0)
+	DOD(creal);
+	DOD(cimag);
+	DOD(cabs);
+	DOD(carg);
 
-	double complex j = conj(z);
-	printf("conj z = %lf + %lfi\n", creal(j), cimag(j));
+#define DOC(X) display(z, X(z), 0, #X, 1)
+	DOC(cexp);
+	DOC(clog);
+	DOC(conj);
+	DOC(cproj);
+	DOC(csqrt);
+	DOC(casinh);
+	DOC(cacosh);
+	DOC(catanh);
+	DOC(csinh);
+	DOC(ccosh);
+	DOC(ctanh);
+	DOC(casin);
+	DOC(cacos);
+	DOC(catan);
+	DOC(csin);
+	DOC(ccos);
+	DOC(ctan);
+
+
 	return 0;
 }
