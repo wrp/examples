625abe9711d6b911b114ed03304f41f2adbb6c91
William Pursell <william.r.pursell@gmail.com>
2022-08-08 22:28:25 -0600

    Stub in autotool meta files and a test

---
+++ libhash/hashtest.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <string.h>
+#include "hashmap.h"
+
+struct user {
+	char *name;
+	int age;
+};
+
+int
+user_compare(const void *a, const void *b, void *udata)
+{
+	const struct user *ua = a;
+	const struct user *ub = b;
+	return strcmp(ua->name, ub->name);
+}
+
+bool
+user_iter(const void *item, void *udata)
+{
+	const struct user *user = item;
+	*(int *)udata += 1;
+	return true;
+}
+
+uint64_t
+user_hash(const void *item, uint64_t seed0, uint64_t seed1)
+{
+	const struct user *user = item;
+	return hashmap_sip(user->name, strlen(user->name), seed0, seed1);
+}
+
+#define expect(x) if( !(x) ){ \
+	fail += 1; \
+	fprintf(stderr, "Test failure in %s at line %d: %s\n", \
+		__FILE__, __LINE__, #x); \
+	}
+
+int
+main(void)
+{
+	// create a new hash map where each item is a `struct user`. The second
+	// argument is the initial capacity. The third and fourth arguments are
+	// optional seeds that are passed to the following hash function.
+	int fail = 0;
+	struct user *user;
+	struct hashmap *map = hashmap_new(
+		sizeof *user, 0, 0, 0, user_hash, user_compare,
+		NULL, NULL
+	);
+
+	// Here we'll load some users into the hash map. Each set operation
+	// performs a copy of the data that is pointed to in the second argument.
+	hashmap_set(map, &(struct user){ .name="Dale", .age=44 });
+	hashmap_set(map, &(struct user){ .name="Roger", .age=68 });
+	hashmap_set(map, &(struct user){ .name="Jane", .age=47 });
+
+
+	user = hashmap_get(map, &(struct user){ .name="Jane" });
+	expect( strcmp(user->name, "Jane") == 0 );
+	expect( user->age == 47 );
+
+	user = hashmap_get(map, &(struct user){ .name="Roger" });
+	expect( strcmp(user->name, "Roger") == 0 );
+	expect( user->age == 68 );
+
+	user = hashmap_get(map, &(struct user){ .name="Dale" });
+	expect( strcmp(user->name, "Dale") == 0 );
+	expect( user->age == 44 );
+
+	user = hashmap_get(map, &(struct user){ .name="Tom" });
+	expect( user == NULL );
+
+	/* terate over all users  */
+	int i = 0;
+	hashmap_scan(map, user_iter, &i);
+	expect( i == 3);
+
+	hashmap_free(map);
+	if( fail ){
+		fprintf(stderr, "%d test%s failed\n", fail, &"s"[fail == 1]);
+	}
+	return fail;
+}
+
+
