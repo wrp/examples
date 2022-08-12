ef85f93751d95c03593d192181d090cd1b5bad70
William Pursell <william.r.pursell@gmail.com>
2022-08-12 13:23:16 -0600

    Update copyrights

--- libhash/hashmap.h
+++ libhash/hashmap.h
@@ -1,6 +1,12 @@
-// Copyright 2020 Joshua J Baker. All rights reserved.
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file.
+/*
+ * Copyright:
+ *   2020 Joshua J Baker
+ *   2022 William R. Pursell
+ * All rights reserved.
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file.
+ */
 
 #ifndef HASHMAP_H
 #define HASHMAP_H
