8cbc2f027962383531d89022c0f70fcb364c8b8f
William Pursell <william.r.pursell@gmail.com>
2022-08-18 08:58:50 -0600

    Put benchmark back in main file
    There was a noticeable benchmark regression at commit
    7a72d51daf012c88d03c3aa706cf0c2aa1a8de6b.  Although the
    commit message there explicitly states that these benchmarks
    are mostly useless, they do have some use and there is no
    need to incur the loss incurred by pulling the test out.
    But...note that the loss is almost certainly just the overhead
    from libtool/etc, and putting them back in merely to try
    to match the numbers we get from the original code remains
    utter folly.  Any real usage of the library cannot do this,
    so the benchmark remains completely uselss.  But at least
    now the numbers remain similar to the original code.

--- hashmap/hashmap.c
+++ hashmap/hashmap.c
@@ -18,6 +18,10 @@
 #include <stddef.h>
 #include "hashmap.h"
 
+#ifdef HASHMAP_TEST
+#include <time.h>
+#endif
+
 struct bucket {
 	uint64_t hash:48;
 	uint64_t dib:16;
@@ -601,3 +605,150 @@ hashmap_murmur(const void *key, size_t len, uint64_t seed, uint64_t seed1)
     ((uint32_t*)out)[3] = h4;
     return *(uint64_t*)out;
 }
+#ifdef HASHMAP_TEST
+
+clock_t begin;
+int N;
+
+static uint64_t
+hash_int(const void *item, uint64_t seed0, uint64_t seed1)
+{
+	return hashmap_murmur(item, sizeof(int), seed0, seed1);
+}
+
+static int
+compare_ints_udata(const void *a, const void *b, void *udata)
+{
+	(void)udata;
+	return *(int*)a - *(int*)b;
+}
+
+static void
+shuffle(void *array, size_t numels, size_t elsize)
+{
+	char tmp[elsize];
+	char *arr = array;
+	for( size_t i = 0; i < numels - 1; i++ ){
+		int j = i + rand() / (RAND_MAX / (numels - i) + 1);
+		memcpy(tmp, arr + j * elsize, elsize);
+		memcpy(arr + j * elsize, arr + i * elsize, elsize);
+		memcpy(arr + i * elsize, tmp, elsize);
+	}
+}
+
+static clock_t
+setup_benchmark(const char *name)
+{
+	printf("%-14s ", name);
+	return begin = clock();
+}
+
+
+static void
+show_bench_results(void)
+{
+	clock_t end = clock();
+	double elapsed_secs = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf(
+		"%d ops in %.3f secs, %.0f ns/op, %.0f op/sec",
+		N,
+		elapsed_secs,
+		elapsed_secs / (double)N*1e9,
+		(double)N / elapsed_secs
+	);
+	putchar('\n');
+}
+
+
+int
+main(int argc, char **argv) /* benchmarks */
+{
+	N = argc > 1 ? strtol(argv[1], NULL, 10) : 5000000;
+	int seed = argc > 2 ? strtol(argv[2], NULL, 10) : time(NULL);
+
+	printf("seed = %d, count = %d, item_size = %zu\n", seed, N, sizeof N);
+	srand(seed);
+
+	int *vals = malloc(N * sizeof *vals);
+	if( vals == NULL ){
+		perror("malloc");
+		return 1;
+	}
+	for( int i = 0; i < N; i += 1 ){
+		vals[i] = i;
+	}
+
+	shuffle(vals, N, sizeof *vals);
+
+	struct hashmap *map;
+	shuffle(vals, N, sizeof *vals);
+
+	struct hash_method hash = {hash_int, {seed, seed} };
+	struct hash_element el = { sizeof *vals, compare_ints_udata, 0, 0 };
+	map = hashmap_new_with_allocator(
+		malloc, free, &el, &hash, 0
+	);
+	if( map == NULL ){
+		perror("out of memory");
+		exit(1);
+	}
+
+	setup_benchmark("set");
+	for( int i = 0; i < N; i++ ){
+		int *v = hashmap_set(map, &vals[i]);
+		assert(!v);
+	}
+	show_bench_results();
+
+	shuffle(vals, N, sizeof *vals);
+
+	setup_benchmark("get");
+	for( int i = 0; i < N; i++ ){
+		int *v = hashmap_get(map, &vals[i]);
+		assert(v && *v == vals[i]);
+	}
+	show_bench_results();
+
+	shuffle(vals, N, sizeof *vals);
+	setup_benchmark("delete");
+	for( int i = 0; i < N; i++ ){
+		int *v = hashmap_delete(map, &vals[i]);
+		assert(v && *v == vals[i]);
+	}
+	show_bench_results();
+	hashmap_free(map);
+
+	map = hashmap_new_with_allocator( malloc, free, &el, &hash, N);
+	if( map == NULL ){
+		perror("out of memory");
+		exit(1);
+	}
+	setup_benchmark("set (cap)");
+	for( int i = 0; i < N; i++ ){
+		int *v = hashmap_set(map, &vals[i]);
+		assert(!v);
+	}
+	show_bench_results();
+
+	shuffle(vals, N, sizeof *vals);
+	setup_benchmark("get (cap)");
+	for( int i = 0; i < N; i++ ){
+		int *v = hashmap_get(map, &vals[i]);
+		assert(v && *v == vals[i]);
+	}
+	show_bench_results();
+
+	shuffle(vals, N, sizeof *vals);
+	setup_benchmark("delete (cap)");
+	for( int i = 0; i < N; i++ ){
+		int *v = hashmap_delete(map, &vals[i]);
+		assert(v && *v == vals[i]);
+	}
+	show_bench_results();
+
+	hashmap_free(map);
+
+	free(vals);
+	return 0;
+}
+#endif
