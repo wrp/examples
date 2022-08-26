c799bd4e8d02bf6a3a5426f834b9b9a4dc89670f
William Pursell <william.pursell@affirm.com>
2022-08-26 08:37:42 -0600

    Add some perl Time::Piece examples

--- perl/time-piece.pl
+++ perl/time-piece.pl
@@ -1,11 +1,24 @@
 #!/usr/bin/env perl
 
-# Replace occurences of 'mm/dd/yy HH:MM:SS' with epoch time
-
-use strict;
-use warnings;
+use 5.12.10;
 use Time::Piece;
 
+# Increment an ISO 8601 time (eg 2022-08-20T02:08:15Z) by 1 second
+while( <> ){
+	if( $_ !~ m@\d{4}-\d{1,2}-\d{1,2}T\d{1,2}:\d{2}:\d{2}Z@ ){
+		say STDERR "Invalid format $_";
+		next
+	}
+	chop;  # Delete newline
+	chop;  # Delete trailing 'Z'
+	my $t = Time::Piece->strptime($_, '%Y-%m-%dT%H:%M:%S');
+	say $t + 1;
+}
+
+exit;
+
+
+# Replace occurences of 'mm/dd/yy HH:MM:SS' with epoch time
 while(<>) {
 	s@(\d{1,2}/\d{1,2}/\d\d \d{1,2}:\d\d:\d\d)@
 		sprintf scalar Time::Piece->strptime(
@@ -14,6 +27,6 @@ while(<>) {
 }
 
 
-
 # shell:
 # three_days_ago=$( perl -MTime::Piece -MTime::Seconds=ONE_DAY -E 'my $g=gmtime; $g -= 3* ONE_DAY; say $g->datetime' )
+# echo 2022-08-20T02:08:59Z | perl -MTime::Piece -lnE 'chop; my $t = Time::Piece->strptime($_, "%Y-%m-%dT%H:%M:%S") + 1; say $t->datetime'
