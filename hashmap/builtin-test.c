d2fcce7f2fb5fc66885521bc600f16bd999699ee
William Pursell <william.r.pursell@gmail.com>
2022-10-15 14:36:31 -0600

    Trivial reformat

--- hashmap/builtin-test.c
+++ hashmap/builtin-test.c
@@ -327,14 +327,10 @@ main(int argc, char **argv)
 		}
 	}
 
-    for (unsigned i = 0; i < N; i++) {
-        while (true) {
-            assert(!hashmap_set(map, &vals[i]));
-            if (!hashmap_oom(map)) {
-                break;
-            }
-        }
-    }
+	for( unsigned i = 0; i < N; i += 1 ){
+		do assert( !hashmap_set(map, vals + i) );
+		while( hashmap_oom(map) );
+	}
 
     assert(hashmap_count(map) != 0);
 
