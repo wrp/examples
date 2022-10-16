98a6cdeef3057b04252d81c43a37a44534f54324
William Pursell <william.pursell@affirm.com>
2022-10-16 08:28:48 -0600

    Insert more newlines, cleanup macros

--- hashmap/hashmap.c
+++ hashmap/hashmap.c
@@ -462,24 +462,31 @@ hashmap_sip(const void *data, size_t inlen, uint64_t seed0, uint64_t seed1)
 	((uint64_t)((p)[4]) << 32) | ((uint64_t)((p)[5]) << 40) | \
 	((uint64_t)((p)[6]) << 48) | ((uint64_t)((p)[7]) << 56) \
 )
+#define U32TO8_LE(p, v) \
+	(p)[0] = (uint8_t)((v)); \
+	(p)[1] = (uint8_t)((v) >> 8); \
+	(p)[2] = (uint8_t)((v) >> 16); \
+	(p)[3] = (uint8_t)((v) >> 24)
 #define U64TO8_LE(p, v) \
 	U32TO8_LE((p) + 0, (uint32_t)((v))); \
 	U32TO8_LE((p) + 4, (uint32_t)((v) >> 32))
-#define U32TO8_LE(p, v) \
-    { (p)[0] = (uint8_t)((v)); \
-      (p)[1] = (uint8_t)((v) >> 8); \
-      (p)[2] = (uint8_t)((v) >> 16); \
-      (p)[3] = (uint8_t)((v) >> 24); }
 #define ROTL(x, b) (uint64_t)(((x) << (b)) | ((x) >> (64 - (b))))
 #define SIPROUND \
-    { v0 += v1; v1 = ROTL(v1, 13); \
-      v1 ^= v0; v0 = ROTL(v0, 32); \
-      v2 += v3; v3 = ROTL(v3, 16); \
-      v3 ^= v2; \
-      v0 += v3; v3 = ROTL(v3, 21); \
-      v3 ^= v0; \
-      v2 += v1; v1 = ROTL(v1, 17); \
-      v1 ^= v2; v2 = ROTL(v2, 32); }
+	v0 += v1; \
+	v1 = ROTL(v1, 13); \
+	v1 ^= v0; \
+	v0 = ROTL(v0, 32); \
+	v2 += v3; \
+	v3 = ROTL(v3, 16); \
+	v3 ^= v2; \
+	v0 += v3; \
+	v3 = ROTL(v3, 21); \
+	v3 ^= v0; \
+	v2 += v1; \
+	v1 = ROTL(v1, 17); \
+	v1 ^= v2; \
+	v2 = ROTL(v2, 32)
+
 	uint64_t k0 = U8TO64_LE((uint8_t*)&seed0);
 	uint64_t k1 = U8TO64_LE((uint8_t*)&seed1);
 	uint64_t v3 = UINT64_C(0x7465646279746573) ^ k1;
