d5ef2ea969c11e778ba9abdf79c1aef0a2c801c6
William Pursell <william.pursell@affirm.com>
2022-10-17 16:27:35 -0600

    Report word count once per file

--- rust/src/count-words.rs
+++ rust/src/count-words.rs
@@ -22,15 +22,15 @@ fn open_file(path: &str) -> std::fs::File {
 fn main() {
 	for filename in env::args().skip(1).collect::<Vec<String>>() {
 		let r = BufReader::new(open_file(&filename));
+		let mut words = HashMap::new();
 		for line in r.lines(){
-			let mut words = HashMap::new();
 			let s = line.expect("read_error");
 			for word in s.split_whitespace(){
-				let count = words.entry(word).or_insert(0);
+				let count = words.entry(String::from(word)).or_insert(0);
 				*count += 1;
 			}
-			println!("{:?}", words);
 		}
+		println!("{:?}", words);
 	}
 }
 
