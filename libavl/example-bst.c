8a917efe3ca3da49cfde23c0cba18f769e8c1eab
William Pursell <william.r.pursell@gmail.com>
2022-10-25 15:11:42 -0600

    Add insert wrapper

--- libavl/example-bst.c
+++ libavl/example-bst.c
@@ -1,6 +1,7 @@
 #include "bst.h"
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
 struct person {
@@ -9,12 +10,39 @@ struct person {
 	char *occupation;
 };
 
+void *
+xmalloc(size_t s)
+{
+	void *rv = malloc(s);
+	if( rv == NULL ){
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	return rv;
+}
 
 int
 comp(const void *a, const void *b, void *u)
 {
 	(void)u;
 	return strcmp(((struct person *)a)->name,((struct person *)b)->name);
+}
+
+
+void *
+insert(struct bst_table *b, char *name, int age, char *occupation)
+{
+	struct person *k = xmalloc(sizeof *k);
+	k->name = name;
+	k->age = age;
+	k->occupation = occupation;
+	return bst_insert(b, k);
+}
+
+void *
+get(struct bst_table *b, char *name)
+{
+	return bst_find(b, &(struct person){name});
 }
 
 int
@@ -32,5 +60,13 @@ main(int argc, char **argv)
 	p = bst_find(b, &c);
 
 	printf("%s -> %d -> %s\n", p->name, p->age, p->occupation);
+
+	insert(b, "larry", 23, "none");
+	insert(b, "bob", 14, "none");
+	insert(b, "linda", 54, "none");
+
+	p = get(b, "linda");
+	printf("%s -> %d -> %s\n", p->name, p->age, p->occupation);
+
 	return 0;
 }
