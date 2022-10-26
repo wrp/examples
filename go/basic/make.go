55639f1edfa709b8225b1eedd49310849751eb9b
William Pursell <william.r.pursell@gmail.com>
2022-10-26 08:52:32 -0600

    Add example using make to create a channel

--- go/basic/make.go
+++ go/basic/make.go
@@ -12,6 +12,9 @@ func make_example() {
 	x := make([]int, 10, 100)  // make(T, length, capacity)
 	var y []int = make([]int, 5, 50)
 
+	c := make(chan int, 5)  // Create a channel with a capacity of 5
+
 	fmt.Printf("x is a %v with capacity %v\n", reflect.ValueOf(x).Type(), cap(x))
 	fmt.Printf("y is a %v with capacity %v\n", reflect.ValueOf(y).Type(), cap(y))
+	fmt.Printf("c is a %v with capacity %v\n", reflect.ValueOf(c).Type(), cap(c))
 }
