27c30fdd7fd23b2f564d5293a5bb384dc0dd3f17
William Pursell <william.r.pursell@gmail.com>
2022-10-25 18:32:12 -0600

    Document arguments to make

--- go/basic/make.go
+++ go/basic/make.go
@@ -9,7 +9,7 @@ import (
 func make_example() {
 
 	// make is used for slices, maps, and channels
-	x := make([]int, 10, 100)
+	x := make([]int, 10, 100)  // make(T, length, capacity)
 	var y []int = make([]int, 5, 50)
 
 	fmt.Printf("x is a %v with capacity %v\n", reflect.ValueOf(x).Type(), cap(x))
