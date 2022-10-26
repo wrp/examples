c1d73eb2527d7a9d274c2ea0bf8310320cb168f6
William Pursell <william.r.pursell@gmail.com>
2022-10-26 09:46:08 -0600

    Add example grouping vars with parens

--- go/basic/array/array.go
+++ go/basic/array/array.go
@@ -5,10 +5,12 @@ import (
 )
 
 func Example() {
-	var a [5]int                          /* An array of 5 integers, initialized to 0 */
+	var (
+		a [5]int                          /* An array of 5 integers, initialized to 0 */
+		c [5]int = [5]int{6, 7, 8, 9, 10} /* An initialized array */
+	)
 	b := [5]int{1, 2, 3}                  /* An initialized array, unset values go to 0 */
 	e := [...]int{1, 2, 3}                /* An initialized array */
-	var c [5]int = [5]int{6, 7, 8, 9, 10} /* An initialized array */
 	var d [2][3]int = [2][3]int{{1, 2, 3}, {4, 5, 6}}
 	_, _, _, _ = a, b, c, d
 
