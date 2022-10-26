035d10acad6bc5532b329703e0e31496f00e0f9e
William Pursell <william.r.pursell@gmail.com>
2022-10-25 18:39:29 -0600

    Add go/src/Makefile go/src/README go/src/map.go

---
+++ go/src/map.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	/* map[K]V  creates a map with keys of type K and values of type V */
+	m := map[int]int{ 1: 10, 2: 12, 5: 13, }
+	fmt.Println(m)
+
+	/* Print unsorted */
+	fmt.Println("unsorted map with", len(m), "elements")
+	for k, v := range m {
+		fmt.Printf("%v -> %v\n", k, v)
+	}
+
+	s := make([]int, 0, len(m))
+	for k := range m {
+		s = append(s, k)
+	}
+	fmt.Println("presorted s = ", s)
+	sort.Ints(s)
+	fmt.Println("   sorted s = ", s)
+
+	fmt.Println("sorted:")
+	for _, v := range s {
+		fmt.Printf("%v -> %v\n", v, m[v])
+	}
+
+
+	p := make(map[string]int, 10)
+	p["foo"] = 5
+	fmt.Printf("p = %v, len = %d\n", p, len(p)) /* cap not defined for maps */
+}
