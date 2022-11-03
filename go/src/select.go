d3fa865c3039ee75d0b76c5a1588179a30ac0e0c
William Pursell <william.r.pursell@gmail.com>
2022-11-03 15:53:07 -0600

    Stop using time.After to create new timer for each call

--- go/src/select.go
+++ go/src/select.go
@@ -6,7 +6,12 @@ import (
 	"time"
 )
 
+
+// Note: time.After creates a new timer object each time it is called
 func fibonacci(c, q chan int) {
+
+	timer := time.NewTimer(100 * time.Millisecond)
+	defer timer.Stop()
 	x, y := 1, 1
 	for {
 		// select blocks until some case is ready.
@@ -17,7 +22,8 @@ func fibonacci(c, q chan int) {
 			x, y = y, x + y
 		case <- q:
 			fmt.Println("quit")
-		case <- time.After(100 * time.Millisecond):
+
+		case <- timer.C:  // expired
 			fmt.Println("timeout")
 			return
 		}
