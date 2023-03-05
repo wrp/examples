5cb778c97d3c39d5b5248ea72fed2928ffcb3605
William Pursell <william.r.pursell@gmail.com>
2023-03-05 13:34:25 -0700

    Pass file handle rather than "/dev/stdin"

--- python/yaml2json.py
+++ python/yaml2json.py
@@ -8,24 +8,22 @@ import json
 # json.dump(yaml.load(sys.stdin.read()), sys.stdout, indent = 8)
 
 
-def main(args):
-
-    for x in args:
-        try:
-            json.dump(
-                yaml.load(open(x, "r").read(), Loader=yaml.SafeLoader),
+def main(fh):
+    try:
+        json.dump(
+                yaml.load(fh.read(), Loader=yaml.SafeLoader),
                 sys.stdout,
                 indent=8,
-            )
-        except IOError as e:
-            if e.errno in [2, 63]:
-                json.dump(yaml.load(x), sys.stdout, indent=8)
-            else:
-                raise e
+        )
+    except:
+        json.dump(yaml.load(x), sys.stdout, indent=8)
+    print()
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        main(sys.argv[1:])
+        for x in sys.argv[1:]:
+            with open(x, "r") as f:
+                main(f)
     else:
-        main(["/dev/stdin/"])
+        main(sys.stdin)
