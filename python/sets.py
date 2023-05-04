c1a17843fb13a51a1164f8d5a23457c6b0902907
William Pursell <william.r.pursell@gmail.com>
2023-05-04 05:03:32 -0600

    Tweak sets.py

--- python/sets.py
+++ python/sets.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # various set operations (2.7)
 
-A = {0, 2, 4, 6, 8};
+A = {0, 2, 4, 6, 8, 2};
 B = {1, 2, 3, 4, 5};
 
-print "type: ",  type(A)
-print "Union :", A | B
-print "Intersection :", A & B
-print "Difference :", A - B
-print "Symmetric difference :", A ^ B
+print("A:                         A = ", A)
+print("B:                         B = ", B)
+print("type:                type(A) = ", type(A))
+print("Union:                 A | B = ", A | B)
+print("Intersection:          A & B = ", A & B)
+print("Difference :           A - B = ", A - B)
+print("Symmetric difference : A ^ B = ", A ^ B)
