0c110746cf43cbfb0b3a0212c73ffc8617f00b82
William Pursell <william.r.pursell@gmail.com>
2023-05-16 17:40:22 -0600

    Show some techniques for reading a single line

--- rust/src/stdin.rs
+++ rust/src/stdin.rs
@@ -4,8 +4,19 @@ use std::io::{self, BufRead};
 
 fn main() -> io::Result<()> {
 	let stdin = io::stdin();
-	let mut lines = stdin.lock().lines();
+	let mut line = String::new();
 
+	// Read one line, demonstrating one technique
+	let line1 = stdin.lock().lines().next().unwrap().unwrap();
+	// line1 does *not* have a trailing newline
+	println!("first line: {}", line1);
+
+	// Read one line, demonstrating another technique
+	// This *does* retain the trailing newline
+	stdin.lock().read_line(&mut line).unwrap();
+	print!("2nd line: {}", line);
+
+	let mut lines = stdin.lock().lines();
 	while let Some(line) = lines.next() {
 		println!("{}", line.unwrap());
 	}
