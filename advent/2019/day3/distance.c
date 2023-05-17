a83ca72eda1e2a6b778e2f878aa5b9d95382a286
William Pursell <william.r.pursell@gmail.com>
2023-05-16 20:47:55 -0600

    Compute distance

--- advent/2019/day3/distance.c
+++ advent/2019/day3/distance.c
@@ -10,7 +10,6 @@ U62,R66,U55,R34,D71,R55,D58,R83
 
 struct point {
 	unsigned path;
-	unsigned sum;
 };
 
 int
@@ -20,7 +19,6 @@ trace_path(struct point *map, unsigned path, size_t S)
 	int val;
 	int c;
 	int dir;
-	int count = 0;
 	do {
 		switch( dir = getchar() ) {
 		default: errx(1, "Unexpected input: %c\n", dir);
@@ -33,7 +31,6 @@ trace_path(struct point *map, unsigned path, size_t S)
 			errx(1, "Unexpected value %d\n", val);
 		}
 		for( ; val > 0; val-- ){
-			count += 1;
 			switch( dir ){
 			case 'U': y -= 1; break;
 			case 'D': y += 1; break;
@@ -42,9 +39,6 @@ trace_path(struct point *map, unsigned path, size_t S)
 			}
 			if( y < 0 || y > (int)S-1 || x < 0 || x > (int)S-1 ){
 				errx(1, "out of bounds\n");
-			}
-			if( (map[S * y + x].path & path) == 0 ){
-				map[S * y + x].sum += count;
 			}
 			map[S * y + x].path |= path;
 		}
@@ -61,7 +55,7 @@ dist(int x, int y, size_t S)
 int
 main(int argc, char *argv[])
 {
-	size_t S = argc > 1 ? strtol(argv[1], NULL, 10) : 512;
+	size_t S = argc > 1 ? strtol(argv[1], NULL, 10) : 23000;
 	struct point *map = calloc(S * S, sizeof *map);
 	unsigned path = 0x1;
 	unsigned min = UINT_MAX;
@@ -69,10 +63,10 @@ main(int argc, char *argv[])
 	while( trace_path(map, path, S) ) {
 		path <<= 1;
 	}
-	for( int y = 0; y < (int)S; y++ ) {
-		for( int x = 0; x < (int)S; x++ ){
+	for( int y = 0; y < (int)S; y += 1 ) {
+		for( int x = 0; x < (int)S; x += 1 ){
 			if( map[S * y + x].path > 2 ){
-				unsigned d = map[S * y + x].sum;
+				unsigned d = dist(x, y, S);
 				min = d < min ? d : min;
 			}
 		}
