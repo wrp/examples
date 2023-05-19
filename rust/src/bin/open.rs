c885dbdc24dd2c82e6b55d61b960cf13ea5cbbe5
William Pursell <william.r.pursell@gmail.com>
2023-05-18 21:08:20 -0600

    Demonstrate using ? in File::open
    This operator is completely useless in this context, since the name
    used in the attempt to open the file does not get into the error message.
    Nevertheless, it is worthwhile to demonstrate the use case.

--- rust/src/bin/open.rs
+++ rust/src/bin/open.rs
@@ -1,24 +1,32 @@
 
+use std::error::Error;
+use std::fs::File;
 use std::{
 	env,
 	process,
-	io::BufReader,
-	io::BufRead,
+	io::{BufReader, BufRead},
 	fs::OpenOptions,
 };
 
 fn open_file(path: &str) -> std::fs::File {
 	OpenOptions::new()
-		.read(true)
-		.open(path)
-		.unwrap_or_else(|err| {
-			eprintln!("unable to open '{}': {}", path, err);
-			process::exit(1);
-			}
-		)
+	.read(true)
+	.open(path)
+	.unwrap_or_else(|err|
+		{
+		eprintln!("unable to open '{}': {}", path, err);
+		process::exit(1);
+		}
+	)
 }
 
-fn main() {
+
+fn last_char_of_first_line(text: BufReader<File>) -> Option<char> {
+    text.lines().next()?.expect("FOO").chars().last()
+}
+
+
+fn main() -> Result<(), Box<dyn Error>> {
 	for filename in env::args().skip(1).collect::<Vec<String>>() {
 		let r = BufReader::new(open_file(&filename));
 		println!("succesfully opened {}", filename);
@@ -27,4 +35,12 @@ fn main() {
 			println!("{}", line.unwrap());
 		}
 	}
+
+	// Demonstrate using ? operator (useless, since the path used
+	// to open the file does not get into the error message)
+	let greeting_file = BufReader::new(File::open("hello.txt")?);
+	println!("Last char of first line is: {}",
+		last_char_of_first_line(greeting_file).unwrap());
+
+	Ok(())
 }
