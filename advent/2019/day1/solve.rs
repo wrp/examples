1e48f2e58b1d538fd008f0bfee4d2b6595a6303a
William Pursell <william.r.pursell@gmail.com>
2023-07-11 19:31:29 -0600

    Trivial reformat

--- advent/2019/day1/solve.rs
+++ advent/2019/day1/solve.rs
@@ -2,11 +2,8 @@
 #![allow(non_camel_case_types)]
 
 use std::{
-	// env,
 	io::{self, Read},
-	// fs::File,
 	str::FromStr,
-	// path::Path,
 };
 
 
@@ -14,10 +11,7 @@ fn main() -> io::Result<()> {
 	let mut string = String::new();
 	io::stdin().read_to_string(&mut string)?;
 	let mut sum = 0;
-	for n in string
-		.lines()
-		.map(i32::from_str)
-	{
+	for n in string .lines() .map(i32::from_str) {
 		sum += n.unwrap() / 3 - 2;
 	}
 	println!("{:?}", sum);
