88911734b825ae268134450b6e7c9bb71af4cdd8
William Pursell <william.r.pursell@gmail.com>
2023-08-17 15:36:10 -0600

    Build config file, import version

--- cmake/basic-project/src/hello.c
+++ cmake/basic-project/src/hello.c
@@ -3,10 +3,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include "config.h"
 
 int
 main(int argc, char **argv)
 {
-	printf("hello, world!\n");
+	printf("hello, world: %s\n", VERSION);
 	return 0;
 }
