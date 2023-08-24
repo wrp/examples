4ba31b98a23d2d31132ff15e4db75fe2e6e9e376
William Pursell <william.r.pursell@gmail.com>
2023-08-24 13:18:07 -0600

    Add cxx/stringstream.cc

---
+++ cxx/stringstream.cc
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+
+
+
+void foo(const auto& s, int k, int j) {
+    std::cout << s->str() << ": " << k << ": " << j << '\n';
+}
+int main(int argc, char **argv)
+{
+    std::stringstream s;
+    s << "bar" << 3.85 << "blah" << 2;
+    // std::string ss = s.str();
+    foo(&s, 1, 3);
+	return 0;
+}
