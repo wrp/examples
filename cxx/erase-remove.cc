ea107a803b79cebe22dd215a341df7e8d0957b4e
William Pursell <william.r.pursell@gmail.com>
2023-09-01 13:20:03 -0600

    Demonstrate remove_if

--- cxx/erase-remove.cc
+++ cxx/erase-remove.cc
@@ -7,6 +7,14 @@
 using namespace std;
 void show_deque(const std::deque<int> & x, const string & msg);
 
+bool remove_and_log_6(int i) {
+	if( i == 6 ){
+		cout << "removing item\n";
+		return false;
+	}
+	return true;
+}
+
 int main()
 {
 	std::deque<int> x { 1, 2, 3 };
@@ -15,6 +23,12 @@ int main()
 	x.erase(std::remove(x.begin(), x.end(), 2));
 
 	show_deque(x, "After  removal");
+	x.push_front(6); x.push_back(6); x.push_front(8);
+	show_deque(x, "Before remove_if");
+
+	x.erase(std::remove_if(x.begin(), x.end(), remove_and_log_6));
+
+	show_deque(x, "After remove_if");
 }
 
 
