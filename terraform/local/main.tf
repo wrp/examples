06c2baa937ec7acdb897c50aa6052dc55d1f533b
William Pursell <william.r.pursell@gmail.com>
2023-10-26 13:01:48 -0600

    Revert "Apply tofu fmt"
    This reverts commit 1848ddffd3d2f1c2e03e38c12335d0bac443c750.
    
    The formatting applied by tofu is completely unreadable shyte.

--- terraform/local/main.tf
+++ terraform/local/main.tf
@@ -1,35 +1,35 @@
 
 
 terraform {
-  required_version = ">= 1.6, ~> 1.6.0, != 1.2"
-  # ~> is the pessimistic constraint which allows only the rightmost
-  #    number to differ
+	required_version = ">= 1.6, ~> 1.6.0, != 1.2"
+	# ~> is the pessimistic constraint which allows only the rightmost
+	#    number to differ
 }
 
 # Create 2 local files
-resource "local_file" "counted" {
-  count    = 2
-  content  = "Hello, World from file ${count.index}!\n"
-  filename = "${path.module}/hello.${count.index}"
+resource local_file counted {
+	count = 2
+	content = "Hello, World from file ${count.index}!\n"
+	filename = "${path.module}/hello.${count.index}"
 }
 
 # Create local files using indent removing heredoc
 # leading hard tabs are removed, other whitespace remains
 # There cannot be whitesapce between '<<-' and the label name
-resource "local_file" "iterated" {
-  for_each = toset(["foo", "bar"])
+resource local_file iterated {
+	for_each = toset(["foo", "bar"])
 
-  content  = <<-EOF
+	content = <<-EOF
 		Hello, Heredoc from ${each.key}
 	EOF
-  filename = "${path.module}/heredoc.${each.key}"
+	filename = "${path.module}/heredoc.${each.key}"
 }
 
 # Create a local file using non indent removing heredoc
 # leading whitespace is retained, label can be indented
-resource "local_file" "c" {
-  content  = <<EOF
+resource local_file c {
+	content = <<EOF
 		Hello, indented heredoc
 	EOF
-  filename = "${path.module}/nodent"
+	filename = "${path.module}/nodent"
 }
