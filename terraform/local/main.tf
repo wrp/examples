5f92b5726c68e609b2e26bb3d3fe18840ecd1745
William Pursell <william.r.pursell@gmail.com>
2023-10-26 08:39:19 -0600

    Add terraform/local/main.tf

---
+++ terraform/local/main.tf
@@ -0,0 +1,25 @@
+
+# Create local file
+resource "local_file" "hello" {
+	content = "Hello, World!\n"
+	filename = "${path.module}/hello"
+}
+
+# Create a local file using indent removing heredoc
+# leading hard tabs are removed, other whitespace remains
+# There cannot be whitesapce between '<<-' and the label name
+resource "local_file" "heredoc" {
+	content = <<-EOF
+		Hello, Heredoc
+	EOF
+	filename = "${path.module}/heredoc"
+}
+
+# Create a local file using non indent removing heredoc
+# leading whitespace is retained, label can be indented
+resource "local_file" "heredoc-indent" {
+	content = <<EOF
+		Hello, indented heredoc
+	EOF
+	filename = "${path.module}/nodent"
+}
