113dd9cc004c63bbc41e3efeb9e68e337bbf54d5
William Pursell <william.r.pursell@gmail.com>
2023-10-27 13:38:03 -0600

    Split variables into variables.tf
    Signed-off-by: William Pursell <william.r.pursell@gmail.com>

--- terraform/local/module/main.tf
+++ terraform/local/module/main.tf
@@ -1,10 +1,3 @@
-
-
-variable "names" {
-	type = list(string)
-	default = ["hello"]
-}
-
 resource local_file sub_module_file {
 	for_each = toset(var.names)
 	content = "Hello, World from file ${path.module}/${each.key}!\n"
---
+++ terraform/local/module/variables.tf
@@ -0,0 +1,4 @@
+variable "names" {
+	type = list(string)
+	default = ["hello"]
+}
