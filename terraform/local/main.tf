084b059b80afb73c455b50a004cc0c14000c3f40
William Pursell <william.r.pursell@gmail.com>
2023-10-27 13:38:55 -0600

    Split example of local-exec to its own file
    Signed-off-by: William Pursell <william.r.pursell@gmail.com>

--- terraform/local/main.tf
+++ terraform/local/main.tf
@@ -34,29 +34,6 @@ resource local_file c {
 	filename = "${path.module}/nodent"
 }
 
-# Create a local file dynamically
-resource local_file dynamic {
-	content = "hello, world!"
-	filename = "dyn"
-	provisioner "local-exec" {
-		command = "printf ' %s\n' '${self.content}' >> ${self.filename}"
-		on_failure = fail
-	}
-	# destroy provisioner does not execute.  Indeed, this file is not deleted
-	# by tofu destroy.  See https://github.com/hashicorp/terraform/issues/13549
-	provisioner "local-exec" {
-		when = destroy
-		command = "printf '${self.filename} destroyed!!\n' > output"
-	}
-	# provisionsers execute in the order specified here in the config file
-	provisioner "local-exec" {
-		when = create
-		command = "date >> ${self.filename}; false"
-		on_failure = continue
-
-	}
-}
-
 module "module" {
 	source = "./module"
 	names = ["foo", "bar", "baz"]
