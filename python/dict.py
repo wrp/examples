6968f179be67103b4c3f111d9f67e782cf4bd07e
William Pursell <william.r.pursell@gmail.com>
2023-11-16 08:42:42 -0700

    Add example slicing a dictionary
    Signed-off-by: William Pursell <william.r.pursell@gmail.com>

--- python/dict.py
+++ python/dict.py
@@ -22,3 +22,13 @@ mutate( dict(b) ) # To avoid mutation, pass a copy
 # iterate over a dictionary
 for k, v in b.items():
     print(f'b[{k}] = {v}')
+
+def slice_a_dict():
+    print("Demonstrate slicing a dictionay")
+    a = {'foo': 1, 'bar': 2, 'baz': 3}
+    keys = ['foo', 'baz']
+    print(f'a = {a}');
+    s = { k:a[k] for k in keys if k in a }
+    print(f'slice = {s}')
+
+slice_a_dict()
