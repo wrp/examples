58fc287bb519073000f650604ae5d041e9c1c637
William Pursell <william.r.pursell@gmail.com>
2023-11-22 13:06:29 -0700

    Add zig/src/primitive_types.zig

---
+++ zig/src/primitive_types.zig
@@ -0,0 +1,33 @@
+//! Example using all the primitive types
+
+pub fn main() void {
+	// integers
+	const a: i8 = 1;
+	const b: u8 = 1;
+	const c: i16 = 1;
+	const d: u16 = 1;
+	const e: i32 = 1;
+	const f: u32 = 1;
+	const g: i64 = 1;
+	const h: u64 = 1;
+	const i: i128 = 1;
+	const j: u128 = 1;
+	const k: isize = 1; // pointer sized integer
+	const l: usize = 1; // unsigned pointer sized integer
+	const m: c_char = 1;
+
+
+_ = a;
+_ = b;
+_ = c;
+_ = d;
+_ = e;
+_ = f;
+_ = g;
+_ = h;
+_ = i;
+_ = j;
+_ = k;
+_ = l;
+_ = m;
+}
