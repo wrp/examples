d2f7b68c44af7e99b10909c4b42f3087551fa10b
William Pursell <william.r.pursell@gmail.com>
2023-11-22 09:04:31 -0700

    Add zig/src/comptime.zig

---
+++ zig/src/comptime.zig
@@ -0,0 +1,31 @@
+
+
+
+const std = @import("std");
+
+
+pub fn main() !void {
+    comptime var x = 3;
+    comptime var y = squareComptime(x);
+    std.debug.print("{d}\n", .{y});
+}
+
+
+pub fn squareComptime(comptime x: comptime_int) comptime_int {
+    return x * x;
+}
+
+pub inline fn minComptime(comptime a: comptime_int, comptime b: comptime_int) comptime_int {
+    return if (a < b) a else b;
+}
+
+test "squareComptime" {
+    try std.testing.expectEqual(9, squareComptime(3));
+    try std.testing.expectEqual(25, squareComptime(3 + 2));
+}
+
+test "minComptime" {
+    try std.testing.expectEqual(minComptime(0, 0), 0);
+    try std.testing.expectEqual(minComptime(30000000, 30000000000), 30000000);
+    try std.testing.expectEqual(minComptime(-10, -3), -10);
+}
