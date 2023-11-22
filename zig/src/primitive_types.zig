7093ca770afbe39996b45d742382c2c471de48cd
William Pursell <william.r.pursell@gmail.com>
2023-11-22 13:51:33 -0700

    Add more primitive types (incomplete)
    Signed-off-by: William Pursell <william.r.pursell@gmail.com>

--- zig/src/primitive_types.zig
+++ zig/src/primitive_types.zig
@@ -16,5 +16,22 @@ pub fn main() void {
 	_ = @as(isize, 1); // pointer sized integer
 	_ = @as(usize, 1); // unsigned pointer sized integer
 	_ = @as(c_char, 1);
+	_ = @as(c_short, 1);
+	_ = @as(c_ushort, 1);
+	_ = @as(c_int, 1);
+	_ = @as(c_uint, 1);
+	_ = @as(c_long, 1);
+	_ = @as(c_ulong, 1);
+	_ = @as(c_longlong, 1);
+	_ = @as(c_ulonglong, 1);
+	_ = @as(c_longdouble, 1);
+
+	_ = @as(f16, 1);
+	_ = @as(f32, 1);
+	_ = @as(f64, 1);
+	_ = @as(f80, 1);
+	_ = @as(f128, 1);
+
+	_ = @as(bool, true);
 
 }
