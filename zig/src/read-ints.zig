2323a42f5f7278fc7b33271ddbea61761f0aa9be
William Pursell <william.r.pursell@gmail.com>
2023-11-27 08:53:12 -0700

    Demonstrate error handling
    In this case, we catch the error raised by the
    try r.readUntilDelimeterOrEof when an input
    token does not fit in the now very small buf.
    Previously, doing this would generate a stack trace
    when the "try" raised an uncaught exception.
    
    Signed-off-by: William Pursell <william.r.pursell@gmail.com>

--- zig/src/read-ints.zig
+++ zig/src/read-ints.zig
@@ -1,20 +1,30 @@
 //! Read a stream of integers
 
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+const stderr = std.io.getStdErr().writer();
 
-pub fn main() !void {
-	const stdout = std.io.getStdOut().writer();
-	const stderr = std.io.getStdErr().writer();
+pub fn read_stream(r: anytype) !void {
+	var buf: [8]u8 = undefined;
 
-	var unbuffered_stdin = std.io.getStdIn().reader();
-	var stdin = std.io.bufferedReader(unbuffered_stdin);
-	var r = stdin.reader();
-	var buf: [1000]u8 = undefined;
 	while (try r.readUntilDelimiterOrEof(&buf, ' ')) |line| {
 		if (std.fmt.parseInt(i32, line, 10)) |value| {
 			try stdout.print("{d}\n", .{value});
 		} else |err| {
 			try stderr.print("Invalid: {}\n", .{err});
 		}
+	}
+}
+
+pub fn main() !void {
+
+	var unbuffered_stdin = std.io.getStdIn().reader();
+	var stdin = std.io.bufferedReader(unbuffered_stdin);
+	var r = stdin.reader();
+
+	if(read_stream(r)){
+
+	} else |err| {
+		try stderr.print("Error: {}\n", .{err});
 	}
 }
