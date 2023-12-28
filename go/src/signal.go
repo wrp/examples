3dcccd2887338eae83dec5000e2d31a464f5bea1
William Pursell <william.r.pursell@gmail.com>
2023-12-28 12:53:50 -0700

    Outfactor handle_interrupts
    Declare intention with function names.  Valid in any language

--- go/src/signal.go
+++ go/src/signal.go
@@ -8,17 +8,21 @@ import (
 	"sync"
 )
 
-func main() {
-	quit := make(chan os.Signal)
-	signal.Notify(quit, os.Interrupt)
-	var wg sync.WaitGroup
-
+func handle_interrupts(wg * sync.WaitGroup, quit chan os.Signal){
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		<-quit
 		log.Println("received interrupt signal")
 	}()
+}
+
+
+func main() {
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	var wg sync.WaitGroup
+	handle_interrupts(&wg, quit)
 
 	wg.Wait()
 }
