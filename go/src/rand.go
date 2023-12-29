7ae1ab48af58f34d83f70166a758fa3563a83629
William Pursell <william.r.pursell@gmail.com>
2023-12-29 06:50:44 -0700

    Discuss integer rep in golang

--- go/src/rand.go
+++ go/src/rand.go
@@ -10,4 +10,10 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	fmt.Println("A random int in [0, 5): ", rand.Int31n(5))
+
+	// Int31n(n) returns a random 31-bit integer as an int32 in
+	// the half-open interval [0,n)
+	// AFAICT, the language mandates that integers be stored in
+	// 2s complement, or at least the documentation certainly
+	// seems to make that assumption
 }
