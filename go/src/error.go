91e3f06de7bcfafa97a9fb2713ffef4a86325b5e
William Pursell <william.r.pursell@gmail.com>
2024-01-04 09:01:14 -0700

    Use pointer receiver

--- go/src/error.go
+++ go/src/error.go
@@ -10,9 +10,10 @@ import (
 )
 
 
-func add(a, b float64) (c float64, err MyError) {
+func add(a, b float64) (c float64, err *MyError) {
 	// Return the sum of a and b if both are even.  Else throw an error
 	// (This is a silly exercise to demonstrate errors)
+	err = &MyError{}
 
 	if (int64(math.Floor(a)) % 2) != 0 || (int64(math.Floor(b)) % 2) != 0 {
 		err.error = fmt.Errorf("Oops, one operand was odd");
@@ -29,7 +30,7 @@ type MyError struct {
 	error
 }
 
-func (e MyError) Error() string {
+func (e *MyError) Error() string {
 	return fmt.Sprintf("%s: %d", e.error, e.int)
 }
 
@@ -37,7 +38,7 @@ func main() {
 
 	var a, b float64;
 //	err  := fmt.Errorf("stuv")
-	var err MyError
+	var err *MyError = &MyError{}
 
 	err.error = errors.New("")
 
@@ -46,14 +47,14 @@ func main() {
 	if len(os.Args) > 1 {  // keyword: argv
 		var e error
 		a, e = strconv.ParseFloat(os.Args[1], 64)
-		err = e.(MyError)
+		err = e.(*MyError)
 	} else {
 		a = float64(1)
 	}
 	if len(os.Args) > 2 {
 		var e error
 		b, e = strconv.ParseFloat(os.Args[2], 64)
-		err = e.(MyError)
+		err = e.(*MyError)
 	} else {
 		b = float64(1)
 	}
