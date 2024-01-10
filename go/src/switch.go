dd1d7b166be98e8dcc7094c3cd692831a736cc79
William Pursell <william.r.pursell@gmail.com>
2024-01-10 08:49:11 -0700

    Trivial cleanup of switch example

--- go/src/switch.go
+++ go/src/switch.go
@@ -3,22 +3,21 @@ package main
 
 import "fmt"
 
+func foo(x int) int {
+	fmt.Println("foo() called with arg ", x)
+	return x;
+}
+
 func main() {
-	n := 4
-	switch n {
-	case 0:
-		fmt.Println("is zero")
-		fallthrough
-	case 1:
-		fmt.Println("is <= 1")
-		fallthrough
+	switch 4 {
+	case foo(1):
+		fmt.Println(1)
 	case 3 - 1:
 		fmt.Println("is <= 2")
-	case 4, 5, 6:
+	case foo(2), foo(4), 5, 6:
 		fmt.Println("is = 4, 5, or 6")
 		fallthrough
-	case 7:
-		fallthrough
+	case 7, foo(5):
 	case 8:
 		fmt.Println("is <= 8")
 		fallthrough
@@ -27,12 +26,15 @@ func main() {
 	}
 
 	switch false {
-	case 1 < 2: fmt.Println("will NOT print")
-	case 1 < -5: fmt.Println("will print ")
+	case 1 < 2: fmt.Println("will NOT print, because expr is true")
+	case 1 < 0: fmt.Println("will print, because expr is false ")
+	case 1 < 0: fmt.Println("will NOT print; no fallthru")
 	}
 
 	switch { // defaults to "switch true"
+	case 1 < 0: fmt.Println("will NOT print ")
+	case false: fmt.Println("will NOT print ")
 	case 1 < 2: fmt.Println("Does print")
-	case 1 < -5: fmt.Println("will print ")
+	case 1 < 0: fmt.Println("will NOT print ")
 	}
 }
