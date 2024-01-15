f84a7948b95db886ff11471020a964696d5b3fef
William Pursell <william.r.pursell@gmail.com>
2024-01-15 06:52:26 -0700

    Add zig/src/linked-list.zig

---
+++ zig/src/linked-list.zig
@@ -0,0 +1,41 @@
+
+
+const std = @import("std");
+
+
+pub fn main() !void {
+	var node = LinkedList(i32).Node {
+		.prev = null,
+		.next = null,
+		.data = 1234,
+	};
+
+	const list = LinkedList(i32) {
+		.first = &node,
+		.last = &node,
+		.len = 1,
+	};
+
+	const stdout = std.io.getStdOut().writer();
+	try stdout.print("{?}\n", .{list.first});
+	try stdout.print("{?}\n", .{list.last});
+	try stdout.print("{?}\n", .{usize});
+	try stdout.print("{?}\n", .{list});
+	if (list.first) |v| {
+		try stdout.print("Value is: {d}\n", .{v.data});
+	}
+}
+
+fn LinkedList(comptime T: type) type {
+	return struct {
+		pub const Node = struct {
+			prev: ?*Node,
+			next: ?*Node,
+			data: T,
+		};
+
+		first: ?*Node,
+		last:  ?*Node,
+		len:   usize,
+	};
+}
