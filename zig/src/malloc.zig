61fd491cb177dbc6eec364a5e4345a60ce9596a4
William Pursell <william.r.pursell@gmail.com>
2024-01-17 09:01:25 -0700

    Fix indentation

--- zig/src/malloc.zig
+++ zig/src/malloc.zig
@@ -1,14 +1,14 @@
 const std = @import("std");
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
+	var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+	defer _ = gpa.deinit();
 
-    const allocator = gpa.allocator();
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
+	const allocator = gpa.allocator();
+	const args = try std.process.argsAlloc(allocator);
+	defer std.process.argsFree(allocator, args);
 
-    const x = try std.fmt.parseInt(i32, args[1], 10);
-    const y = x * x;
-    std.debug.print("{d}\n", .{y});
+	const x = try std.fmt.parseInt(i32, args[1], 10);
+	const y = x * x;
+	std.debug.print("{d}\n", .{y});
 }
