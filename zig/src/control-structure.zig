53bbbad61b13966d7d51f167a14b70abf95bf468
William Pursell <william.r.pursell@gmail.com>
2024-01-19 11:38:22 -0700

    Add zig/src/control-structure.zig

---
+++ zig/src/control-structure.zig
@@ -0,0 +1,62 @@
+// Simple demonstrations of basic control structures
+
+const std = @import("std");
+
+fn for_loops(stdout: std.fs.File.Writer) !void {
+	const names = [_][]const u8{"Alice", "Bob"};  // Initialize array of strings
+
+	// Basic usage
+	for(0..3) |i| {
+		const delim = if(i==2) "\n" else ", ";
+		try stdout.print("{d}{s}", .{i, delim});
+	}
+
+	// Iterate over an array
+	for(names) |n| {
+		try stdout.print("{s}\n", .{n});
+	}
+
+	// Enumerate an array
+	for(0.., names) |i, n| {
+		const delim = if(i==1) "\n" else ", ";
+		try stdout.print("{s}{s}", .{n, delim});
+	}
+
+	// Else clause is only executed on non-breaking exit
+	var a: usize = undefined;
+	for(0..3) |i| {
+		a = i;
+	} else {
+		// In the else clause, captured i is not available
+		try stdout.print("in else: a = {d}\n", .{a});
+	}
+}
+
+const ParityError = error {
+	IsOdd,
+	IsEven,
+};
+
+fn is_odd(x: i32) !bool {
+	if (@rem(x, 2) == 0) {
+		return ParityError.IsEven;
+	}
+	return true;
+}
+
+fn conditionals(stdout: std.fs.File.Writer) !void {
+	// capture clause on the if captures the non-error value
+	if (is_odd(8)) |a| {
+		try stdout.print("Cannot happen, that {any} is true\n", .{a});
+	// capture on the else captures the error
+	} else |e| {
+		try stdout.print("8 is not odd: {}\n", .{e});
+	}
+}
+
+
+pub fn main() !void {
+	const stdout = std.io.getStdOut().writer();
+	try for_loops(stdout);
+	try conditionals(stdout);
+}
