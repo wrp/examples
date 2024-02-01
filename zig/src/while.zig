8bbbb53c042925c4a497114efadbe8c83da0e67d
William Pursell <william.r.pursell@gmail.com>
2024-01-31 20:02:29 -0700

    Demonstrate capturing an error in a while loop

--- zig/src/while.zig
+++ zig/src/while.zig
@@ -9,12 +9,29 @@ fn count() ?i32 {
 	return if (state.counter == 3) null else state.counter;
 }
 
+const CountError = error {
+	Count3,
+};
+fn counter() !i32 {
+	const state = struct { var counter: i32 = 0; };
+	state.counter += 1;
+	return if (state.counter == 3) CountError.Count3 else state.counter;
+}
+
 pub fn main() !void {
 	const stdout = std.io.getStdOut().writer();
+	const stderr = std.io.getStdErr().writer();
 
 	// Capture the value of an optional
 	while (count()) |n| {
 		try stdout.print("{d}\n", .{n});
+	}
+
+	// Capture the value of an error
+	while (counter()) |n| {
+		try stdout.print("{d}\n", .{n});
+	} else |err| {
+		try stderr.print("{}\n", .{err});
 	}
 
 	// Demonstrate a continue expression
