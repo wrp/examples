95c8975b633ab19a9c065ba2fc9c7ae0b7246585
William Pursell <william.r.pursell@gmail.com>
2024-03-10 09:21:16 -0600

    Add some casting examples

--- zig/src/cast.zig
+++ zig/src/cast.zig
@@ -5,22 +5,27 @@ const std = @import("std");
 pub fn main() !void {
 	const stdout = std.io.getStdOut().writer();
 
-	// Iterator type is usize
-	for(0..3) |i| {
-		try stdout.print("{d},", .{i});
-	}
+	for(0..1) |i| { try stdout.print("for(0..1) type is {}\n", .{ @TypeOf(i) }); } // usize
 
 	// Narrowing cast
-	for(0..3) |i| {
-		const u = @as(u8, @truncate(i));
-		try stdout.print("{d},", .{u});
+	for(15..18) |us| {
+		const u = @as(u4, @truncate(us));
+		try stdout.print("@as(u4, @truncate({d}) -> {b:0>4}\t", .{us, u});
+
+		// const i = @as(i4, @truncate(us));  comptime error! signedness
+		const i = @as(i4,
+				@truncate(
+					@as(i64, @bitCast(us) // comptime error if usize not 64 bits
+				)
+			));
+		try stdout.print("i4: {d}\n", .{i});
 	}
 
-	const arr = [4]i8{1, 2, 3, 4};
-	for(arr) |i| {
-		try stdout.print("{d},", .{i});
+	for([_]i8{-128, -127, -8, -7, -1, 1, 7, 127}) |i| {
+		const narrow = @as(i4, @truncate(i));
+		const u = @as(u8, @bitCast(i));
+		try stdout.print("i8: {0}({1b:0>8} -> i4: {2}\n", .{ i, u, narrow });
 	}
-	try stdout.print("EOL\n", .{});
 
 	for([_]i4{-8, -2, -1, 7}) |i| {
 		const sm = @as(i3, @truncate(i));
