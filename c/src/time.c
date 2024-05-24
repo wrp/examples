006ad4ff618624ec15ab59e8db73b15decf8de69
William Pursell <william.r.pursell@gmail.com>
2024-05-24 07:11:19 -0600

    Demonstrate gettimeofday and timersub

--- c/src/time.c
+++ c/src/time.c
@@ -1,12 +1,30 @@
 
+#include <assert.h>
 #include <time.h>
 #include <stdio.h>
+#include <sys/time.h>
+
 
 int
 main(void)
 {
+	struct timeval start;
+	int s = gettimeofday(&start, NULL);
+
 	time_t t;
 	time(&t);
-	printf("%s\n", ctime(&t));
+	char *d = ctime(&t);
+	printf("%s", d);  /* value returned has newline */
+	assert(d[24] == '\n');
+
+	if (s == 0) {
+		struct timeval end, diff;
+
+		if( 0 == gettimeofday(&end, NULL)) {
+			timersub(&end, &start, &diff);
+			printf("Completed in %ld.%06lds\n",
+				(long)diff.tv_sec, (long)diff.tv_usec);
+		}
+	}
 	return 0;
 }
