ee4fb37595f8c2e59efc2258d9b146eef8979de7
William Pursell <william.r.pursell@gmail.com>
2024-10-01 12:16:21 -0600

    Add getchar like example

--- rust/src/bin/stdin.rs
+++ rust/src/bin/stdin.rs
@@ -1,9 +1,9 @@
 // Read lines of input
 
-use std::io::{self, BufRead};
+use std::{ io::{self, BufRead, prelude::* } };
 
 fn main() -> io::Result<()> {
-	let stdin = io::stdin();
+	let mut stdin = io::stdin();
 	let mut line = String::new();
 
 	// Read one line, demonstrating one technique
@@ -16,6 +16,16 @@ fn main() -> io::Result<()> {
 	stdin.lock().read_line(&mut line).unwrap();
 	print!("2nd line: {}", line);
 
+	// Read one line, one char at a time
+	print!("3rd line: (char at a time): '");
+	let mut c;
+	while { c = getchar(&mut stdin, 1); c[0] != '\n' as u8 } {
+		print!("{}", c[0] as char);
+	}
+	println!("'");
+
+
+	// Read the remainder, by line
 	let mut lines = stdin.lock().lines();
 	while let Some(line) = lines.next() {
 		println!("{}", line.unwrap());
@@ -23,6 +33,18 @@ fn main() -> io::Result<()> {
 
 	Ok(())
 }
+
+fn getchar<R>(reader: R, bytes_to_read: u64) -> Vec<u8>
+where
+	R: Read,
+{
+	let mut buf = vec![];
+	let mut chunk = reader.take(bytes_to_read);
+	let n = chunk.read_to_end(&mut buf).expect("Unexpected EOF");
+	assert_eq!(bytes_to_read as usize, n);
+	buf
+}
+
 
 
 // Read newline separated integers
