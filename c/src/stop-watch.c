cce64d197365eb7b6e2b52922d80fc29192486b3
William Pursell <william.r.pursell@gmail.com>
2025-01-03 09:27:11 -0700

    Implement intervals
    SIGINT will now print a lap time, quit with SIGQUIT (^-\)
    Probably want to add a usage statement somewhere to mention
    SIGQUIT, since no-one every remembers ctrl-\

--- c/src/stop-watch.c
+++ c/src/stop-watch.c
@@ -19,25 +19,42 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-sig_atomic_t stop;
+sig_atomic_t stop, lap;
 static void get_time(struct timeval *tp);
 static void handle(int sig, siginfo_t *i, void *v);
 static void establish_handlers(void);
 static void print_delta(struct timeval begin, struct timeval end);
 
+static void
+show_lap(struct timeval now, struct timeval *prev)
+{
+	if (lap) {
+		fputs("   ", stdout);
+		print_delta(*prev, now);
+		puts("         ");
+		*prev = now;
+		lap = 0;
+	} else {
+		fputs("                                 ", stdout);
+	}
+
+}
+
 
 int
 main(void)
 {
-	struct timeval start, now;
+	struct timeval start, now, prev;
 
 	get_time(&start);
+	prev = start;
 	establish_handlers();
 
 	while(!stop) {
 		get_time(&now);
 		putchar('\r');
 		print_delta(start, now);
+		show_lap(now, &prev);
 		fflush(stdout);
 		pause();
 	}
@@ -63,7 +80,8 @@ handle(int sig, siginfo_t *i, void *v)
 	(void)i;
 	(void)v;
 	switch(sig) {
-	case SIGINT: stop = 1; break;
+	case SIGINT: lap = 1; break;
+	case SIGQUIT: stop = 1; break;
 	}
 }
 
@@ -91,12 +109,11 @@ establish_handlers(void)
 static void
 print_delta(struct timeval begin, struct timeval end)
 {
-	const char *spaces = "                                 ";
 	struct timeval delta;
 	timersub(&end, &begin, &delta);
 	unsigned minutes = delta.tv_sec / 60;
 	unsigned seconds = delta.tv_sec % 60;
 	char usec[4];
 	snprintf(usec, sizeof usec, "%u", delta.tv_usec);
-	printf("%0um%02u.%ss%s", minutes, seconds, usec, spaces);
+	printf("%0um%02u.%ss", minutes, seconds, usec);
 }
