c73b2384ee7f0825082f44e3591883003d1db882
William Pursell <william.r.pursell@gmail.com>
2025-01-04 07:34:20 -0700

    Trivial reformat
    Grrr.  Today, I am once again liking the style "if( ... ){"
    instead of "if (...) {"  The mind is a fickle thing.

--- c/src/pyramid.c
+++ c/src/pyramid.c
@@ -6,15 +6,16 @@
 int
 main(int argc, char **argv)
 {
-	int size = argc > 1 ? strtol(argv[1],NULL,10) : 5;
-	if( size > 9 || size < 0) {
+	unsigned size = argc > 1 ? strtoul(argv[1],NULL,10) : 5;
+	if( size > 9 || size == 0 ){
 		fprintf(stderr, "Invalid size\n");
 		return EXIT_FAILURE;
 	}
-	for(int line = 0; line <= size; line++) {
+	for( unsigned line = 0; line <= size; line += 1 ){
 		char template[]="9876543210123456789";
 		template[10 + line] = '\0';
-		if(puts(memset(template + 9 - size, ' ', size - line)) == EOF) {
+		char *end = template + 9;
+		if( puts(memset(end - size, ' ', size - line)) == EOF ){
 			perror("puts");
 			return EXIT_FAILURE;
 		}
