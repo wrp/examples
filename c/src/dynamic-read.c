527b633ef51ecb113492c069a0935ea1950de8b3
William Pursell <william.r.pursell@gmail.com>
2025-01-05 16:08:20 -0700

    Factor out get_line()
    Baby steps.

--- c/src/dynamic-read.c
+++ c/src/dynamic-read.c
@@ -21,6 +21,7 @@ struct read_buf {
 	char *s;
 	char *prev;  /* start of data from previous read */
 	char *end;  /* one past last char read from input */
+	char *eol;  /* A newline, or one past valid data */
 };
 
 
@@ -34,44 +35,53 @@ init_read_buf(struct read_buf *b)
 	** the unused values of the previous read. */
 }
 
+
+static int
+get_lines(int fd, struct read_buf *b)
+{
+	ssize_t rc = read(fd, b->s, BUFSIZ);
+	if( rc < 1 ){
+		return (int)rc;
+	}
+	b->end = b->s + rc;
+
+	if( (b->eol = findchr(b->s, b->end, '\n')) == b->end ) {
+		/* No newlines found in the last read.  Read more. */
+		if( b->end > b->data + b->size ){
+			ptrdiff_t p_off = b->prev - b->data;
+			b->size += BUFSIZ;
+			b->data = xrealloc(b->data, BUFSIZ + b->size, sizeof *b->data, &b->end);
+			b->eol = b->end;
+			b->prev = b->data + p_off;
+		}
+		b->s = b->end;
+		assert( b->s <= b->data + b->size );
+		return get_lines(fd, b);  /* Is tail recursion any better than a goto? */
+	}
+	b->s = b->prev;
+	return 1;
+}
+
+
 int
 main(int argc, char **argv)
 {
-	ssize_t rc;
-
+	int rc;
 	struct read_buf b;
 
 	init_read_buf(&b);
-
 	int fd = argc > 1 ? xopen(argv[1], O_RDONLY) : STDIN_FILENO;
 
-	while(( rc = read(fd, b.s, BUFSIZ)) > 0 ){
-		char *eol; /* A newline, or one past valid data */
-		b.end = b.s + rc;
-
-		if( (eol = findchr(b.s, b.end, '\n')) == b.end ) {
-			/* No newlines found in the last read.  Read more. */
-			if( b.end > b.data + b.size ){
-				ptrdiff_t p_off = b.prev - b.data;
-				b.size += BUFSIZ;
-				b.data = xrealloc(b.data, BUFSIZ + b.size, sizeof *b.data, &b.end);
-				eol = b.end;
-				b.prev = b.data + p_off;
-			}
-			b.s = b.end;
-			assert( b.s <= b.data + b.size );
-			continue;
-		}
-		b.s = b.prev;
+	while(( rc = get_lines(fd, &b)) > 0 ){
 		do {
-			assert( *eol == '\n' );
-			assert( eol < b.end );
-			reverse( b.s, eol - 1 );
-			b.s = eol + 1;
+			assert( *b.eol == '\n' );
+			assert( b.eol < b.end );
+			reverse( b.s, b.eol - 1 );
+			b.s = b.eol + 1;
 			assert( b.s <= b.end );
-		} while( (eol = findchr(b.s, b.end, '\n')) < b.end );
-		assert( eol == b.end );
-		assert( eol[-1] != '\n' || b.s == b.end );
+		} while( (b.eol = findchr(b.s, b.end, '\n')) < b.end );
+		assert( b.eol == b.end );
+		assert( b.eol[-1] != '\n' || b.s == b.end );
 
 		fwrite(b.prev, 1, b.s - b.prev, stdout);
 		b.prev = b.data + BUFSIZ - (b.end - b.s);
