c944778dfcd55d2d1a3d67ba6eacfbbc827bfa13
William Pursell <william.r.pursell@gmail.com>
2025-01-15 15:44:33 -0700

    Use SA_NOCLDSTOP

--- signals/sigchld.c
+++ signals/sigchld.c
@@ -16,7 +16,7 @@
 
 static void child(int x);
 static pid_t make_child(int);
-static void establish_handler(void);
+static void establish_handler(int);
 static void check_status(pid_t p);
 
 
@@ -32,20 +32,25 @@ int
 main(int argc, char **argv)
 {
 	pid_t p[3];
-	establish_handler();
+	establish_handler(0);
 	p[0] = make_child(0);
 	p[1] = make_child(1);
 	p[2] = make_child(2);
 
 	check_status(p[0]);
 
+	printf("Stopping %d\n", (int)p[1]);
+	kill(p[1], SIGSTOP);
+	printf("continuing %d\n", (int)p[1]);
+	kill(p[1], SIGCONT);
 	printf("Terminating %d\n", (int)p[1]);
 	kill(p[1], SIGTERM);
 	check_status(p[1]);
 
+	printf("Re-establising handler with SA_NOCLDSTOP\n");
+	establish_handler(1);
 	printf("Stopping %d\n", (int)p[2]);
 	kill(p[2], SIGSTOP);
-
 	printf("continuing %d\n", (int)p[2]);
 	kill(p[2], SIGCONT);
 
@@ -102,13 +107,13 @@ child(int x)
 }
 
 static void
-establish_handler(void)
+establish_handler(int x)
 {
 	struct sigaction act;
 
 	memset(&act, 0, sizeof act);
 	act.sa_sigaction = handle;
-	act.sa_flags = 0; /* SA_NOCLDSTOP */
+	act.sa_flags = x ? SA_NOCLDSTOP : 0;
 
 	/* SA_NOCLDSTOP    If this bit is set when installing a
 	** catching function for the SIGCHLD signal, the SIGCHLD
