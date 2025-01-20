d080adc29133f08767ad68f92051d76426ebfa00
William Pursell <william.r.pursell@gmail.com>
2025-01-20 15:00:20 -0700

    Add rust/src/bin/getchar.rs

---
+++ rust/src/bin/getchar.rs
@@ -0,0 +1,36 @@
+// Demonstrate reading n bytes from a reader
+
+#![allow(non_camel_case_types)]
+
+
+use std::{
+	io::{prelude::*, BufReader},
+	str,
+};
+
+fn getchar<R>(reader: R, bytes_to_read: u64) -> Vec<u8>
+where
+	R: Read,
+{
+	let mut buf = vec![];
+	let mut chunk = reader.take(bytes_to_read);
+	let n = chunk.read_to_end(&mut buf).expect("Unexpected EOF");
+	assert_eq!(bytes_to_read as usize, n);
+	buf
+}
+
+
+fn main() {
+	let input_data = b"hello world";
+	let mut reader = BufReader::new(&input_data[..]);
+
+	let first = getchar(&mut reader, 5);
+	let _ = getchar(&mut reader, 1);
+	let second = getchar(&mut reader, 5);
+
+	println!(
+		"{}, {}",
+		str::from_utf8(&first).unwrap(),
+		str::from_utf8(&second).unwrap()
+	);
+}
