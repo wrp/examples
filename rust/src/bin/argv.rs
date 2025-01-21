d15b04120803d2375506759d8a5fc6501de77108
William Pursell <william.r.pursell@gmail.com>
2025-01-21 06:53:19 -0700

    Add keyword argv
    git grep should find this file!

--- rust/src/bin/argv.rs
+++ rust/src/bin/argv.rs
@@ -1,17 +1,18 @@
+// keywords: argv
 
 use std::env;
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
+	let argv: Vec<String> = env::args().collect();
 
 	// The first argument is the path that was used to call the program.
-	println!("My path is {}.", args[0]);
+	println!("My path is {}.", argv[0]);
 
 	// The rest of the arguments are the passed command line parameters.
-	println!("I got {:?} arguments: {:?}.", args.len() - 1, &args[1..]);
+	println!("I got {:?} arguments: {:?}.", argv.len() - 1, &argv[1..]);
 
-	// Get integer argument from first 2 args, defaulting to 5
-	let c1 = args.get(1).unwrap_or(&5.to_string()).parse::<u32>().unwrap_or(5);
-	let c2 = match args.get(2) { Some(d) => d.parse::<i32>().unwrap_or(5), None => 5 };
+	// Get integer argument from first 2 arguments, defaulting to 5
+	let c1 = argv.get(1).unwrap_or(&5.to_string()).parse::<u32>().unwrap_or(5);
+	let c2 = match argv.get(2) { Some(d) => d.parse::<i32>().unwrap_or(5), None => 5 };
 	println!("args = {}, {}", c1, c2);
 }
