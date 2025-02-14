f079406fa101e59ba055f8025f996075df7ebbd2
William Pursell <william.r.pursell@gmail.com>
2025-02-14 10:57:08 -0700

    Cleanup class example (add assertions)
    lang: python

--- python/Foo.py
+++ python/Foo.py
@@ -1,20 +1,24 @@
 # Class Foo used in script 'class'
 
-def initialize(x):
-    print(f'initializing{x}')
-    return x
-
-
 
 class Foo:
-    bar = initialize(5)  # Initialization of class attribute happens only once
+    bar = 0  # Initialization of class attribute happens only once
+
+
+def __init(self, x, **kwargs):
+        self.x = x
+        self.bar += x  # Does not modify class instance
+        self.map = {}
+        assert Foo.bar == 0
 
 def __meth(self: Foo):
     print(f'A do nothing method on {self}')
 
 def __new(cls: Foo, *args, **kwargs):
     print(f'Foo.__new__ called with {args} {kwargs}')
-    return super(Foo, cls).__new__(cls)
+    s = super(Foo, cls)
+    assert s.__self_class__.bar == 0
+    return s.__new__(cls)
 
 def __str(self: Foo) -> str:
     rv = f"x = {self.x}, bar = {self.bar}"
@@ -23,5 +27,6 @@ def __str(self: Foo) -> str:
     return rv
 
 Foo.__new__ = __new
+Foo.__init__ =__init
 Foo.__str__ = __str
 Foo.meth = __meth
