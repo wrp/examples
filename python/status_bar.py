9dbb0a310071bd03eacc44dcea78ab991d56367a
William Pursell <william.r.pursell@gmail.com>
2025-04-01 14:33:56 -0600

    Provide display() API
    lang: python

--- python/status_bar.py
+++ python/status_bar.py
@@ -10,18 +10,26 @@ class status_bar:
     def run(self, message='', interval=.3):
         self.message = message
         self.interval = interval
-        a = threading.Thread(target=self.show_status)
+        self.spinner = '-\|/'
+        self.idx = 0
+
+        a = threading.Thread(target=self._main_loop)
         a.daemon = True
         a.start()
         return self
 
-    def show_status(self):
-        spinner = '-\|/'
-        idx = 0
-        while True:
-            m = datetime.now().strftime(f'%T {spinner[idx]}{spinner[-idx]}')
+    def display(self, message):
+        self.message = message
+
+    def _show_message(self):
+            c = self.spinner[self.idx] + self.spinner[-self.idx]
+            m = datetime.now().strftime(f'%T {c}')
             end = f'\033[K\r'
-            idx = (idx+1) % 4
+            self.idx = (self.idx+1) % 4
             print(f'{m}: ', end='')
             print(self.message, end=end, flush=True)
+
+    def _main_loop(self):
+        while True:
+            self._show_message()
             time.sleep(self.interval)
