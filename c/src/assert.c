2c8ac67c49d49a4bb68a95635b5ff760d5401577
William Pursell <william.r.pursell@gmail.com>
2025-09-20 09:30:43 -0600

    Demonstrate adding message to runtime assertion
    type: c

--- c/src/assert.c
+++ c/src/assert.c
@@ -1,5 +1,6 @@
 
 
+#include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -7,6 +8,8 @@
 int
 main(int argc, char **argv)
 {
+	int x = 5;
+	assert(x == 4 && "Unexpected runtime value");
 	_Static_assert(1, "This is how to do a compile time assertion");
 	return 0;
 }
