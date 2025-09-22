121f64346e0e050210f6dfa2f90f58ae3e2f9137
William Pursell <william.r.pursell@gmail.com>
2025-09-22 09:15:36 -0600

    Use move
    type: curses

--- c/src/build-errors/ncurses.c
+++ c/src/build-errors/ncurses.c
@@ -8,6 +8,9 @@ main(void)
 {
 	int r;
 	int w = 0;
+	int x = 0, y = 0;
+	int *this = &x;
+
 	unsetenv("COLUMNS");
 	unsetenv("LINES");
 	if( initscr() == NULL ) {
@@ -16,19 +19,30 @@ main(void)
 	printw("Type some characters, 'q' to quit\n");
 	while( (w = getch()) != ERR ) {
 		if (w == KEY_RESIZE) {
-			int y, x;
-			getmaxyx(stdscr, y, x);
-			printw("y = %d; x = %d\n", y, x);
+			int a, b;
+			getmaxyx(stdscr, a, b);
+			printw("y = %d; x = %d\n", a, b);
 			continue;
 		}
-		if (w == 'q') {
+		switch(w){
+		case 'q': goto fini;
+		case 'g':
+			printw("Moving to (%d, %d)", y, x);
+			move(y, x);
+			/* fall thru */
+		case ',':
+			this = this == &x ? &y : &x;
+			*this = 0;
 			break;
-		}
-		if (w >= 'a' && w <= 'z') {
-			printw("%c", toupper(w));
+		case '0': case '1': case '2': case '3': case '4':
+		case '5': case '6': case '7': case '8': case '9':
+			*this = 10 * *this + (w - '0');
+			break;
+		default: printw("%c", toupper(w));
 		}
 		doupdate();
 	}
+fini:
 	endwin();
 	return 0;
 }
