2cb37ae06637d45d6b71e94c62bd2085ef4ba71c
William Pursell <william.r.pursell@gmail.com>
2025-09-22 06:55:47 -0600

    Stop using wide
    This is not currently building on my broken laptop.  Just
    discard for now.
    
    type: curses

--- c/src/build-errors/ncurses.c
+++ c/src/build-errors/ncurses.c
@@ -1,26 +1,30 @@
 #include <err.h>
 #include <curses.h>
+#include <ctype.h>
 #include <stdlib.h>
 
 int
 main(void)
 {
 	int r;
-	wint_t w = 0;
+	int w = 0;
 	unsetenv("COLUMNS");
 	unsetenv("LINES");
 	if( initscr() == NULL ) {
 		err(1, "initscr");
 	}
-	while( (r = get_wch(&w)) != ERR ) {
-		if( w == KEY_RESIZE ) {
+	printw("Type some characters, 'q' to quit\n");
+	while( (w = getch()) != ERR ) {
+		if (w == KEY_RESIZE) {
 			int y, x;
 			getmaxyx(stdscr, y, x);
 			printw("y = %d; x = %d\n", y, x);
 			continue;
 		}
-		char c[128];
-		if( wctomb(c, w) == 1 && w >= 'a' && w <= 'z' ) {
+		if (w == 'q') {
+			break;
+		}
+		if (w >= 'a' && w <= 'z') {
 			printw("%c", toupper(w));
 		}
 		doupdate();
