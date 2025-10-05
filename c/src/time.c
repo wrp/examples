8efc467d53309008a9afd7d834dc5f56433d5817
William Pursell <william.r.pursell@gmail.com>
2025-10-05 06:22:18 -0600

    Add timeval macros
    type: c
    
    # Previously edited commit message:
    # Reset note upon receipt of SIGINT
    # 
    # type: c

--- c/src/time.c
+++ c/src/time.c
@@ -11,6 +11,21 @@ struct timespec == { .tv_sec, .tv_nsec }
 
 Common macros:
 	timersub(&end, &start, &diff);   // timeval
+	timerclear(struct timeval *tv) → set to 0
+	timerisset(const struct timeval *tv) → non-zero?
+	timercmp(const struct timeval *a, const struct timeval *b, CMP) →
+		compare using a relational operator (e.g., <, <=, ==, >=, >).
+	timeradd(const struct timeval *a, const struct timeval *b,
+		struct timeval *res) → res = a + b
+		(normalizes tv_usec < 1,000,000).
+	timersub(const struct timeval *a, const struct timeval *b,
+		struct timeval *res) → res = a − b (normalizes).
+
+	Conversions:
+	TIMEVAL_TO_TIMESPEC(const struct timeval *tv, struct timespec *ts)
+	TIMESPEC_TO_TIMEVAL(struct timeval *tv, const struct timespec *ts).
+	Some systems (OpenBSD) provide timerisvalid(const struct timeval *tv)
+
 
 	timespecadd(a,b,result);         // timespec
 	timespecsub(a,b,result);         // timespec
