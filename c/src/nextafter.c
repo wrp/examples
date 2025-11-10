a2ea8d1e43f067f2c38a78936853dd5e1a66751e
William Pursell <william.r.pursell@gmail.com>
2025-11-10 10:38:21 -0700

    Add some more nextafter examples
    ticket: QUI-13
    
    type: c

--- c/src/nextafter.c
+++ c/src/nextafter.c
@@ -3,7 +3,9 @@
  * the closest representable number to .01
  */
 
+#include <float.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 int
@@ -16,5 +18,25 @@ main(int argc, char **argv)
 		r = nextafter(r, 0.0);
 		printf("      --> %30.30f\n", r);
 	}
+
+	if(argc > 1) {
+		float f = strtof(argv[1], NULL);
+		double d = strtod(argv[1], NULL);
+		long double ld = strtold(argv[1], NULL);
+
+		float nf = nextafterf(f, INFINITY);
+		double nd = nextafter(d, INFINITY);
+		long double nld = nextafterl(ld, INFINITY);
+
+		printf( "f     = %.119f\n", f);
+		printf( "nextf = %.119f\n", nf );
+		printf( "prevf = %.119f\n", nextafterf(f, -INFINITY));
+
+		printf( "float : %g - %g = %g\n", nf, f, nf - f );
+		printf( "double: %lg - %lg = %lg\n", nd, d, nd - d );
+		printf( "long  : %Lg = %Lg = %Lg\n", nld, ld, nld - ld );
+	}
+
+
 	return 0;
 }
