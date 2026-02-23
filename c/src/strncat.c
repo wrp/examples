05e24248cc61024dbac3c5ae969b1db2d5419087
William Pursell <william.r.pursell@gmail.com>
2026-02-22 18:56:11 -0700

    Add some strncat assertions

--- c/src/strncat.c
+++ c/src/strncat.c
@@ -13,5 +13,16 @@ main(int argc, char **argv)
 	dest[0] = '\0';
 	strncat(dest, "abcdefg", 6);
 	assert(0 == strcmp(dest, "abcdef"));
+
+	dest[0] = '\0';
+	strncat(dest, "XXXXXXX", 7);
+	dest[0] = '\0';
+	strncat(dest, "ab", 3);
+
+	assert(dest[2] == '\0');
+	assert(dest[3] == 'X');
+	assert(dest[4] == 'X');
+
+
 	return 0;
 }
