3e908f56a39db63915b88a70ba0837ce28e3553c
William Pursell <william.r.pursell@gmail.com>
2026-04-20 07:41:22 -0600

    Demonstrate copy assignment operator
    Type: cxx

--- cxx/emplace_back.cc
+++ cxx/emplace_back.cc
@@ -10,13 +10,18 @@ public:
 	int i;
 	Foo(int x) : i{x} { cout << "regular ctor: " << i << "\n";}
 	Foo(Foo&& other) {
-		cout << "move constructor: " << i << "<--" << other.i << '\n';
+		cout << "move ctor: " << this->i << "<--" << other.i << '\n';
 	}
 	Foo& operator=(Foo&& other) {
-		cout << "move operator: " << i << "<--" << other.i << '\n';
-		return other;
+		cout << "move op: " << this->i << "<--" <<  other.i << '\n';
+		return *this;
 	}
-
+	Foo& operator=(const Foo& other) {
+		cout << "copy assignment operator " <<
+			this->i << "<--" <<  other.i << '\n';
+		i = other.i;
+		return *this;
+	}
 };
 
 
@@ -26,15 +31,22 @@ main()
 	vector<unique_ptr<Foo>> x{};
 
 	unique_ptr<Foo> a = make_unique<Foo>(7);
-	unique_ptr<Foo> b = make_unique<Foo>(9);
+	Foo *bp = new Foo(9);
+	auto b = std::unique_ptr<Foo>(bp);
 
-	x.push_back(std::move(a));
+	x.push_back(std::move(a));  // Does not use move ctor
 	x.push_back(std::move(b));
-
 	x.emplace_back(make_unique<Foo>(11));
 
 	for (const auto &f: x) {
 		cout << f->i << '\n';
 	}
+
+
+	Foo c(3);
+	Foo d(5);
+	Foo e = std::move(c);   /* move assignment ctor */
+	c = e;   /* copy assignment operator */
+
 	return 0;
 }
