4c8d856e701a9840cad2ca6fe97e1975d20e0969
William Pursell <william.r.pursell@gmail.com>
2026-04-20 15:29:36 -0600

    Fix style in class.cc
    Split double declaration.  Add space before brace.
    
    Type: cxx

--- cxx/class.cc
+++ cxx/class.cc
@@ -7,7 +7,8 @@ using namespace std;
 
 class foo {
 public:
-	double x, y;
+	double x;
+	double y;
 
 	foo() : x{1.0}, y{2.0} {};
 	foo(double x, double y) : x{x}, y{y} {}
@@ -16,7 +17,7 @@ public:
 	void bar(int);
 };
 
-void foo::bar(int z){
+void foo::bar(int z) {
 	x = z + y;
 }
 
