de403d42f04115e76791c1f3da7a333892dd4ca9
William Pursell <william.r.pursell@gmail.com>
2026-04-20 15:00:36 -0600

    Fix malloc.cc
    Check for nullptr, report error, and free.
    
    Type: cxx

--- cxx/malloc.cc
+++ cxx/malloc.cc
@@ -3,6 +3,7 @@
 // IMO, malloc should *never* be used in C++.  But if you do
 // use it, use it with the proper cast:
 
+#include <cstdio>
 #include <cstdlib>
 
 int
@@ -10,5 +11,10 @@ main()
 {
 	int n = 5;
 	int *x = static_cast<decltype(x)>(malloc(n * sizeof *x));
+	if (x == nullptr) {
+		perror("malloc");
+		return 1;
+	}
+	free(x);
 	return 0;
 }
