7bd0329220a2d343a0a5284c37589bea5f464c82
William Pursell <william.r.pursell@gmail.com>
2026-07-28 12:31:43 -0600

    Demonstrate function assignment
    Type: go

--- go/src/print.go
+++ go/src/print.go
@@ -4,12 +4,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
-	fmt.Println("hello", "world")      // Println does insert space
+	var p func(...any) (int, error)
+	p = fmt.Println
+	p("hello world")
+	p("hello", "world")      // Println does insert space
 
 	// Print inserts a space between arguments only if neither is a string
-	fmt.Print("hello", "world", "\n")  // Print does *not* insert space
-	fmt.Print("hello", 6, 7, "world", "\n")  // Does insert space between 6 and 7
+	p = fmt.Print
+	p("hello", "world", "\n")  // Print does *not* insert space
+	p("hello", 6, 7, "world", "\n")  // Does insert space between 6 and 7
 
 
 }
