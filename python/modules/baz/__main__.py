1b09a4ebbe47fa97087116c90c47a7f72a83427b
William Pursell <william.r.pursell@gmail.com>
2026-07-31 04:45:33 -0600

    Demonstrate __main__ in module invocation
    Type: python

---
+++ python/modules/baz/__main__.py
@@ -0,0 +1,2 @@
+
+print(f'This is called via "python -m baz"; file:{__file__}, name{__name__}')
--- python/modules/qux.py
+++ python/modules/qux.py
@@ -1,3 +1,5 @@
 
+print('importing module {} from file {}'.format( __name__, __file__))
+
 def f():
-    print('importing module {} from file {}'.format( __name__, __file__))
+    print(f'calling function f in module {__name__} from file {__file__}')
