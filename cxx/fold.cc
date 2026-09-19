436d9512612017e232e6d8ab6aaba723d6c63a83
William Pursell <william.r.pursell@gmail.com>
2026-09-19 13:00:19 -0600

    Demonstrate left/right fold
    Type: cxx

--- cxx/fold.cc
+++ cxx/fold.cc
@@ -13,8 +13,22 @@ auto sum(Args... args) {
 	return (args + ... ); // Right fold: return the sum of all arguments
 }
 
+template<typename... Args>
+auto right_subtract(Args... args) {
+	return (args - ...); // Right fold: (a - (b - c))
+}
+
+template<typename... Args>
+auto left_subtract(Args... args) {
+	return (... - args); // Left fold: ((a - b) - c)
+}
+
+
 int
 main() {
 	assert(sum(1, 2, 3, 4, 5) == 15);
 	assert(sum(string("foo"), string("bar")) == string("foobar"));
+
+	assert(right_subtract(5, 4, 3) == 4);
+	assert(left_subtract(5, 4, 3) == -2);
 }
