2f066e2b6efe9466c507bf8d5499397cce20b7ec
William Pursell <william.r.pursell@gmail.com>
2026-09-21 17:37:55 -0600

    Remove unused member
    Mostly to silence a compiler warning.
    
    Type: cxx

--- cxx/move-constructor.cc
+++ cxx/move-constructor.cc
@@ -1,32 +1,30 @@
 
+#include <cassert>
 #include <string>
 #include <utility>
 #include <iostream>
 
 class A {
-	int member{};
 public:
 	~A() {}; // User defined destructor;
 };
 
 class B {
-	int member{};
 public:
 	B(const B &obj) {} // User defined copy constructor
 };
 
 class C {
-	int member{};
 };
 
 int main() {
-	std::cout << "A: " << std::is_trivially_move_constructible<A>::value << std::endl;
-	// A: 0 since A has a user-defined destructor.
+	// False since A has a user-defined destructor.
+	assert(! std::is_trivially_move_constructible<A>::value);
 
-	std::cout << "B: " << std::is_trivially_move_constructible<B>::value << std::endl;
-	// B: 0 since B has a user-defined copy-constructor.
+	// False since B has a user-defined copy-constructor.
+	assert(! std::is_trivially_move_constructible<B>::value);
 
-	std::cout << "C: " << std::is_trivially_move_constructible<C>::value << std::endl;
-	// C: 1 all the rules for generating an implicit move are satisfied.
+	// True: all the rules for generating an implicit move are satisfied.
+	assert(std::is_trivially_move_constructible<C>::value);
 }
 
